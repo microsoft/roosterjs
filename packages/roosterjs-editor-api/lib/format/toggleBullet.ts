85bb4f0ee52fddf84147cfb54aa894955601cb59
REDMOND\jisong <jisong@microsoft.com>
2017-09-26 16:19:30 -0700

    fix comments

--- packages/roosterjs-editor-api/lib/format/toggleBullet.ts
+++ packages/roosterjs-editor-api/lib/format/toggleBullet.ts
@@ -5,32 +5,31 @@ import { NodeType } from 'roosterjs-editor-types';
 
 // Edge may incorrectly put cursor after toggle bullet, workaround it by adding a space.
 // The space will be removed by Edge after toggle bullet
-export function workaroundForEdge(editor: Editor): boolean {
+export function workaroundForEdge(editor: Editor): HTMLElement {
     if (browserData.isEdge) {
-        let node = getNodeAtCursor(editor);
-        if (node.nodeType == NodeType.Element && (<Element>node).textContent == '') {
-            (<Element>node).textContent = ' ';
-            return true;
+        let node = getNodeAtCursor(editor) as Element;
+        if (node.nodeType == NodeType.Element && node.textContent == '') {
+            let span = editor.getDocument().createElement('span');
+            node.insertBefore(span, node.firstChild);
+            span.innerHTML = 'a';
+            return span;
         }
     }
 
-    return false;
+    return null;
 }
 
-export function removeWorkaroundForEdge(editor: Editor) {
-    let node = getNodeAtCursor(editor);
-    if (node.nodeType == NodeType.Element && (<Element>node).textContent == ' ') {
-        (<Element>node).textContent = '';
+export function removeWorkaroundForEdge(workaroundSpan: HTMLElement) {
+    if (workaroundSpan && workaroundSpan.parentNode) {
+        workaroundSpan.parentNode.removeChild(workaroundSpan);
     }
 }
 
 export default function toggleBullet(editor: Editor): void {
     editor.focus();
     execFormatWithUndo(editor, () => {
-        let hasWoraround = workaroundForEdge(editor);
+        let workaroundSpan = workaroundForEdge(editor);
         editor.getDocument().execCommand('insertUnorderedList', false, null);
-        if (hasWoraround) {
-            removeWorkaroundForEdge(editor);
-        }
+        removeWorkaroundForEdge(workaroundSpan);
     });
 }
--- packages/roosterjs-editor-api/lib/format/toggleNumbering.ts
+++ packages/roosterjs-editor-api/lib/format/toggleNumbering.ts
@@ -5,10 +5,8 @@ import { workaroundForEdge, removeWorkaroundForEdge } from './toggleBullet';
 export default function toggleNumbering(editor: Editor): void {
     editor.focus();
     execFormatWithUndo(editor, () => {
-        let hasWorkaround = workaroundForEdge(editor);
+        let workaroundSpan = workaroundForEdge(editor);
         editor.getDocument().execCommand('insertOrderedList', false, null);
-        if (hasWorkaround) {
-            removeWorkaroundForEdge(editor);
-        }
+        removeWorkaroundForEdge(workaroundSpan);
     });
 }
