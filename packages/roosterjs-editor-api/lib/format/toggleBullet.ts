c336a91b3dd6834d2cd47ad85410117bfb6f7af0
Jiuqing Song <jisong@microsoft.com>
2017-09-26 18:25:03 -0700

    Fix bullet bug (#2)
    * Fix bullet bug
    
    * fix comments

--- packages/roosterjs-editor-api/lib/format/toggleBullet.ts
+++ packages/roosterjs-editor-api/lib/format/toggleBullet.ts
@@ -1,9 +1,35 @@
 import execFormatWithUndo from './execFormatWithUndo';
-import { Editor } from 'roosterjs-editor-core';
+import getNodeAtCursor from '../cursor/getNodeAtCursor';
+import { Editor, browserData } from 'roosterjs-editor-core';
+import { NodeType } from 'roosterjs-editor-types';
+
+// Edge may incorrectly put cursor after toggle bullet, workaround it by adding a space.
+// The space will be removed by Edge after toggle bullet
+export function workaroundForEdge(editor: Editor): HTMLElement {
+    if (browserData.isEdge) {
+        let node = getNodeAtCursor(editor) as Element;
+        if (node.nodeType == NodeType.Element && node.textContent == '') {
+            let span = editor.getDocument().createElement('span');
+            node.insertBefore(span, node.firstChild);
+            span.innerHTML = 'a';
+            return span;
+        }
+    }
+
+    return null;
+}
+
+export function removeWorkaroundForEdge(workaroundSpan: HTMLElement) {
+    if (workaroundSpan && workaroundSpan.parentNode) {
+        workaroundSpan.parentNode.removeChild(workaroundSpan);
+    }
+}
 
 export default function toggleBullet(editor: Editor): void {
     editor.focus();
     execFormatWithUndo(editor, () => {
+        let workaroundSpan = workaroundForEdge(editor);
         editor.getDocument().execCommand('insertUnorderedList', false, null);
+        removeWorkaroundForEdge(workaroundSpan);
     });
 }
