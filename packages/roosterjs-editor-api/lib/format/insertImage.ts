cd8bef6318148038b52e126ddf71bf558e7a2db8
Jiuqing Song <jisong@microsoft.com>
2017-12-05 09:44:38 -0800

    Trigger event for insertImage

--- packages/roosterjs-editor-api/lib/format/insertImage.ts
+++ packages/roosterjs-editor-api/lib/format/insertImage.ts
@@ -1,4 +1,5 @@
 import { Editor } from 'roosterjs-editor-core';
+import { PluginEvent, PluginEventType } from 'roosterjs-editor-types';
 
 export default function insertImage(editor: Editor, imageFile: File) {
     editor.addUndoSnapshot();
@@ -9,6 +10,10 @@ export default function insertImage(editor: Editor, imageFile: File) {
             image.src = (event.target as FileReader).result;
             image.style.maxWidth = '100%';
             editor.insertNode(image);
+            editor.triggerEvent({
+                eventType: PluginEventType.ContentChanged,
+                source: 'Format',
+            } as PluginEvent);
             editor.addUndoSnapshot();
         }
     };
