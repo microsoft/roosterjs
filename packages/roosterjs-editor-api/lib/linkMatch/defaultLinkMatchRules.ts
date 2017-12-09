eceabc2e2a34f619e0bd61bf633b8a277eb0f350
Jiuqing Song <jisong@microsoft.com>
2017-12-08 21:40:35 -0800

    support microsoft-edge protocol

--- packages/roosterjs-editor-api/lib/linkMatch/defaultLinkMatchRules.ts
+++ packages/roosterjs-editor-api/lib/linkMatch/defaultLinkMatchRules.ts
@@ -27,6 +27,12 @@ const httpMatchingRegEx = /^http:\/\/\S+|www\.\S+/i;
 // https matching regex
 const httpsMatchingRegEx = /^https:\/\/\S+/i;
 
+// http matching regex
+const edgeHttpMatchingRegEx = /^microsoft-edge:http:\/\/\S+|www\.\S+/i;
+
+// https matching regex
+const edgeHttpsMatchingRegEx = /^microsoft-edge:https:\/\/\S+/i;
+
 // mailto matching regex
 const mailtoMatchingRegEx = /^mailto:\S+@\S+\.\S+/i;
 
@@ -58,6 +64,8 @@ const waisMatchingRegEx = /^wais:\S+/i;
 const defaultLinkMatchRules: LinkMatchRule[] = [
     new RegExLinkMatchRule('http', 'http://', httpMatchingRegEx, httpExcludeRegEx),
     new RegExLinkMatchRule('https', 'https://', httpsMatchingRegEx, httpExcludeRegEx),
+    new RegExLinkMatchRule('microsoft-edge:http', 'microsoft-edge:http://', edgeHttpMatchingRegEx),
+    new RegExLinkMatchRule('microsoft-edge:https', 'microsoft-edge:https://', edgeHttpsMatchingRegEx),
     new RegExLinkMatchRule('mailto', 'mailto:', mailtoMatchingRegEx),
     new RegExLinkMatchRule('notes', 'notes://', notesMatchingRegEx),
     new RegExLinkMatchRule('file', 'file://', fileMatchingRegEx),
