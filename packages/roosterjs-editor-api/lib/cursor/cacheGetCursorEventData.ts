2c041ebe46c0f6caeac9fb5c85d3e7f51e837fda
REDMOND\bowang <bowang@microsoft.com>
2018-01-17 11:58:14 -0800

    Add comments for exported functions in roosterjs-editor-api/lib/cursor and roosterjs/

--- packages/roosterjs-editor-api/lib/cursor/cacheGetCursorEventData.ts
+++ packages/roosterjs-editor-api/lib/cursor/cacheGetCursorEventData.ts
@@ -4,14 +4,23 @@ import { PluginEvent } from 'roosterjs-editor-types';
 
 const EVENTDATACACHE_CURSORDATA = 'CURSORDATA';
 
-// Read CursorData from plugin event cache. If not, create one
+/**
+ * Read CursorData from plugin event cache. If not, create one
+ * @param event The plugin event
+ * @param editor The editor instance
+ * @returns The cursor data
+ */
 export default function cacheGetCursorEventData(event: PluginEvent, editor: Editor): CursorData {
     return cacheGetEventData<CursorData>(event, EVENTDATACACHE_CURSORDATA, (): CursorData => {
         return new CursorData(editor);
     });
 }
 
-// Clear the cursor data in a plugin event
-export function clearCursorEventDataCache(event: PluginEvent): void {
+/**
+ * Clear the cursor data in a plugin event.
+ * This is called when the cursor data is changed, e.g, the text is replace with HyperLink
+ * @param event The plugin event
+ */
+export function clearCursorEventDataCache(event: PluginEvent) {
     clearEventDataCache(event, EVENTDATACACHE_CURSORDATA);
 }
--- packages/roosterjs-editor-api/lib/cursor/replaceRangeWithNode.ts
+++ packages/roosterjs-editor-api/lib/cursor/replaceRangeWithNode.ts
@@ -1,8 +1,17 @@
 import { Editor } from 'roosterjs-editor-core';
 
-// exactMatch means we're changing the text right before at cursor, reset the selection to end of range
-// ensures that the cursor is in the right place. Otherwise, try to restore to previously stored selection
-// to end of the range
+/**
+ * Replace the specified range with a node
+ * @param editor The editor instance
+ * @param range The range in which content needs to be replaced
+ * @param node The node to be inserted
+ * @param exactMatch True case: in Mentions, you type @nick, and then pick a suggestion from picker, we need to replace exactly everything before cursor (@nick)
+ * with the suggestion from Mentions picker.
+ * False case: In auto linkification, users could type "www.bing.com,<space>". The auto link will kick in on space
+ * at the moment, what is before cursor is "www.bing.com,", however, only "www.bing.com" makes the link. by setting exactMatch = false, it does not match
+ * right from the end, but can scan through till first same char is seen.
+ * @returns True if we complete the replacement, false otherwise
+ */
 function replaceRangeWithNode(
     editor: Editor,
     range: Range,
