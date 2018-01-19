e074676d39748729fd635e0eec0b83991e2b98f4
REDMOND\bowang <bowang@microsoft.com>
2018-01-19 11:38:10 -0800

    Resolved comments.

--- packages/roosterjs-editor-api/lib/format/toggleBold.ts
+++ packages/roosterjs-editor-api/lib/format/toggleBold.ts
@@ -4,6 +4,10 @@ import { Editor } from 'roosterjs-editor-core';
 
 /**
  * Toggle bold at selection
+ * If selection is collapsed, it will only affect the following input after caret
+ * If selection contains only bold text, the bold style will be removed
+ * If selection contains only normal text, bold style will be added to the whole selected text
+ * If selection contains both bold and normal text, bold stle will be added to the whole selected text
  * @param editor The editor instance
  */
 export default function toggleBold(editor: Editor) {
--- packages/roosterjs-editor-api/lib/format/toggleBullet.ts
+++ packages/roosterjs-editor-api/lib/format/toggleBullet.ts
@@ -37,6 +37,10 @@ export function removeWorkaroundForEdge(workaroundSpan: HTMLElement) {
 
 /**
  * Toggle bullet at selection
+ * If selection contains bullet in deep level, toggle bullet will decrease the bullet level by one
+ * If selection contains number list, toggle bullet will convert the number list into bullet list
+ * If selection contains both bullet/numbering and normal text, the behavior is decided by corresponding
+ * browser execCommand API
  * @param editor The editor instance
  */
 export default function toggleBullet(editor: Editor) {
