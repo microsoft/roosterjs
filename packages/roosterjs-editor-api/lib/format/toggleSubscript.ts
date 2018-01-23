a0b46dd5d2feb2a12541b0d04780dbdff36f69fb
REDMOND\bowang <bowang@microsoft.com>
2018-01-23 12:32:46 -0800

    Resolved comments.

--- packages/roosterjs-editor-api/lib/format/toggleSubscript.ts
+++ packages/roosterjs-editor-api/lib/format/toggleSubscript.ts
@@ -8,6 +8,7 @@ import { Editor } from 'roosterjs-editor-core';
  * If selection contains only subscript text, the subscript style will be removed
  * If selection contains only normal text, subscript style will be added to the whole selected text
  * If selection contains both subscript and normal text, the subscript style will be removed from whole selected text
+ * If selection contains any superscript text, the superscript style will be removed and then subscript style will be applied
  * @param editor The editor instance
  */
 export default function toggleSubscript(editor: Editor) {
--- packages/roosterjs-editor-api/lib/format/toggleSuperscript.ts
+++ packages/roosterjs-editor-api/lib/format/toggleSuperscript.ts
@@ -8,6 +8,7 @@ import { Editor } from 'roosterjs-editor-core';
  * If selection contains only superscript text, the superscript style will be removed
  * If selection contains only normal text, superscript style will be added to the whole selected text
  * If selection contains both superscript and normal text, the superscript style will be removed from whole selected text
+ * If selection contains any subscript text, the subscript style will be removed and then superscript style will be applied
  * @param editor The editor instance
  */
 export default function toggleSuperscript(editor: Editor) {
