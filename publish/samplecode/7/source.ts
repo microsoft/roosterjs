0e68f64cb00f5b6f223f4494c69cb3ba7707f6ac
Jiuqing Song <jisong@microsoft.com>
2018-02-28 22:53:58 -0800

    Add a sample project for plugin

---
+++ publish/samplecode/7/source.ts
@@ -0,0 +1,47 @@
+import { Editor, EditorPlugin } from 'roosterjs-editor-core';
+import { PluginEvent, PluginEventType, PluginDomEvent } from 'roosterjs-editor-types';
+import { createEditor } from 'roosterjs';
+
+const NUMBERS = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+];
+const KEY_0 = 0x30;
+const KEY_9 = 0x39;
+
+// This plugin will insert an English word when user is inputting numbers
+class MyPlugin implements EditorPlugin {
+    private editor: Editor;
+
+    initialize(editor: Editor) {
+        this.editor = editor;
+    }
+
+    dispose() {
+        this.editor = null;
+    }
+
+    onPluginEvent(event: PluginEvent) {
+        if (event.eventType == PluginEventType.KeyPress) {
+            let domEvent = <PluginDomEvent>event;
+            let keyboardEvent = <KeyboardEvent>domEvent.rawEvent;
+            if (keyboardEvent.which >= KEY_0 && keyboardEvent.which <= KEY_9) {
+                let text = NUMBERS[keyboardEvent.which - KEY_0] + ' ';
+                this.editor.insertContent(text);
+                keyboardEvent.preventDefault();
+            }
+        }
+    }
+}
+
+let contentDiv = document.getElementById("contentDiv") as HTMLDivElement;
+let myPlugin = new MyPlugin();
+let editor = createEditor(contentDiv, [ myPlugin ]);
