1c372358598e13bf6692e33d74c3099b3c62cbb4
Jiuqing Song <jisong@microsoft.com>
2018-11-20 17:02:29 -0800

    6.17.3 Do not auto generate DIV before editing

--- packages/roosterjs-editor-core/lib/editor/CorePlugin.ts
+++ packages/roosterjs-editor-core/lib/editor/CorePlugin.ts
@@ -118,7 +118,7 @@ export default class CorePlugin implements EditorPlugin {
      * @param node Current node
      * @returns A new position to select
      */
-    public ensureTypeInElement(position: Position): Position {
+    private ensureTypeInElement(position: Position): Position {
         position = position.normalize();
         let block = getBlockElementAtNode(this.contentDiv, position.node);
         let formatNode: HTMLElement;
