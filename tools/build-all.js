6227f6909cb0221f4f1c0367e46712381ff8f81b
Jiuqing Song <jisong@microsoft.com>
2019-01-03 16:52:37 -0800

    Fix build issue under node 11

--- tools/build-all.js
+++ tools/build-all.js
@@ -7,34 +7,34 @@ var packages = collectPackages(packagePath).map(p => p.replace('packages/', ''))
 var isProduction = checkParam('-p', '-prod');
 
 var typescriptPath = `node ../../node_modules/typescript/lib/tsc.js`;
-function buildPackages(module, folderName) {
-    packages.forEach((package) => {
+function buildPackages(module) {
+    packages.forEach(package => {
         console.log(`building ${package}...`);
 
         var options = {
             stdio: 'inherit',
-            cwd: path.join(packagePath, package)
+            cwd: path.join(packagePath, package),
         };
         exec(`node ../../tools/copy-project-files.js`, options);
 
         exec(typescriptPath + ` -t es5 --moduleResolution node -m ${module}`, options);
     });
-
-    packages.forEach((package) => {
-        var outDirBase = `${packagePath}/../dist/${package}`;
-        fs.renameSync(`${outDirBase}/lib`, `${outDirBase}/${folderName}`);
-    });
 }
 
 if (isProduction) {
     console.log('AMD:');
-    buildPackages('amd', 'lib-amd');
+    buildPackages('amd');
     console.log('');
+
+    packages.forEach(package => {
+        var outDirBase = path.resolve(packagePath, '..', 'dist', package);
+        fs.renameSync(`${outDirBase}/lib`, `${outDirBase}/lib-amd`);
+    });
 }
 
 // important: build lib + commonjs last since the default folder is lib
 console.log('CommonJS:');
-buildPackages('commonjs', 'lib');
+buildPackages('commonjs');
 
 function checkParam() {
     var params = process.argv;
