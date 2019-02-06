b1aadaa5fbe25958401729521e2efebf2a05f385
William Saulnier <wisaulni@microsoft.com>
2019-02-06 15:46:45 -0800

    better comments

--- packages/roosterjs-editor-types/lib/interface/InsertOption.ts
+++ packages/roosterjs-editor-types/lib/interface/InsertOption.ts
@@ -5,40 +5,38 @@ import { ContentPosition } from '../enum/ContentPosition';
  */
 export interface InsertOptionBase {
     /**
-     * Whether need to update cursor
+     * Whether need to update cursor.
      */
     updateCursor?: boolean;
 
     /**
-     * Whether need to replace current selection
-     */
-    replaceSelection?: boolean;
-
-    /**
-     * Whether need to insert the content into a new line
+     * Boolean flag for inserting the content onto a new line.
+     * No-op for ContentPosition.Outside
      */
     insertOnNewLine?: boolean;
+
+    /**
+     * Boolean flag for inserting the content onto a new line.
+     * No-op for ContentPosition.Begin, End, and Outside
+     */
+    replaceSelection?: boolean;
 }
 
-/**
- * The "basic" insertNode related ContentPositions that require no additional parameters to use.
- */
 export interface InsertOptionBasic extends InsertOptionBase {
     position: ContentPosition.Begin | ContentPosition.End | ContentPosition.Outside | ContentPosition.SelectionStart;
 }
 
-/**
- * The Range varient where insertNode will opperate on a range disjointed from the current selection state.
- */
 export interface InsertOptionRange extends InsertOptionBase {
     position: ContentPosition.Range;
-
-    /**
-     * The range to be targeted when insertion happens.
-     */
     range: Range;
 }
 
-type InsertOption = InsertOptionBasic | InsertOptionRange;
+/**
+ * Type definition for the InsertOption, used in the insertNode API.
+ * The position parameter defines how the node will be inserted.
+ * In a future revision, this will become strongly typed
+ * Only parameters applicable to the given position will be accepted.
+ */
+type InsertOption = InsertOptionRange | InsertOptionBasic;
 
 export default InsertOption;
