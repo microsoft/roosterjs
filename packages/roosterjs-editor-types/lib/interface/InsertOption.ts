8a884aaf96ffa60852ed95a8d717bc5094b53671
William Saulnier <wisaulni@microsoft.com>
2019-02-06 15:34:34 -0800

    actual typing now

--- packages/roosterjs-editor-types/lib/interface/InsertOption.ts
+++ packages/roosterjs-editor-types/lib/interface/InsertOption.ts
@@ -1,28 +1,44 @@
 import { ContentPosition } from '../enum/ContentPosition';
 
 /**
- * Options for insertContent API
+ * Shared options for insertNode related APIs
  */
-export default interface InsertOption {
-    /**
-     * Target position
-     */
-    position: ContentPosition;
-
+export interface InsertOptionBase {
     /**
      * Whether need to update cursor
      */
-    updateCursor: boolean;
+    updateCursor?: boolean;
 
     /**
      * Whether need to replace current selection
      */
-    replaceSelection: boolean;
+    replaceSelection?: boolean;
 
     /**
      * Whether need to insert the content into a new line
      */
-    insertOnNewLine: boolean;
-
-    range?: Range;
+    insertOnNewLine?: boolean;
 }
+
+/**
+ * The "basic" insertNode related ContentPositions that require no additional parameters to use.
+ */
+export interface InsertOptionBasic extends InsertOptionBase {
+    position: ContentPosition.Begin | ContentPosition.End | ContentPosition.Outside | ContentPosition.SelectionStart;
+}
+
+/**
+ * The Range varient where insertNode will opperate on a range disjointed from the current selection state.
+ */
+export interface InsertOptionRange extends InsertOptionBase {
+    position: ContentPosition.Range;
+
+    /**
+     * The range to be targeted when insertion happens.
+     */
+    range: Range;
+}
+
+type InsertOption = InsertOptionBasic | InsertOptionRange;
+
+export default InsertOption;
