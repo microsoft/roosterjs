4e499388c7c059c52c9350b140516e6eb4a98f2e
JiuqingSong <jisong@microsoft.com>
2019-02-27 23:06:08 -0800

    Fix bad merge

--- packages/roosterjs-editor-core/lib/editor/Editor.ts
+++ packages/roosterjs-editor-core/lib/editor/Editor.ts
@@ -1,7 +1,6 @@
 import createEditorCore from './createEditorCore';
 import EditorCore from '../interfaces/EditorCore';
 import EditorOptions from '../interfaces/EditorOptions';
-import { Browser, getRangeFromSelectionPath, getSelectionPath } from 'roosterjs-editor-dom';
 import { GenericContentEditFeature } from '../interfaces/ContentEditFeature';
 import {
     BlockElement,
@@ -373,7 +372,7 @@ export default class Editor {
     public getContent(
         triggerExtractContentEvent: boolean = true,
         includeSelectionMarker: boolean = false,
-        normalizeColor: boolean = true,
+        normalizeColor: boolean = true
     ): string {
         let contentDiv = this.core.contentDiv;
         let content = contentDiv.innerHTML;
@@ -435,7 +434,11 @@ export default class Editor {
      * @param triggerContentChangedEvent True to trigger a ContentChanged event. Default value is true
      * @param convertToDarkMode True to conver the editor's new content to dark mode formatting. Default value is false.
      */
-    public setContent(content: string, triggerContentChangedEvent: boolean = true, convertToDarkMode?: boolean) {
+    public setContent(
+        content: string,
+        triggerContentChangedEvent: boolean = true,
+        convertToDarkMode?: boolean
+    ) {
         let contentDiv = this.core.contentDiv;
         if (contentDiv.innerHTML != content) {
             contentDiv.innerHTML = content || '';
@@ -448,7 +451,7 @@ export default class Editor {
                     this.deleteNode(pathComment);
                     let range = getRangeFromSelectionPath(contentDiv, path);
                     this.select(range);
-                } catch { }
+                } catch {}
             }
 
             if (convertToDarkMode) {
@@ -667,8 +670,8 @@ export default class Editor {
         nameOrMap:
             | string
             | {
-                [eventName: string]: (event: UIEvent) => void;
-            },
+                  [eventName: string]: (event: UIEvent) => void;
+              },
         handler?: (event: UIEvent) => void
     ): () => void {
         if (nameOrMap instanceof Object) {
@@ -913,13 +916,15 @@ export default class Editor {
 
         const currentContent = this.getContent(
             undefined /* triggerContentChangedEvent */,
-            true /* getSelectionMarker */);
+            true /* getSelectionMarker */
+        );
         this.core.inDarkMode = nextDarkMode;
         if (nextDarkMode) {
             this.setContent(
                 currentContent,
                 undefined /* triggerContentChangedEvent */,
-                true /* convertToDarkMode */);
+                true /* convertToDarkMode */
+            );
         } else {
             this.setContent(currentContent);
         }
@@ -961,17 +966,19 @@ export default class Editor {
             childElements = Array.prototype.slice.call(node.querySelectorAll('*'));
         }
 
-        return childElements.length > 0 ? () => {
-            const darkModeOptions = this.getDarkModeOptions();
-            childElements.forEach((element) => {
-                if (darkModeOptions && darkModeOptions.onExternalContentTransform) {
-                    darkModeOptions.onExternalContentTransform(element);
-                } else {
-                    element.style.color = null;
-                    element.style.backgroundColor = null;
-                }
-            });
-        } : null;
+        return childElements.length > 0
+            ? () => {
+                  const darkModeOptions = this.getDarkModeOptions();
+                  childElements.forEach(element => {
+                      if (darkModeOptions && darkModeOptions.onExternalContentTransform) {
+                          darkModeOptions.onExternalContentTransform(element);
+                      } else {
+                          element.style.color = null;
+                          element.style.backgroundColor = null;
+                      }
+                  });
+              }
+            : null;
     }
 
     //#endregion
