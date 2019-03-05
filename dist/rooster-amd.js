d107b973796b403ca3ddc82f18fcafdcee034999
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-03-05 19:13:52 +0000

    Deploy Microsoft/roosterjs to github.com/Microsoft/roosterjs.git:gh-pages

--- dist/rooster-amd.js
+++ dist/rooster-amd.js
@@ -1790,7 +1790,12 @@ var attachDomEvent = function (core, eventName, pluginEventType, beforeDispatch)
         // This detection is not 100% accurate. event.key is not fully supported by all brwosers, and in some browser (e.g. IE)
         // event.key is longer than 1 for num pad input. But here we just want to improve performance as mush as possible.
         // So if we missed some case here it is still acceptable.
-        if (isKeyboardEvent(event) && event.key && event.key.length == 1) {
+        if (isKeyboardEvent(event) &&
+            !event.ctrlKey &&
+            !event.altKey &&
+            !event.metaKey &&
+            event.key &&
+            event.key.length == 1) {
             event.stopPropagation();
         }
         if (beforeDispatch) {
