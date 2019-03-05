97ef2408a6a13d1c8aa8bc4f99efe46533ca19fe
Adrian Tran <adtran@microsoft.com>
2019-03-05 14:13:20 -0800

    Improve hyperlink onLinkClick option (#255)
    Allow Hyperlink onLinkClick to fall back to default handling to reduce redundant code.

--- packages/roosterjs-editor-plugins/lib/HyperLink/HyperLink.ts
+++ packages/roosterjs-editor-plugins/lib/HyperLink/HyperLink.ts
@@ -14,12 +14,12 @@ export default class HyperLink implements EditorPlugin {
      * @param getTooltipCallback A callback function to get tooltip text for an existing hyperlink.
      * Default value is to return the href itself. If null, there will be no tooltip text.
      * @param target (Optional) Target window name for hyperlink. If null, will use "_blank"
-     * @param onLinkClick (Optional) Open link callback
+     * @param onLinkClick (Optional) Open link callback (return false to use default behavior)
      */
     constructor(
         private getTooltipCallback: (href: string, a: HTMLAnchorElement) => string = href => href,
         private target?: string,
-        private onLinkClick?: (anchor: HTMLAnchorElement, mouseEvent: MouseEvent) => void
+        private onLinkClick?: (anchor: HTMLAnchorElement, mouseEvent: MouseEvent) => boolean | void
     ) {}
 
     /**
@@ -73,8 +73,7 @@ export default class HyperLink implements EditorPlugin {
             ) as HTMLAnchorElement;
 
             if (anchor) {
-                if (this.onLinkClick) {
-                    this.onLinkClick(anchor, event.rawEvent);
+                if (this.onLinkClick && this.onLinkClick(anchor, event.rawEvent) !== false) {
                     return;
                 }
 
