72da5323118ba592caf264e5d942284725128da1
Jiuqing Song <jisong@microsoft.com>
2019-03-05 11:02:37 -0800

    Fix keyboard event issue (#251)

--- packages/roosterjs-editor-core/lib/coreAPI/attachDomEvent.ts
+++ packages/roosterjs-editor-core/lib/coreAPI/attachDomEvent.ts
@@ -12,7 +12,14 @@ const attachDomEvent: AttachDomEvent = (
         // This detection is not 100% accurate. event.key is not fully supported by all brwosers, and in some browser (e.g. IE)
         // event.key is longer than 1 for num pad input. But here we just want to improve performance as mush as possible.
         // So if we missed some case here it is still acceptable.
-        if (isKeyboardEvent(event) && event.key && event.key.length == 1) {
+        if (
+            isKeyboardEvent(event) &&
+            !event.ctrlKey &&
+            !event.altKey &&
+            !event.metaKey &&
+            event.key &&
+            event.key.length == 1
+        ) {
             event.stopPropagation();
         }
 
