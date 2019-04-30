7c96ad3ad42ac5ddf988dae27697ea5359b4e66a
Jiuqing Song <jisong@microsoft.com>
2019-04-30 10:04:56 -0700

    Export code to codepen from demo site (#289)

--- publish/samplesite/scripts/controls/sidePane/editorOptions/codes/PickerPluginCode.ts
+++ publish/samplesite/scripts/controls/sidePane/editorOptions/codes/PickerPluginCode.ts
@@ -5,24 +5,15 @@ export default class PickerPluginCode extends CodeElement {
         super();
     }
 
-    getImports() {
-        return [
-            {
-                name: 'PickerPlugin',
-                path: 'roosterjs-picker-plugin',
-                isDefault: false,
-            },
-            {
-                name: 'YourPickerPluginDataProvider',
-                path: 'your-picker-plugin-data-provider',
-                isDefault: false,
-            },
-        ];
-    }
-
     getCode() {
         return (
-            'new PickerPlugin(new YourPickerPluginDataProvider(), {\n' +
+            'new roosterjs.PickerPlugin({\n' +
+            '    onInitalize: (insertNodeCallback, setIsSuggestingCallback) => {},\n' +
+            '    onDispose: () => {},\n' +
+            '    onIsSuggestingChanged: (isSuggesting) => {},\n' +
+            '    queryStringUpdated: (queryString) => {},\n' +
+            '    onRemove: (nodeRemoved, isBackwards) => null,\n' +
+            '    }, {\n' +
             "    elementIdPrefix: 'samplepicker-',\n" +
             "    changeSource: 'SAMPLE_COLOR_PICKER',\n" +
             "    triggerCharacter: ':',\n" +
