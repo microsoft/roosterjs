90628f96384eb2e3a30f81e843aa441e0ed47fc3
Francis Meng <xiameng@microsoft.com>
2019-06-05 14:02:15 -0700

    remove comments

--- packages/roosterjs-editor-dom/lib/utils/getTextContent.ts
+++ packages/roosterjs-editor-dom/lib/utils/getTextContent.ts
@@ -3,7 +3,6 @@ import ContentTraverser from '../contentTraverser/ContentTraverser';
 /**
  * get block element's text content.
  * @param rootNode Root node that the get the textContent of.
- * @param startNode Node the node start traverser to start traversing.
  * @returns text content of given text content.
  */
 export default function getTextContent(rootNode: Node): string {
