a0d19049f22fdd30b21c618eebff0bc58d7679f0
JiuqingSong <jisong@microsoft.com>
2019-08-10 22:00:41 -0700

    fix comment

--- packages/roosterjs-editor-dom/lib/utils/splitTextNode.ts
+++ packages/roosterjs-editor-dom/lib/utils/splitTextNode.ts
@@ -6,9 +6,9 @@
  * Otherwise return the second part, and the passed in textNode will become the first part
  */
 export default function splitTextNode(textNode: Text, offset: number, returnFirstPart: boolean) {
-    let firstPart = textNode.nodeValue.substr(0, offset);
-    let secondPart = textNode.nodeValue.substr(offset);
-    let newNode = textNode.ownerDocument.createTextNode(returnFirstPart ? firstPart : secondPart);
+    const firstPart = textNode.nodeValue.substr(0, offset);
+    const secondPart = textNode.nodeValue.substr(offset);
+    const newNode = textNode.ownerDocument.createTextNode(returnFirstPart ? firstPart : secondPart);
     textNode.nodeValue = returnFirstPart ? secondPart : firstPart;
     textNode.parentNode.insertBefore(newNode, returnFirstPart ? textNode : textNode.nextSibling);
     return newNode;
