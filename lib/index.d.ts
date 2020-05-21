8443bc543e04805a41f9ea8a4727ca971df55dd6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-21 19:14:06 +0000

    Deploy microsoft/roosterjs to github.com/microsoft/roosterjs.git:gh-pages

--- lib/index.d.ts
+++
@@ -1,9 +0,0 @@
-export { default as createEditor } from './createEditor';
-export * from 'roosterjs-editor-types';
-export * from 'roosterjs-editor-dom';
-export * from 'roosterjs-editor-core';
-export * from 'roosterjs-editor-api';
-export * from 'roosterjs-editor-plugins';
-export * from 'roosterjs-plugin-image-resize';
-export * from 'roosterjs-html-sanitizer';
-export * from 'roosterjs-plugin-picker';
