5f80ab4c41dca606089e890f754c0cc692d28cfb
William S <lego6245@gmail.com>
2020-05-21 09:54:38 -0700

    re-enable tests and added new ones

--- packages/roosterjs-editor-api/lib/test/format/createLinkTest.ts
+++ packages/roosterjs-editor-api/lib/test/format/createLinkTest.ts
@@ -17,7 +17,20 @@ describe('createLink()', () => {
         TestHelper.removeElement(testID);
     });
 
-    xit('adds <a></a> as link', () => {
+    it('adds <a></a> as link', () => {
+        // Arrange
+        editor.setContent(originalContent);
+        TestHelper.selectNode(document.getElementById('text'));
+
+        // Act
+        createLink(editor, 'https://www.example.com');
+
+        // Assert
+        let link = document.getElementsByTagName('a')[0];
+        expect(link.outerHTML).toBe('<a href="https://www.example.com">text</a>');
+    });
+
+    it('appends a http protocol if it is missing', () => {
         // Arrange
         editor.setContent(originalContent);
         TestHelper.selectNode(document.getElementById('text'));
@@ -30,7 +43,46 @@ describe('createLink()', () => {
         expect(link.outerHTML).toBe('<a href="http://www.example.com">text</a>');
     });
 
-    xit('adds altText in the link', () => {
+    it('appends a mailto: protocol if it is an email address missing', () => {
+        // Arrange
+        editor.setContent(originalContent);
+        TestHelper.selectNode(document.getElementById('text'));
+
+        // Act
+        createLink(editor, 'LauraJPowers@contoso.com');
+
+        // Assert
+        let link = document.getElementsByTagName('a')[0];
+        expect(link.outerHTML).toBe('<a href="mailto:LauraJPowers@contoso.com">text</a>');
+    });
+
+    it('appends a ftp:// protocol if the link starts with an ftp.', () => {
+        // Arrange
+        editor.setContent(originalContent);
+        TestHelper.selectNode(document.getElementById('text'));
+
+        // Act
+        createLink(editor, 'ftp.contoso.com');
+
+        // Assert
+        let link = document.getElementsByTagName('a')[0];
+        expect(link.outerHTML).toBe('<a href="ftp://ftp.contoso.com">text</a>');
+    });
+
+    it('preserves any URI scheme that already exists', () => {
+        // Arrange
+        editor.setContent(originalContent);
+        TestHelper.selectNode(document.getElementById('text'));
+
+        // Act
+        createLink(editor, 'steam://gaben@valvesoftware.com');
+
+        // Assert
+        let link = document.getElementsByTagName('a')[0];
+        expect(link.outerHTML).toBe('<a href="steam://gaben@valvesoftware.com">text</a>');
+    });
+
+    it('adds altText in the link', () => {
         // Arrange
         editor.setContent(originalContent);
         TestHelper.selectNode(document.getElementById('text'));
