f25bd70edd116e02b17907d065c690f5135083ce
Jiuqing Song <jisong@microsoft.com>
2020-05-26 12:23:44 -0700

    VList preparation Step 1 (#418)
    * VList preparation 1
    
    * Fix Position bug
    
    * Skip unstable tests

--- packages/roosterjs-editor-api/lib/test/utils/processListTest.ts
+++ packages/roosterjs-editor-api/lib/test/utils/processListTest.ts
@@ -1,6 +1,6 @@
 import * as TestHelper from '../TestHelper';
 import processList from '../../utils/processList';
-import { DocumentCommand } from 'roosterjs-editor-types';
+import { DocumentCommand, PositionType } from 'roosterjs-editor-types';
 import { Editor } from 'roosterjs-editor-core';
 
 describe('processList()', () => {
@@ -19,72 +19,105 @@ describe('processList()', () => {
     it('properly outdents, preserving the span', () => {
         // Arrange
         const originalContent =
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li><li><span style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"><img id="focus helper" /><br></span></li></ul></div>';
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+            '<ul><li>test</li><li><span id="focusNode" style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"></span></li></ul>' +
+            '</div>';
         editor.setContent(originalContent);
-        const focusNode = document.getElementById('focus helper');
-        TestHelper.setSelection(focusNode, 0);
-        editor.deleteNode(focusNode);
+        editor.focus();
+        editor.select(document.getElementById('focusNode'), PositionType.Begin);
 
         // Act
         processList(editor, DocumentCommand.Outdent);
 
         // Assert
         expect(editor.getContent()).toBe(
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li></ul><div><span style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"><br></span></div></div>'
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+                '<ul><li>test</li></ul>' +
+                '<div><span id="focusNode" style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"></span></div>' +
+                '</div>'
         );
     });
 
     it('properly outdents from the middle, preserving the span.', () => {
         // Arrange
         const originalContent =
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li><li><span style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"><img id="focus helper" /><br></span></li><li>test</li></ul></div>';
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+            '<ul><li>test</li><li><span id="focusNode" style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"></span></li><li>test</li></ul>' +
+            '</div>';
         editor.setContent(originalContent);
-        const focusNode = document.getElementById('focus helper');
-        TestHelper.setSelection(focusNode, 0);
-        editor.deleteNode(focusNode);
+        editor.focus();
+        editor.select(document.getElementById('focusNode'), PositionType.Begin);
 
         // Act
         processList(editor, DocumentCommand.Outdent);
 
         // Assert
         expect(editor.getContent()).toBe(
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li></ul><div><span style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"><br></span></div><ul><li>test</li></ul></div>'
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+                '<ul><li>test</li></ul>' +
+                '<div><span id="focusNode" style="font-family: &quot;Courier New&quot;; font-size: 20pt; color: rgb(208, 92, 18);"></span></div><ul><li>test</li></ul>' +
+                '</div>'
         );
     });
 
     it('properly outdents when default format is applied', () => {
         // Arrange
         const originalContent =
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li><li>test</li><li><img id="focus helper" /><br></li></ul></div>';
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+            '<ul><li>test</li><li>test</li><li id="focusNode"><br></li></ul>' +
+            '</div>';
         editor.setContent(originalContent);
-        const focusNode = document.getElementById('focus helper');
-        TestHelper.setSelection(focusNode, 0);
-        editor.deleteNode(focusNode);
+        editor.focus();
+        editor.select(document.getElementById('focusNode'), PositionType.Begin);
 
         // Act
         processList(editor, DocumentCommand.Outdent);
 
         // Assert
         expect(editor.getContent()).toBe(
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li><li>test</li></ul><div><br></div></div>'
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+                '<ul><li>test</li><li>test</li></ul>' +
+                '<div><br></div>' +
+                '</div>'
         );
     });
 
     it('properly outdents from the middle when default format is applied', () => {
         // Arrange
         const originalContent =
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li><li><img id="focus helper" /><br></li><li>test</li></ul></div>';
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+            '<ul><li>test</li><li id="focusNode"><br></li><li>test</li></ul>' +
+            '</div>';
         editor.setContent(originalContent);
-        const focusNode = document.getElementById('focus helper');
-        TestHelper.setSelection(focusNode, 0);
-        editor.deleteNode(focusNode);
+        editor.focus();
+        editor.select(document.getElementById('focusNode'), PositionType.Begin);
 
         // Act
         processList(editor, DocumentCommand.Outdent);
 
         // Assert
         expect(editor.getContent()).toBe(
-            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div><div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><ul><li>test</li></ul><div><br></div><ul><li>test</li></ul></div>'
+            '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">default format</div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);"><br></div>' +
+                '<div style="font-family: Arial; font-size: 16pt; color: rgb(0, 111, 201);">' +
+                '<ul><li>test</li></ul>' +
+                '<div><br></div>' +
+                '<ul><li>test</li></ul>' +
+                '</div>'
         );
     });
 });
