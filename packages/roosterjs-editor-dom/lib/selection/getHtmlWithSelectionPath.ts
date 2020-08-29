06ad39043c095fa5b8efc184937a24f67fd0f764
Jiuqing Song <jisong@microsoft.com>
2020-08-28 22:02:36 -0700

    Fix #475

--- packages/roosterjs-editor-dom/lib/selection/getHtmlWithSelectionPath.ts
+++ packages/roosterjs-editor-dom/lib/selection/getHtmlWithSelectionPath.ts
@@ -1,4 +1,6 @@
 import getSelectionPath from './getSelectionPath';
+import getTagOfNode from '../utils/getTagOfNode';
+import queryElements from '../utils/queryElements';
 
 /**
  * Get inner Html of a root node with a selection path which can be used for restore selection.
@@ -10,6 +12,36 @@ import getSelectionPath from './getSelectionPath';
 export default function getHtmlWithSelectionPath(rootNode: HTMLElement, range: Range): string {
     if (!rootNode) {
         return '';
+    }
+
+    const { startContainer, endContainer, startOffset, endOffset } = range || {};
+    let isDOMChanged = false;
+
+    queryElements(rootNode, 'table', table => {
+        let tbody: HTMLTableSectionElement = null;
+
+        for (let child = table.firstChild; child; child = child.nextSibling) {
+            if (getTagOfNode(child) == 'TR') {
+                if (!tbody) {
+                    tbody = table.ownerDocument.createElement('tbody');
+                    table.insertBefore(tbody, child);
+                }
+
+                tbody.appendChild(child);
+                child = tbody;
+
+                isDOMChanged = true;
+            } else {
+                tbody = null;
+            }
+        }
+    });
+
+    if (range && isDOMChanged) {
+        try {
+            range.setStart(startContainer, startOffset);
+            range.setEnd(endContainer, endOffset);
+        } catch {}
     }
 
     const content = rootNode.innerHTML;
