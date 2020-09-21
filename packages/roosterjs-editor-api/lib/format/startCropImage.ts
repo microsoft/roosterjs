5c76da240e39539f36d2bc9412127fe1d47f7a02
Ania Gonzalez <angon@microsoft.com>
2020-09-21 13:32:28 -0700

    Fix startCropImage event

--- packages/roosterjs-editor-api/lib/format/startCropImage.ts
+++ packages/roosterjs-editor-api/lib/format/startCropImage.ts
@@ -7,15 +7,8 @@ import { PluginEventType, StartCropEvent, IEditor } from 'roosterjs-editor-types
  */
 export default function startCropImage(editor: IEditor): void {
     editor.addUndoSnapshot(() => {
-        //image.style.transform = 'scale(3,1)'; // this should call the plugin to actually CROP: ImageCrop plugin
-
-        //TRY EVENT FOR PLUGIN
-        //this.editor.triggerContentChangedEvent(ChangeSource.ImageCrop); //try this, otherwise use triggerEvent and create new type of event
-        // could not pass the raw event??
-
         let event: StartCropEvent = {
             eventType: PluginEventType.StartCrop,
-            test: 'ania string test',
         };
 
         editor.triggerPluginEvent(PluginEventType.StartCrop, event, true);
