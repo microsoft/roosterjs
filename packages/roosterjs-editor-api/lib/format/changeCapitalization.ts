ebb8b28f8ba6610d5a548c316717f077730dc815
Ania Gonzalez <aniagr@gmail.com>
2021-01-12 17:14:16 -0800

    Change to using string.prototype.toLocale*Case

--- packages/roosterjs-editor-api/lib/format/changeCapitalization.ts
+++ packages/roosterjs-editor-api/lib/format/changeCapitalization.ts
@@ -12,39 +12,45 @@ export default function g(editor: IEditor, capitalization: Capitalization, langu
     applyInlineStyle(editor, element => {
         for (let node = getFirstLeafNode(element); node; node = getNextLeafSibling(element, node)) {
             if (node.nodeType == NodeType.Text) {
-                if (capitalization == Capitalization.CapitalizeEachWord) {
-                    node.textContent = preprocessText(node.textContent);
-                }
-                node.textContent = getCapitalizedText(node.textContent, capitalization);
+                node.textContent = getCapitalizedText(node.textContent);
             }
         }
     });
 
-    function preprocessText(textToPreprocess: string) {
-        const lowercaseText = getCapitalizedText(textToPreprocess, Capitalization.Lowercase);
-        return lowercaseText;
-    }
+    function getCapitalizedText(originalText: string): string {
+        // // To get the capitalized text, we let the textTransform style do it for us which will take into account
+        // // the language-specific case mapping rules (https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform).
+        // // To retrieve the rendered text, we need a temporary node that is not hidden (so we add it to the document temporarily).
+        // let transformedText;
+        // let document = editor.getDocument();
+        // if (document) {
+        //     let tempNode = document.createElement('div');
+        //     // We need the whiteSpace to be set to Preserve so that spaces are not stripped
+        //     tempNode.style.whiteSpace = 'pre';
+        //     document.body.appendChild(tempNode);
+        //     if (language) {
+        //         tempNode.lang = language;
+        //     }
+        //     tempNode.innerText = originalText;
+        //     tempNode.style.textTransform = capitalization;
+        //     transformedText = tempNode.innerText;
 
-    function getCapitalizedText(originalText: string, capitalization: Capitalization): string {
-        // To get the capitalized text, we let the textTransform style do it for us which will take into account
-        // the language-specific case mapping rules (https://developer.mozilla.org/en-US/docs/Web/CSS/text-transform).
-        // To retrieve the rendered text, we need a temporary node that is not hidden (so we add it to the document temporarily).
-        let transformedText;
-        let document = editor.getDocument();
-        if (document) {
-            let tempNode = document.createElement('div');
-            // We need the whiteSpace to be set to Preserve so that spaces are not stripped
-            tempNode.style.whiteSpace = 'pre';
-            document.body.appendChild(tempNode);
-            if (language) {
-                tempNode.lang = language;
-            }
-            tempNode.innerText = originalText;
-            tempNode.style.textTransform = capitalization;
-            transformedText = tempNode.innerText;
+        //     document.body.removeChild(tempNode);
+        // }
+        // return transformedText;
 
-            document.body.removeChild(tempNode);
+        switch (capitalization) {
+            case Capitalization.Lowercase:
+                return originalText.toLocaleLowerCase(language);
+            case Capitalization.Uppercase:
+                return originalText.toLocaleUpperCase(language);
+            case Capitalization.CapitalizeEachWord:
+                const wordArray = originalText.toLocaleLowerCase(language).split(' ');
+                for (let i = 0; i < wordArray.length; i++) {
+                    wordArray[i] =
+                        wordArray[i].charAt(0).toLocaleUpperCase(language) + wordArray[i].slice(1);
+                }
+                return wordArray.join(' ');
         }
-        return transformedText;
     }
 }
