8670da1f39218e68944cd5c6e18d7ba1443c3e9f
Bi Wu <biwu@microsoft.com>
2021-05-05 09:11:45 -0700

    shift resize table

--- packages/roosterjs-editor-plugins/lib/plugins/TableResize/TableResize.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/TableResize/TableResize.ts
@@ -591,8 +591,7 @@ export default class TableResize implements EditorPlugin {
                     vtable.table.style.height = null;
                     vtable.forEachCellOfCurrentRow(cell => {
                         if (cell.td) {
-                            cell.td.style.height =
-                                cell.td == this.currentTd ? `${newPos - rect.top}px` : null;
+                            cell.td == this.currentTd ? `${newPos - rect.top}px` : null;
                         }
                     });
                 } else {
@@ -621,7 +620,14 @@ export default class TableResize implements EditorPlugin {
                                 : Number.MAX_SAFE_INTEGER;
                     }
 
-                    if (
+                    if (e.shiftKey) {
+                        if (
+                            (!this.isRTL && newPos <= leftBoundary + MIN_CELL_WIDTH) ||
+                            (this.isRTL && newPos >= rightBoundary - MIN_CELL_WIDTH)
+                        ) {
+                            return;
+                        }
+                    } else if (
                         newPos <= leftBoundary + MIN_CELL_WIDTH ||
                         newPos >= rightBoundary - MIN_CELL_WIDTH
                     ) {
@@ -637,14 +643,16 @@ export default class TableResize implements EditorPlugin {
                             : `${newPos - rect.left}px`;
                     });
 
-                    this.nextCellsToResize.forEach(td => {
-                        td.style.wordBreak = 'break-word';
-                        const tdWidth = this.isRTL
-                            ? newPos - parseInt(td.getAttribute('originalLeftBorder'))
-                            : parseInt(td.getAttribute('originalRightBorder')) - newPos;
-                        td.style.boxSizing = 'border-box';
-                        td.style.width = `${tdWidth}px`;
-                    });
+                    if (!e.shiftKey) {
+                        this.nextCellsToResize.forEach(td => {
+                            td.style.wordBreak = 'break-word';
+                            const tdWidth = this.isRTL
+                                ? newPos - parseInt(td.getAttribute('originalLeftBorder'))
+                                : parseInt(td.getAttribute('originalRightBorder')) - newPos;
+                            td.style.boxSizing = 'border-box';
+                            td.style.width = `${tdWidth}px`;
+                        });
+                    }
                 }
                 vtable.writeBack();
             }
