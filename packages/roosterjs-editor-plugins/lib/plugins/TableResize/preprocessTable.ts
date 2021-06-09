2174ee082a40996eb841aa2c06b20409922d01e8
Bi Wu <biwu@microsoft.com>
2021-06-08 17:57:36 -0700

    measure and set at once

--- packages/roosterjs-editor-plugins/lib/plugins/TableResize/preprocessTable.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/TableResize/preprocessTable.ts
@@ -13,22 +13,32 @@ export const enum ResizeState {
     Both, // when resizing the whole table
 }
 
-export function setHTMLElementSizeInPx(element: HTMLElement, dimension: Dimension) {
+export function setHTMLElementSizeInPx(
+    element: HTMLElement,
+    dimension: Dimension,
+    storeOnly: boolean
+) {
     if (!!element) {
-        //element.style.borderColor = 'white';
-
         if (dimension === Dimension.WIDTH || dimension === Dimension.BOTH) {
-            element.removeAttribute('width');
             const rect = normalizeRect(element.getBoundingClientRect());
-            element.style.boxSizing = 'border-box';
-            element.style.width = `${rect.right - rect.left}px`;
+            if (!storeOnly) {
+                element.removeAttribute('width');
+                element.style.boxSizing = 'border-box';
+                element.style.width = `${rect.right - rect.left}px`;
+            } else {
+                element.setAttribute('newWidth', `${rect.right - rect.left}px`);
+            }
         }
 
         if (dimension === Dimension.HEIGHT || dimension === Dimension.BOTH) {
-            element.removeAttribute('height');
             const rect = normalizeRect(element.getBoundingClientRect());
-            element.style.boxSizing = 'border-box';
-            element.style.height = `${rect.bottom - rect.top}px`;
+            if (!storeOnly) {
+                element.removeAttribute('height');
+                element.style.boxSizing = 'border-box';
+                element.style.height = `${rect.bottom - rect.top}px`;
+            } else {
+                element.setAttribute('newHeight', `${rect.bottom - rect.top}px`);
+            }
         }
     }
 }
@@ -44,6 +54,14 @@ export function setEmptyTableCells(table: HTMLTableElement) {
 }
 
 export function setTableCells(table: HTMLTableElement, dimension: Dimension) {
+    // measure and store the width/height into attribute
+    for (let i = 0, row; (row = table.rows[i]); i++) {
+        for (let j = 0, cell; (cell = row.cells[j]); j++) {
+            setHTMLElementSizeInPx(cell, dimension, true);
+        }
+    }
+
+    // set width/height for each cell
     for (let i = 0, row; (row = table.rows[i]); i++) {
         if (dimension === Dimension.BOTH || dimension === Dimension.WIDTH) {
             row.removeAttribute('width');
@@ -60,18 +78,22 @@ export function setTableCells(table: HTMLTableElement, dimension: Dimension) {
             if (dimension === Dimension.BOTH || dimension === Dimension.HEIGHT) {
                 cell.removeAttribute('height');
             }
-            setHTMLElementSizeInPx(cell, dimension);
+
+            cell.style.boxSizing = 'border-box';
+            const newWidth = cell.getAttribute('newWidth');
+            const newHeight = cell.getAttribute('newHeight');
+            if (newWidth) {
+                cell.style.width = newWidth;
+                cell.removeAttribute('newWidth');
+            }
+
+            if (newHeight) {
+                cell.style.height = newHeight;
+                cell.removeAttribute('newHeight');
+            }
         }
     }
 }
-
-/*export function setTableBorders(table: HTMLTableElement, shouldShow: boolean) {
-    for (let i = 0, row; (row = table.rows[i]); i++) {
-        for (let j = 0, cell; (cell = row.cells[j]); j++) {
-            cell.style.borderColor = shouldShow ? 'rgb(212, 212, 212)' : 'white';
-        }
-    }
-}*/
 
 export default function preProcessTable(
     table: HTMLTableElement,
@@ -81,12 +103,12 @@ export default function preProcessTable(
     if (resizeState === ResizeState.Both) {
         setTableCells(table, Dimension.BOTH); // Make sure every cell has both 'width' and 'height' instead of null in order to be resized properly
     } else if (resizeState === ResizeState.Horizontal) {
-        setHTMLElementSizeInPx(table, Dimension.WIDTH); // we should not chnage table width when resizing table height
+        setHTMLElementSizeInPx(table, Dimension.WIDTH, false); // we should not chnage table width when resizing table height
         // unlock table height by removing height properties
         table.removeAttribute('height');
         table.style.height = null;
     } else if (resizeState === ResizeState.Vertical) {
-        setHTMLElementSizeInPx(table, Dimension.BOTH); // Make sure table width/height is fixed to avoid shifting effect
+        setHTMLElementSizeInPx(table, Dimension.BOTH, false); // Make sure table width/height is fixed to avoid shifting effect
         setTableCells(table, Dimension.WIDTH); // Make sure every cell has 'width' instead of null in order to be resized properly
         /*
         Since dragging the last border would cause table width to change, we need to remove width properties
