0a0bf3e0f82cd302af0b66c9d7263f7563ff368d
Jiuqing Song <jisong@microsoft.com>
2021-07-14 21:15:01 -0700

    Fix build

--- packages/roosterjs-editor-plugins/test/imageEdit/getEditInfoFromImageTest.ts
+++ packages/roosterjs-editor-plugins/test/imageEdit/getEditInfoFromImageTest.ts
@@ -1,4 +1,5 @@
 import getEditInfoFromImage from '../../lib/plugins/ImageEdit/editInfoUtils/getEditInfoFromImage';
+import ImageEditInfo from '../../lib/plugins/ImageEdit/types/ImageEditInfo';
 
 const IMG_SRC =
     'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABQAAAAKCAYAAAC0VX7mAAAALUlEQVQ4EWNgYGD4T2U8lAz8TyZACzKEl8k0Dz0OhpKBaGGB7hVi+QgvD0oDATe/bqDDw39VAAAAAElFTkSuQmCC';
@@ -13,15 +14,15 @@ describe('getEditInfoFromImage', () => {
 
         expect(editInfo).toEqual({
             src: '',
-            width: 0,
-            height: 0,
+            widthPx: 0,
+            heightPx: 0,
             naturalWidth: 0,
             naturalHeight: 0,
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            angle: 0,
+            leftPercent: 0,
+            rightPercent: 0,
+            topPercent: 0,
+            bottomPercent: 0,
+            angleRad: 0,
         });
     });
 
@@ -31,31 +32,31 @@ describe('getEditInfoFromImage', () => {
 
         expect(editInfo).toEqual({
             src: IMG_SRC,
-            width: 0,
-            height: 0,
+            widthPx: 0,
+            heightPx: 0,
             naturalWidth: WIDTH,
             naturalHeight: HEIGHT,
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            angle: 0,
+            leftPercent: 0,
+            rightPercent: 0,
+            topPercent: 0,
+            bottomPercent: 0,
+            angleRad: 0,
         });
     });
 
     it('getEditInfoFromImage() returns the edit info of an image which already has edit info', async () => {
         const img = await loadImage(IMG_SRC);
-        const expectedEditInfo = {
+        const expectedEditInfo: ImageEditInfo = {
             src: IMG_SRC,
-            width: 100,
-            height: 100,
+            widthPx: 100,
+            heightPx: 100,
             naturalWidth: WIDTH,
             naturalHeight: HEIGHT,
-            left: 0.1,
-            right: 0.2,
-            top: 0.3,
-            bottom: 0.4,
-            angle: 0.5,
+            leftPercent: 0.1,
+            rightPercent: 0.2,
+            topPercent: 0.3,
+            bottomPercent: 0.4,
+            angleRad: 0.5,
         };
         img.dataset[IMAGE_EDIT_EDITINFO_NAME] = JSON.stringify(expectedEditInfo);
         const editInfo = getEditInfoFromImage(img);
@@ -65,31 +66,31 @@ describe('getEditInfoFromImage', () => {
 
     it('getEditInfoFromImage() returns the edit info of an image which already has invalid edit info', async () => {
         const img = await loadImage(IMG_SRC);
-        const expectedEditInfo = {
+        const expectedEditInfo: ImageEditInfo = <any>{
             src: IMG_SRC,
-            width: 'a',
-            height: 100,
+            widthPx: <any>'a',
+            heightPx: 100,
             naturalWidth: WIDTH,
             naturalHeight: HEIGHT,
-            right: 0.2,
-            top: 0.3,
-            bottom: 0.4,
-            angle: 0.5,
+            rightPercent: 0.2,
+            topPercent: 0.3,
+            bottomPercent: 0.4,
+            angleRad: 0.5,
         };
         img.dataset[IMAGE_EDIT_EDITINFO_NAME] = JSON.stringify(expectedEditInfo);
         const editInfo = getEditInfoFromImage(img);
 
         expect(editInfo).toEqual({
             src: IMG_SRC,
-            width: 0,
-            height: 0,
+            widthPx: 0,
+            heightPx: 0,
             naturalWidth: WIDTH,
             naturalHeight: HEIGHT,
-            left: 0,
-            right: 0,
-            top: 0,
-            bottom: 0,
-            angle: 0,
+            leftPercent: 0,
+            rightPercent: 0,
+            topPercent: 0,
+            bottomPercent: 0,
+            angleRad: 0,
         });
     });
 });
