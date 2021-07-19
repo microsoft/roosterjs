99b9c92361b55f16465341348bb5bc328bfd68d5
Jiuqing Song <jisong@microsoft.com>
2021-07-19 13:33:43 -0700

    Fix #592

--- packages/roosterjs-editor-plugins/lib/plugins/Paste/commonConverter/convertPastedContentForLI.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/Paste/commonConverter/convertPastedContentForLI.ts
@@ -16,20 +16,32 @@ export default function convertPastedContentForLI(fragment: DocumentFragment) {
     // So always assume it is UL here, and later user can change it.
     if (isPureLiNode(fragment)) {
         wrap(toArray(fragment.childNodes), 'UL');
-    } else if (isPureLiNode(fragment.firstChild)) {
+    } else if (
+        safeInstanceOf(fragment.firstChild, 'HTMLElement') &&
+        isPureLiNode(fragment.firstChild)
+    ) {
         changeElementTag(fragment.firstChild as HTMLElement, 'UL');
     }
 }
 
-function isPureLiNode(node: Node) {
-    return (
-        node &&
-        !node.nextSibling &&
-        ['OL', 'UL', 'MENU'].indexOf(getTagOfNode(node)) < 0 &&
-        toArray(node.childNodes).every(
-            childNode =>
-                (safeInstanceOf(childNode, 'Text') && !childNode.nodeValue?.trim()) ||
-                getTagOfNode(childNode) == 'LI'
-        )
-    );
+function isPureLiNode(node: ParentNode & Node) {
+    if (node && !node.nextSibling && ['OL', 'UL', 'MENU'].indexOf(getTagOfNode(node)) < 0) {
+        let hasLi = false;
+        if (
+            toArray(node.childNodes).every(childNode => {
+                if (safeInstanceOf(childNode, 'Text') && !childNode.nodeValue?.trim()) {
+                    return true;
+                } else if (getTagOfNode(childNode) == 'LI') {
+                    hasLi = true;
+                    return true;
+                } else {
+                    return false;
+                }
+            }) &&
+            hasLi
+        ) {
+            return true;
+        }
+    }
+    return false;
 }
