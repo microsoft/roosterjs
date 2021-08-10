c67f3d3998e173abf79e6e20e6db4a7c8784263c
Jiuqing Song <jisong@microsoft.com>
2021-08-09 22:21:23 -0700

    Fix setHtmlWithSelectionPath with DOMPurify (#605)
    * Fix setHtmlWithSelectionPath with DOMPurify
    
    * fix comment

--- packages/roosterjs-editor-dom/lib/selection/setHtmlWithSelectionPath.ts
+++ packages/roosterjs-editor-dom/lib/selection/setHtmlWithSelectionPath.ts
@@ -1,6 +1,8 @@
 import createRange from './createRange';
 import { NodeType, SelectionPath, TrustedHTMLHandler } from 'roosterjs-editor-types';
 
+const LastCommentRegex = /<!--([^-]+)-->$/;
+
 /**
  * Restore inner Html of a root element from given html string. If the string contains selection path,
  * remove the selection path and return a range represented by the path
@@ -13,22 +15,41 @@ export default function setHtmlWithSelectionPath(
     html: string,
     trustedHTMLHandler?: TrustedHTMLHandler
 ): Range {
-    html = html || '';
-    rootNode.innerHTML = trustedHTMLHandler?.(html) || html;
-    let path: SelectionPath = null;
-    let pathComment = rootNode.lastChild;
+    if (!rootNode) {
+        return null;
+    }
 
-    try {
-        path =
-            pathComment &&
-            pathComment.nodeType == NodeType.Comment &&
-            (JSON.parse(pathComment.nodeValue) as SelectionPath);
-        if (path && path.end && path.end.length > 0 && path.start && path.start.length > 0) {
-            rootNode.removeChild(pathComment);
-        } else {
-            path = null;
-        }
-    } catch {}
+    html = html || '';
+    const lastComment = LastCommentRegex.exec(html);
+    rootNode.innerHTML = trustedHTMLHandler?.(html) || html;
+    const path = getSelectionPath(rootNode, lastComment?.[1]);
 
     return path && createRange(rootNode, path.start, path.end);
+}
+
+function getSelectionPath(root: HTMLElement, alternativeComment: string): SelectionPath {
+    let pathCommentValue: string = '';
+    let pathCommentNode: Node = null;
+    let path: SelectionPath = null;
+    if (root.lastChild?.nodeType == NodeType.Comment) {
+        pathCommentNode = root.lastChild;
+        pathCommentValue = pathCommentNode.nodeValue;
+    } else {
+        pathCommentValue = alternativeComment;
+    }
+
+    if (pathCommentValue) {
+        try {
+            path = JSON.parse(pathCommentValue) as SelectionPath;
+            if (path && path.start?.length > 0 && path.end?.length > 0) {
+                if (pathCommentNode) {
+                    root.removeChild(pathCommentNode);
+                }
+            } else {
+                path = null;
+            }
+        } catch {}
+    }
+
+    return path;
 }
