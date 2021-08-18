2ae77842e50d0ea5dddd9db36290c14a2cfcecd5
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2021-08-17 18:44:07 -0600

    Fix issue

--- packages/roosterjs-editor-api/lib/format/setOrderedListNumbering.ts
+++ packages/roosterjs-editor-api/lib/format/setOrderedListNumbering.ts
@@ -21,9 +21,10 @@ export default function setOrderedListNumbering(
         editor.focus();
         const regions = editor.getSelectedRegions();
         regions.forEach(region => {
+            debugger;
             const vList = createVListFromRegion(region, true, separator /*includeSiblingLists*/);
             if (vList) {
-                if (separator != vList.items[1].getNode()) {
+                if (separator != vList.items[0].getNode()) {
                     vList.split(separator);
                     vList.writeBack();
                 }
