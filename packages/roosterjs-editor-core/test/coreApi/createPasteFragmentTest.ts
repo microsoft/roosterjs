092a7f2453ccc977666f136229780acb7a689d79
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2021-09-30 16:18:30 -0600

    Fix

--- packages/roosterjs-editor-core/test/coreApi/createPasteFragmentTest.ts
+++ packages/roosterjs-editor-core/test/coreApi/createPasteFragmentTest.ts
@@ -391,6 +391,29 @@ describe('createPasteFragment', () => {
             '<style>.class1{}</style>'
         );
     });
+
+    it('html input, with one img tag', () => {
+        const triggerEvent = jasmine.createSpy();
+        const core = createEditorCore(div, {
+            coreApiOverride: {
+                triggerEvent,
+            },
+        });
+        const clipboardData: ClipboardData = {
+            types: ['image/png', 'text/html'],
+            text: '',
+            image: null,
+            rawHtml:
+                '<html>\r\n<body>\r\n<!--StartFragment--><img /><!--EndFragment-->\r\n</body>\r\n</html>',
+            customValues: {},
+            snapshotBeforePaste: '<div><br></div><!--{"start":[0,0],"end":[0,0]}-->',
+            htmlFirstLevelChildTags: ['IMG'],
+            html: '<img />',
+        };
+        const fragment = createPasteFragment(core, clipboardData, null, false, false);
+        const html = getHTML(fragment);
+        expect(html).toBe('<img>');
+    });
 });
 
 function getHTML(fragment: DocumentFragment) {
