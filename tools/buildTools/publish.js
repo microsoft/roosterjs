dfb0b1d8edfe0e236db562278d7cfab6b5c915f0
Jiuqing Song <jisong@microsoft.com>
2021-10-29 15:50:24 -0700

    fix build script

--- tools/buildTools/publish.js
+++ tools/buildTools/publish.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const fs = require('fs');
 const exec = require('child_process').execSync;
 const { packages, distPath, readPackageJson } = require('./common');
 
