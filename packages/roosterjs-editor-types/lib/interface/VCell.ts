c7b9104a74359e5fb356eb94445a5c8662218ded
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2021-12-07 18:32:10 -0600

    Remove uneeded change

--- packages/roosterjs-editor-types/lib/interface/VCell.ts
+++ packages/roosterjs-editor-types/lib/interface/VCell.ts
@@ -26,9 +26,4 @@ export default interface VCell {
      * The height in pixel of the actual td (including border and padding)
      */
     height?: number;
-
-    /**
-     * Wether the row is the last one in a Row.
-     */
-    isLastInRow?: boolean;
 }
