c0dcd3c02df757959dd0ec5fc27cd42fc4575bac
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2022-02-07 10:08:16 -0600

    Remove internal

--- packages/roosterjs-editor-dom/lib/utils/normalizeTableSelection.ts
+++ packages/roosterjs-editor-dom/lib/utils/normalizeTableSelection.ts
@@ -1,6 +1,6 @@
 import { TableSelection } from 'roosterjs-editor-types';
+
 /**
- * @internal
  * Make the first Cell of a table selection always be on top of the last cell.
  * @param input Table selection
  * @returns Table Selection where the first cell is always going to be first selected in the table
