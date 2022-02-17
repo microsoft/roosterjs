50b82c20555547f8914649f927749674b87292ec
Simon Winzen <simon-winzen@freenet.de>
2022-02-17 22:56:12 +0100

    Fix for scroll issue in the table resize plugin (#762)
    * table-resize-scroll-fix
    
    * using editor window object for TableResize scroll fix
    
    Co-authored-by: Jiuqing Song <jisong@microsoft.com>

--- packages/roosterjs-editor-plugins/lib/plugins/TableResize/TableResize.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/TableResize/TableResize.ts
@@ -61,7 +61,10 @@ export default class TableResize implements EditorPlugin {
         }
 
         this.ensureTableRects();
-        const { pageX: x, pageY: y } = e;
+
+        const editorWindow = this.editor.getDocument().defaultView;
+        const x = e.pageX - editorWindow.scrollX;
+        const y = e.pageY - editorWindow.scrollY;
         let currentTable: HTMLTableElement | null = null;
 
         for (let i = this.tableRectMap.length - 1; i >= 0; i--) {
