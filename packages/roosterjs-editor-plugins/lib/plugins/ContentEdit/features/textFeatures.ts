d55003c28e8a4b67b1f475ac50e727ac3e23c9a0
Bryan Valverde U <bvalverde@microsoft.com>
2022-03-14 13:06:14 -0600

    Add Tab functionalities 1 (Tab) (#810)
    * init
    
    * init
    
    * Fix
    
    * init
    
    * fix
    
    * use getBlockElementAtNode
    
    * remove uneeded param
    
    * fix comments
    
    * fix
    
    * fix
    
    * remove debugger

---
+++ packages/roosterjs-editor-plugins/lib/plugins/ContentEdit/features/textFeatures.ts
@@ -0,0 +1,127 @@
+import { createRange, Position, queryElements } from 'roosterjs-editor-dom';
+import { setIndentation } from 'roosterjs-editor-api';
+import {
+    BuildInEditFeature,
+    IEditor,
+    Indentation,
+    TextFeatureSettings,
+    Keys,
+    PluginKeyboardEvent,
+    SelectionRangeTypes,
+    ContentPosition,
+    PositionType,
+    ExperimentalFeatures,
+    NodePosition,
+    QueryScope,
+} from 'roosterjs-editor-types';
+
+const TAB_SPACES = 6;
+
+/**
+ * Requires @see ExperimentalFeatures.TabKeyTextFeatures to be enabled
+ * Provides additional functionality when press Tab:
+ *      If Whole Paragraph selected, indent paragraph,
+ *      If range is collapsed, add tab spaces
+ *      If range is not collapsed but not all the paragraph is selected, replace selection with Tab spaces
+ *      If there are more than one block in the selection, indent all selection
+ */
+const IndentWhenTabText: BuildInEditFeature<PluginKeyboardEvent> = {
+    keys: [Keys.TAB],
+    shouldHandleEvent: (event, editor) =>
+        editor.isFeatureEnabled(ExperimentalFeatures.TabKeyTextFeatures) &&
+        !event.rawEvent.shiftKey &&
+        !editor.getElementAtCursor('LI,TABLE', null /*startFrom*/, event),
+    handleEvent: (event, editor) => {
+        const selection = editor.getSelectionRangeEx();
+        if (selection.type == SelectionRangeTypes.Normal) {
+            editor.addUndoSnapshot(() => {
+                if (selection.areAllCollapsed) {
+                    insertTab(editor, event);
+                } else {
+                    const { ranges } = selection;
+                    const range = ranges[0];
+                    if (shouldIndent(editor, range)) {
+                        setIndentation(editor, Indentation.Increase);
+                    } else {
+                        const tempRange = createRange(range.startContainer, range.startOffset);
+                        ranges.forEach(range => range.deleteContents());
+                        editor.select(tempRange);
+                        insertTab(editor, event);
+                    }
+                }
+            });
+
+            event.rawEvent.preventDefault();
+        }
+    },
+};
+
+/**
+ * @internal
+ */
+export const TextFeatures: Record<
+    keyof TextFeatureSettings,
+    BuildInEditFeature<PluginKeyboardEvent>
+> = {
+    indentWhenTabText: IndentWhenTabText,
+};
+
+function shouldIndent(editor: IEditor, range: Range): boolean {
+    let result: boolean = false;
+
+    const startPosition: NodePosition = Position.getStart(range);
+    const endPosition: NodePosition = Position.getEnd(range);
+    const firstBlock = editor.getBlockElementAtNode(startPosition.node);
+    const lastBlock = editor.getBlockElementAtNode(endPosition.node);
+
+    if (!firstBlock.equals(lastBlock)) {
+        //If the selections has more than one block, we indent all the blocks in the selection
+        return true;
+    } else {
+        //We only indent a single block if all the block is selected.
+        const blockStart = new Position(firstBlock.getStartNode(), PositionType.Begin);
+        const blockEnd = new Position(firstBlock.getEndNode(), PositionType.End);
+
+        const rangeBefore = createRange(blockStart, Position.getStart(range));
+        const rangeAfter = createRange(Position.getEnd(range), blockEnd);
+
+        if (!result && isRangeEmpty(rangeBefore) && isRangeEmpty(rangeAfter)) {
+            result = true;
+        }
+
+        return result;
+    }
+}
+
+function isRangeEmpty(range: Range) {
+    return (
+        range.toString() == '' &&
+        queryElements(
+            range.commonAncestorContainer as ParentNode,
+            'img,table,ul,ol',
+            null,
+            QueryScope.InSelection,
+            range
+        ).length == 0
+    );
+}
+
+function insertTab(editor: IEditor, event: PluginKeyboardEvent) {
+    const span = editor.getDocument().createElement('span');
+    let searcher = editor.getContentSearcherOfCursor(event);
+    const charsBefore = searcher.getSubStringBefore(Number.MAX_SAFE_INTEGER);
+
+    const numberOfChars = TAB_SPACES - (charsBefore.length % TAB_SPACES);
+
+    let textContent = '';
+    for (let index = 0; index < numberOfChars; index++) {
+        textContent += '&ensp;';
+    }
+    editor.insertNode(span);
+    editor.insertContent(textContent, {
+        position: ContentPosition.Range,
+        range: createRange(span, PositionType.Begin),
+        updateCursor: false,
+    });
+    editor.select(createRange(span, PositionType.After));
+}
