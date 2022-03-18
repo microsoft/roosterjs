b6a9c578b7d74dd5752be4c7b780680eaf89c7a8
Jiuqing Song <jisong@microsoft.com>
2022-03-18 14:01:51 -0700

    Dark and undo step 2: Prepare for content snapshot (#840)
    * Dark and undo step 1
    
    * Dark and undo step 2

--- packages/roosterjs-editor-dom/test/selections/getHtmlWithSelectionPathTest.ts
+++ packages/roosterjs-editor-dom/test/selections/getHtmlWithSelectionPathTest.ts
@@ -22,30 +22,4 @@ describe('getHtmlWithSelectionPath', () => {
             'test1<div>text 2<span>test3</span>test 4</div>test 5<!--{"start":[1,0,2],"end":[2,3]}-->'
         );
     });
-
-    it('TABLE content', () => {
-        const div = document.createElement('div');
-        const range = document.createRange();
-        const table = document.createElement('table');
-
-        div.appendChild(document.createTextNode('test1'));
-        div.appendChild(table);
-        div.appendChild(document.createTextNode('test2'));
-
-        const tr = document.createElement('tr');
-        table.appendChild(tr);
-
-        const td = document.createElement('td');
-        tr.appendChild(td);
-
-        const text = document.createTextNode('test');
-        td.appendChild(text);
-
-        range.setStart(text, 2);
-        range.setEnd(text, 3);
-        const html = getHtmlWithSelectionPath(div, range);
-        expect(html).toBe(
-            'test1<table><tbody><tr><td>test</td></tr></tbody></table>test2<!--{"start":[1,0,0,0,0,2],"end":[1,0,0,0,0,3]}-->'
-        );
-    });
 });
