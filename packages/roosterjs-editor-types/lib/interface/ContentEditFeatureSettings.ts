d2d779ad24c953ddf377e089bde12f226fcbf461
Bryan Valverde U <bvalverde@microsoft.com>
2022-03-29 16:45:14 -0600

    Indent whole table when all cells are selected. (#865)
    * init
    
    * Add comments
    
    * fix comment

--- packages/roosterjs-editor-types/lib/interface/ContentEditFeatureSettings.ts
+++ packages/roosterjs-editor-types/lib/interface/ContentEditFeatureSettings.ts
@@ -199,6 +199,11 @@ export interface TableFeatureSettings {
      * @default true for Chrome and safari, false for other browsers since they already have correct behavior
      */
     upDownInTable: boolean;
+
+    /**
+     * IndentTableOnTab edit feature, provides the ability to indent the table if it is all cells are selected.
+     */
+    indentTableOnTab: boolean;
 }
 
 /**
