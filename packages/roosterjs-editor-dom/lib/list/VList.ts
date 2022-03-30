fb672c5144d0b77aa2d53f5c9546319512dbe0b6
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2022-03-30 15:19:31 -0600

    remove debugger

--- packages/roosterjs-editor-dom/lib/list/VList.ts
+++ packages/roosterjs-editor-dom/lib/list/VList.ts
@@ -330,7 +330,6 @@ export default class VList {
      */
     getListItemIndex(input: Node) {
         if (this.items) {
-            debugger;
             let listIndex = (this.getStart() || 1) - 1;
 
             for (let index = 0; index < this.items.length; index++) {
