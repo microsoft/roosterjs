84d875c429e5c800acf267097d7b72b8e189b34d
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2022-04-06 09:46:02 -0600

    remove debugger

--- packages/roosterjs-editor-core/lib/corePlugins/CopyPastePlugin.ts
+++ packages/roosterjs-editor-core/lib/corePlugins/CopyPastePlugin.ts
@@ -78,7 +78,6 @@ export default class CopyPastePlugin implements PluginWithState<CopyPastePluginS
 
     private onCutCopy(event: Event, isCut: boolean) {
         const selection = this.editor.getSelectionRangeEx();
-        debugger;
         if (selection && !selection.areAllCollapsed) {
             const html = this.editor.getContent(GetContentMode.RawHTMLWithSelection);
             const tempDiv = this.getTempDiv(true /*forceInLightMode*/);
