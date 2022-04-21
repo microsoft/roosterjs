b7e5102fd5e214dcb20202de3d16f6dfa9561459
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2022-04-21 16:40:28 -0600

    Remove not used code.

--- packages/roosterjs-editor-plugins/test/TestHelper.ts
+++ packages/roosterjs-editor-plugins/test/TestHelper.ts
@@ -1,6 +1,5 @@
 import { Editor } from 'roosterjs-editor-core';
-import { EditorOptions, EditorPlugin, SanitizeHtmlOptions } from 'roosterjs-editor-types';
-import { HtmlSanitizer } from 'roosterjs-editor-dom';
+import { EditorOptions, EditorPlugin } from 'roosterjs-editor-types';
 export * from 'roosterjs-editor-dom/test/DomTestHelper';
 
 export function initEditor(id: string, plugins?: EditorPlugin[]) {
@@ -20,11 +19,4 @@ export function initEditor(id: string, plugins?: EditorPlugin[]) {
     let editor = new Editor(node as HTMLDivElement, options);
 
     return editor;
-}
-
-export function callSanitizer(fragment: DocumentFragment, sanitizingOption: SanitizeHtmlOptions) {
-    const sanitizer = new HtmlSanitizer(sanitizingOption);
-
-    sanitizer.convertGlobalCssToInlineCss(fragment);
-    sanitizer.sanitize(fragment);
 }
