66808ee9d2316791af2721d88923c65641f980ae
Haowen Chen <haowchen@microsoft.com>
2022-05-20 16:00:06 +0800

    Fix lint

--- packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/index.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/index.ts
@@ -3,3 +3,4 @@ export { default as canRegenerateImage } from './api/canRegenerateImage';
 export { default as resizeByPercentage } from './api/resizeByPercentage';
 export { default as isResizedTo } from './api/isResizedTo';
 export { default as resetImage } from './api/resetImage';
+export { OnShowResizeHandle } from './imageEditors/Resizer';
