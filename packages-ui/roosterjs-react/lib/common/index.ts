ef703634bbfa4c7b78bda8551ced6ef133ae16f5
Jiuqing Song <jisong@microsoft.com>
2022-06-13 09:46:26 -0700

    improve

--- packages-ui/roosterjs-react/lib/common/index.ts
+++ packages-ui/roosterjs-react/lib/common/index.ts
@@ -5,4 +5,5 @@ export {
 } from './type/LocalizedStrings';
 export { default as UIUtilities } from './type/UIUtilities';
 export { default as ReactEditorPlugin } from './type/ReactEditorPlugin';
+export { default as createUIUtilities } from './utils/createUIUtilities';
 export { default as getLocalizedString } from './utils/getLocalizedString';
