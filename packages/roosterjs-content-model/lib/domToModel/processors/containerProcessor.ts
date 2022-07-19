307c287f46678924562947fdd195296b8c4a7efb
Jiuqing Song <jisong@microsoft.com>
2022-07-19 16:15:24 -0700

    Content Model Table Step 1: Add initial support to table (#1105)
    * Content Model step 0
    
    * Content Model step 1
    
    * change versoin
    
    * improve
    
    * Content Model Step 2
    
    * bug fix
    
    * Content Model Step 3
    
    * Add test case
    
    * add test case
    
    * Content Model Step 2.1: Auto generat compatible enum
    
    * fix build
    
    * Do not add ContentModel to rooster.d.ts
    
    * delete unused file
    
    * add comment
    
    * add test
    
    * Add initial support to Table
    
    * fix build
    
    * add test
    
    * Remove unnecessary code
    
    * fix build

--- packages/roosterjs-content-model/lib/domToModel/processors/containerProcessor.ts
+++ packages/roosterjs-content-model/lib/domToModel/processors/containerProcessor.ts
@@ -1,6 +1,7 @@
 import { ContentModelBlockGroup } from '../../publicTypes/block/group/ContentModelBlockGroup';
 import { generalBlockProcessor } from './generalBlockProcessor';
 import { generalSegmentProcessor } from './generalSegmentProcessor';
+import { getProcessor } from './getProcessor';
 import { isBlockElement } from 'roosterjs-editor-dom';
 import { isNodeOfType } from '../../domUtils/isNodeOfType';
 import { NodeType } from 'roosterjs-editor-types';
@@ -12,9 +13,9 @@ import { textProcessor } from './textProcessor';
 export function containerProcessor(group: ContentModelBlockGroup, parent: ParentNode) {
     for (let child = parent.firstChild; child; child = child.nextSibling) {
         if (isNodeOfType(child, NodeType.Element)) {
-            const processor = isBlockElement(child)
-                ? generalBlockProcessor
-                : generalSegmentProcessor;
+            const processor =
+                getProcessor(child.tagName) ||
+                (isBlockElement(child) ? generalBlockProcessor : generalSegmentProcessor);
 
             processor(group, child);
         } else if (isNodeOfType(child, NodeType.Text)) {
