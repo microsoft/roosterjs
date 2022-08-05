55d202d39ab823de10882c320a882a7797ddd571
JiuqingSong <jisong@microsoft.com>
2022-08-05 16:51:43 -0700

    fix test

--- packages/roosterjs-content-model/lib/modelApi/table/normalizeTable.ts
+++ packages/roosterjs-content-model/lib/modelApi/table/normalizeTable.ts
@@ -15,75 +15,77 @@ export function normalizeTable(table: ContentModelTable) {
 
     // Move blocks from spanned cell to its main cell,
     // and remove rows/columns that all cells in it are spanned
-    for (let colIndex = table.cells[0].length - 1; colIndex >= 0; colIndex--) {
-        let allSpannedLeft = true;
+    if (table.cells.length > 0) {
+        for (let colIndex = table.cells[0].length - 1; colIndex >= 0; colIndex--) {
+            let allSpannedLeft = true;
 
-        for (let rowIndex = 0; rowIndex < table.cells.length; rowIndex++) {
-            const cell = table.cells[rowIndex][colIndex];
+            for (let rowIndex = 0; rowIndex < table.cells.length; rowIndex++) {
+                const cell = table.cells[rowIndex][colIndex];
 
-            if (cell?.spanLeft) {
-                tryMoveBlocks(table.cells[rowIndex][colIndex - 1], cell, true /*inSameRow*/);
-            } else {
-                allSpannedLeft = false;
+                if (cell?.spanLeft) {
+                    tryMoveBlocks(table.cells[rowIndex][colIndex - 1], cell, true /*inSameRow*/);
+                } else {
+                    allSpannedLeft = false;
+                }
+            }
+
+            if (allSpannedLeft) {
+                table.cells.forEach(row => row.splice(colIndex, 1));
             }
         }
 
-        if (allSpannedLeft) {
-            table.cells.forEach(row => row.splice(colIndex, 1));
+        for (let rowIndex = table.cells.length - 1; rowIndex >= 0; rowIndex--) {
+            let allSpannedAbove = true;
+            const row = table.cells[rowIndex];
+
+            for (let colIndex = 0; colIndex < row.length; colIndex++) {
+                const cell = row[colIndex];
+
+                if (cell?.spanAbove) {
+                    tryMoveBlocks(table.cells[rowIndex - 1]?.[colIndex], cell, false /*inSameRow*/);
+                } else {
+                    allSpannedAbove = false;
+                }
+            }
+
+            if (allSpannedAbove) {
+                table.cells.splice(rowIndex, 1);
+            }
         }
+
+        for (let i = table.cells.length - 1; i >= 0; i--) {
+            if (table.cells[i].every(cell => cell.spanAbove)) {
+                table.cells.splice(i, 1);
+            }
+        }
+
+        // Make sure all first cells are not spanned
+        // Make sure all inner cells are not header
+        // Make sure all cells have content and width
+        table.cells.forEach((row, rowIndex) =>
+            row.forEach((cell, colIndex) => {
+                if (cell.blocks.length == 0) {
+                    addSegment(cell, createBr());
+                }
+
+                if (typeof cell.format.width === 'undefined') {
+                    cell.format.width = getTableCellWidth(row.length);
+                }
+
+                if (rowIndex == 0) {
+                    cell.spanAbove = false;
+                } else {
+                    cell.isHeader = false;
+                }
+
+                if (colIndex == 0) {
+                    cell.spanLeft = false;
+                }
+
+                cell.format.useBorderBox = true;
+            })
+        );
     }
-
-    for (let rowIndex = table.cells.length - 1; rowIndex >= 0; rowIndex--) {
-        let allSpannedAbove = true;
-        const row = table.cells[rowIndex];
-
-        for (let colIndex = 0; colIndex < row.length; colIndex++) {
-            const cell = row[colIndex];
-
-            if (cell?.spanAbove) {
-                tryMoveBlocks(table.cells[rowIndex - 1]?.[colIndex], cell, false /*inSameRow*/);
-            } else {
-                allSpannedAbove = false;
-            }
-        }
-
-        if (allSpannedAbove) {
-            table.cells.splice(rowIndex, 1);
-        }
-    }
-
-    for (let i = table.cells.length - 1; i >= 0; i--) {
-        if (table.cells[i].every(cell => cell.spanAbove)) {
-            table.cells.splice(i, 1);
-        }
-    }
-
-    // Make sure all first cells are not spanned
-    // Make sure all inner cells are not header
-    // Make sure all cells have content and width
-    table.cells.forEach((row, rowIndex) =>
-        row.forEach((cell, colIndex) => {
-            if (cell.blocks.length == 0) {
-                addSegment(cell, createBr());
-            }
-
-            if (typeof cell.format.width === 'undefined') {
-                cell.format.width = getTableCellWidth(row.length);
-            }
-
-            if (rowIndex == 0) {
-                cell.spanAbove = false;
-            } else {
-                cell.isHeader = false;
-            }
-
-            if (colIndex == 0) {
-                cell.spanLeft = false;
-            }
-
-            cell.format.useBorderBox = true;
-        })
-    );
 }
 
 function getTableCellWidth(columns: number): number {
