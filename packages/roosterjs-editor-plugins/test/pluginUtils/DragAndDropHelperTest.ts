b262038dcfa9fa45f87afaff9932c8fd26db31ea
AndresCT-98 <acampostams@microsoft.com>
2022-08-11 17:53:18 -0500

    Added simple tests

--- packages/roosterjs-editor-plugins/test/pluginUtils/DragAndDropHelperTest.ts
+++ packages/roosterjs-editor-plugins/test/pluginUtils/DragAndDropHelperTest.ts
@@ -14,17 +14,19 @@ interface DragAndDropInitValue {
     originalRect: DOMRect;
 }
 
-describe('DragAndDropHelper', () => {
+describe('DragAndDropHelper |', () => {
     let editor: IEditor;
     let id = 'DragAndDropHelperId';
     let dndHelper: DragAndDropHelper<DragAndDropContext, DragAndDropInitValue>;
 
     beforeEach(() => {
+        let s = Browser;
+        spyOn(s, 'isMobileOrTablet').and.returnValue(true);
         let node = document.createElement('div');
         node.id = id;
         node.style.width = '50px';
         node.style.height = '50px';
-        node.style.backgroundColor = 'red';
+        node.style.backgroundColor = 'black';
         node.style.position = 'fixed';
 
         document.body.insertBefore(node, document.body.childNodes[0]);
@@ -35,12 +37,16 @@ describe('DragAndDropHelper', () => {
             () => {},
             {
                 onDragEnd(context, event, initValue) {
+                    context.node.style.backgroundColor = 'red';
                     return true;
                 },
                 onDragStart(context, event) {
+                    console.log(event.type);
+                    context.node.style.backgroundColor = 'green';
                     return { originalRect: context.node.getBoundingClientRect() };
                 },
                 onDragging(context, event, initValue, deltaX, deltaY) {
+                    context.node.style.backgroundColor = 'yellow';
                     context.node.style.left = event.pageX + 'px';
                     context.node.style.top = event.pageY + 'px';
                     return true;
@@ -48,21 +54,52 @@ describe('DragAndDropHelper', () => {
             },
             1
         );
+
+        console.log(dndHelper);
     });
 
     afterEach(() => {});
 
     function runTest() {
-        debugger;
+        //debugger;
     }
 
-    it('should replace', () => {
+    it('mouse movement', () => {
+        const target = document.getElementById(id);
         //1 Mouse DOwn Div
-
+        simulateMouseEvent('mousedown', target);
+        debugger;
+        expect(target?.style.backgroundColor).toBe('green');
         //2 Mouse Move Move
-
+        let targetEnd = target;
+        targetEnd.style.top = 50 + 'px';
+        simulateMouseEvent('mousemove', targetEnd);
+        debugger;
+        expect(target?.style.backgroundColor).toBe('yellow');
+        simulateMouseEvent('mouseup', targetEnd);
+        debugger;
         //3 Mouse Up Event
-        runTest();
+        expect(target?.style.top).toBe('50px');
+        expect(target?.style.backgroundColor).toBe('red');
+    });
+
+    it('touch movement', () => {
+        const target = document.getElementById(id);
+        //1 Mouse DOwn Div
+        simulateTouchEvent('touchstart', target);
+        debugger;
+        expect(target?.style.backgroundColor).toBe('green');
+        //2 Mouse Move Move
+        let targetEnd = target;
+        targetEnd.style.left = 50 + 'px';
+        simulateTouchEvent('touchmove', targetEnd);
+        debugger;
+        expect(target?.style.backgroundColor).toBe('yellow');
+        simulateTouchEvent('touchend', targetEnd);
+        debugger;
+        //3 Mouse Up Event
+        expect(target?.style.left).toBe('50px');
+        expect(target?.style.backgroundColor).toBe('red');
     });
 });
 
@@ -75,6 +112,20 @@ function simulateMouseEvent(type: string, target: HTMLElement, shiftKey: boolean
         clientX: rect.left,
         clientY: rect.top,
         shiftKey,
+    });
+    target.dispatchEvent(event);
+}
+
+function simulateTouchEvent(type: string, target: HTMLElement) {
+    const rect = target.getBoundingClientRect();
+    let touchList: Touch[] = [
+        new Touch({ identifier: 0, target: target, clientX: rect.left, clientY: rect.top }),
+    ];
+    var event = new TouchEvent(type, {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+        targetTouches: touchList,
     });
     target.dispatchEvent(event);
 }
