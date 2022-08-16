microsoft/roosterjs

commit 647148550287fd54987b4378e2354a1f6e68c065

fix: imageEditPlugin cause content bounce and original image size change (#1172)
* fix:ImageEdit Plugin minWidth or minHeight work failed

* fix: alter imageEdit resizer test case data

* fix: imageEditPlugin cause content bounce and original image size change

* fix: optimize the method of get verticalAlign style

Co-authored-by: Jiuqing Song <jisong@microsoft.com>

packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/editInfoUtils/applyChange.ts
@@ -87,11 +87,5 @@ function setImageSize(
         image.height = targetHeight;
         image.style.width = targetWidth + 'px';
         image.style.height = targetHeight + 'px';
-    } else {
-        image.style.maxWidth = '100%';
-        image.style.height = 'initial';
-        image.style.width = 'initial';
-        image.removeAttribute('height');
-        image.removeAttribute('width');
     }
 }

packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/ImageEdit.ts
@@ -364,7 +364,11 @@ export default class ImageEdit implements EditorPlugin {
 
         wrapper.style.position = 'relative';
         wrapper.style.maxWidth = '100%';
-        wrapper.style.verticalAlign = 'bottom';
+        // keep the same vertical align
+        const originalVerticalAlign = this.image.ownerDocument.defaultView.getComputedStyle(this.image).getPropertyValue('vertical-align');
+        if (originalVerticalAlign) {
+          wrapper.style.verticalAlign = originalVerticalAlign;
+        }
         wrapper.style.display = Browser.isSafari ? 'inline-block' : 'inline-flex';
 
         // Cache current src so that we can compare it after edit see if src is changed

commit d37172bcf22056add9273a4340e72c79842c5995

8.29.2 Fix a regression from #1172

packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/editInfoUtils/applyChange.ts
@@ -68,20 +68,8 @@ export default function applyChange(
     // Write back the change to image, and set its new size
     const { targetWidth, targetHeight } = getGeneratedImageSize(editInfo);
     image.src = newSrc;
-    setImageSize(image, wasResized, targetWidth, targetHeight);
-}
 
-/**
- * @param img The current image.
- * @param wasResized the current resize state of the image
- */
-function setImageSize(
-    image: HTMLImageElement,
-    wasResized: boolean,
-    targetWidth: number,
-    targetHeight: number
-) {
-    if (wasResized) {
+    if (wasResized || state == ImageEditInfoState.FullyChanged) {
         image.style.maxWidth = 'initial';
         image.width = targetWidth;
         image.height = targetHeight;