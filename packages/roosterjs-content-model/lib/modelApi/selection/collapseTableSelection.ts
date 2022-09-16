eb801def67dae7f33e6cb5f14b0a8efcf543f0a8
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2022-09-16 10:01:49 -0600

    Fix

--- packages/roosterjs-content-model/lib/modelApi/selection/collapseTableSelection.ts
+++ packages/roosterjs-content-model/lib/modelApi/selection/collapseTableSelection.ts
@@ -10,14 +10,9 @@ export function collapseTableSelection(
     cells: ContentModelTableCell[][],
     selection: TableSelectionCoordinates
 ) {
-    const { firstCol, firstRow, lastCol, lastRow } = selection;
-
-    for (let row = 0; row < cells.length; row++) {
-        for (let col = 0; col < cells[row].length; col++) {
-            if (row >= firstRow && row <= lastRow && col >= firstCol && col <= lastCol) {
-                addSegment(cells[row][col], createSelectionMarker());
-                return;
-            }
-        }
+    const { firstCol, firstRow } = selection;
+    const cell = cells[firstRow]?.[firstCol];
+    if (cell) {
+        addSegment(cell, createSelectionMarker());
     }
 }
