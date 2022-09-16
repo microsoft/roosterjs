770d5a021a8cb710346f37de2899978c4a18f52d
Júlia Roldi <juliaroldi@microsoft.com>
2022-09-16 17:55:59 -0300

    fix maintain list chain

--- packages/roosterjs-editor-plugins/lib/plugins/ContentEdit/utils/getAutoNumberingListStyle.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/ContentEdit/utils/getAutoNumberingListStyle.ts
@@ -21,14 +21,6 @@ const characters: Record<string, number> = {
     ')': Character.Parenthesis,
 };
 
-const numberingTriggers: Record<string, number> = {
-    '1': NumberingTypes.Decimal,
-    i: NumberingTypes.LowerRoman,
-    I: NumberingTypes.UpperRoman,
-    a: NumberingTypes.LowerAlpha,
-    A: NumberingTypes.UpperAlpha,
-};
-
 const identifyNumberingType = (text: string) => {
     if (!isNaN(parseInt(text))) {
         return NumberingTypes.Decimal;
@@ -92,18 +84,15 @@ const DecimalsTypes: Record<number, number> = {
 
 const identifyNumberingListType = (
     numbering: string,
-    isDoubleParenthesis: boolean,
-    startNumber?: number
+    isDoubleParenthesis: boolean
 ): NumberingListType | null => {
     const separatorCharacter = isDoubleParenthesis
         ? Character.DoubleParenthesis
         : characters[numbering[1]];
     // if separator is not valid, no need to check if the number is valid.
     if (separatorCharacter) {
-        const number = numbering.length === 3 ? numbering[1] : numbering[0];
-        const numberingType = startNumber
-            ? identifyNumberingType(number)
-            : numberingTriggers[number];
+        const number = numbering[numbering.length - 2];
+        const numberingType = identifyNumberingType(number);
         return numberingType ? numberingListTypes[numberingType](separatorCharacter) : null;
     }
     return null;
@@ -117,15 +106,22 @@ const identifyNumberingListType = (
  */
 export default function getAutoNumberingListStyle(
     textBeforeCursor: string,
-    startNumber?: number
+    isTheFirstItem?: boolean
 ): NumberingListType {
     const trigger = textBeforeCursor.trim();
+    //Only the staring items ['1', 'a', 'A', 'I', 'i'] must trigger a new list. All the other triggers is used to keep the list chain.
+    const listIndex = trigger[trigger.length - 2];
+    const numberingTriggers = ['1', 'a', 'A', 'I', 'i'];
+    if (isTheFirstItem && numberingTriggers.indexOf(listIndex) < 0) {
+        return null;
+    }
+
     // the marker must be a combination of 2 or 3 characters, so if the length is less than 2, no need to check
     // If the marker length is 3, the marker style is double parenthesis such as (1), (A).
     const isDoubleParenthesis = trigger.length === 3 && trigger[0] === '(' && trigger[2] === ')';
     const numberingType =
         trigger.length === 2 || isDoubleParenthesis
-            ? identifyNumberingListType(trigger, isDoubleParenthesis, startNumber)
+            ? identifyNumberingListType(trigger, isDoubleParenthesis)
             : null;
     return numberingType;
 }
