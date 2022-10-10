56e838eb05e0e49d1e1e807beddd46678cc38810
Jiuqing Song <jisong@microsoft.com>
2022-10-10 13:23:33 -0700

    Content Model Entity step 2: Add implementation code (#1314)
    * Content Model Entity step 1
    
    * Content Model Entity step 2

---
+++ packages/roosterjs-content-model/test/domToModel/processors/entityProcessorTest.ts
@@ -0,0 +1,82 @@
+import { commitEntity } from 'roosterjs-editor-dom';
+import { createContentModelDocument } from '../../../lib/modelApi/creators/createContentModelDocument';
+import { createDomToModelContext } from '../../../lib/domToModel/context/createDomToModelContext';
+import { DomToModelContext } from '../../../lib/publicTypes/context/DomToModelContext';
+import { entityProcessor } from '../../../lib/domToModel/processors/entityProcessor';
+
+describe('entityProcessor', () => {
+    let context: DomToModelContext;
+
+    beforeEach(() => {
+        context = createDomToModelContext();
+    });
+
+    it('Not an entity', () => {
+        const group = createContentModelDocument(document);
+        const div = document.createElement('div');
+
+        entityProcessor(group, div, context);
+
+        expect(group).toEqual({
+            blockGroupType: 'Document',
+            document: document,
+            blocks: [],
+        });
+    });
+
+    it('Block element entity', () => {
+        const group = createContentModelDocument(document);
+        const div = document.createElement('div');
+
+        commitEntity(div, 'entity', true, 'entity_1');
+
+        entityProcessor(group, div, context);
+
+        expect(group).toEqual({
+            blockGroupType: 'Document',
+            document: document,
+            blocks: [
+                {
+                    blockType: 'Entity',
+                    segmentType: 'Entity',
+                    format: {},
+                    id: 'entity_1',
+                    type: 'entity',
+                    isReadonly: true,
+                    wrapper: div,
+                },
+            ],
+        });
+    });
+
+    it('Inline element entity', () => {
+        const group = createContentModelDocument(document);
+        const span = document.createElement('span');
+
+        commitEntity(span, 'entity', true, 'entity_1');
+
+        entityProcessor(group, span, context);
+
+        expect(group).toEqual({
+            blockGroupType: 'Document',
+            document: document,
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    isImplicit: true,
+                    segments: [
+                        {
+                            blockType: 'Entity',
+                            segmentType: 'Entity',
+                            format: {},
+                            id: 'entity_1',
+                            type: 'entity',
+                            isReadonly: true,
+                            wrapper: span,
+                        },
+                    ],
+                },
+            ],
+        });
+    });
+});
