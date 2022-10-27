7bcf3db1a723538ecac2778542a827ebee34e63d
Júlia Roldi <juliaroldi@microsoft.com>
2022-10-27 16:41:50 -0300

    dispose properties

--- packages/roosterjs-editor-plugins/lib/plugins/Picker/PickerPlugin.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/Picker/PickerPlugin.ts
@@ -124,6 +124,13 @@ export default class PickerPlugin<T extends PickerDataProvider = PickerDataProvi
      */
     public dispose() {
         this.editor = null;
+        this.isSuggesting = null;
+        this.blockSuggestions = null;
+        this.eventHandledOnKeyDown = null;
+        this.lastKnownRange = null;
+        this.isPendingInputEventHandling = null;
+        this.currentInputLength = null;
+        this.newInputLength = null;
         this.dataProvider.onDispose();
     }
 
--- packages/roosterjs-editor-plugins/test/Picker/pickerPluginTest.ts
+++ packages/roosterjs-editor-plugins/test/Picker/pickerPluginTest.ts
@@ -1,89 +1,65 @@
 import * as TestHelper from '../TestHelper';
-import { IEditor, PickerDataProvider, PluginEvent, PluginEventType } from 'roosterjs-editor-types';
+import { ChangeSource, IEditor, PluginEvent, PluginEventType } from 'roosterjs-editor-types';
 import { PickerPlugin } from '../../lib/Picker';
 
 const BACKSPACE_CHAR_CODE = 'Backspace';
+const ESC_CHAR_CODE = 'Escape';
 
-type InsertNodeCallback = (nodeToInsert: HTMLElement) => void;
-export default class SampleColorPickerPluginDataProvider implements PickerDataProvider {
-    /**
-     * For inserting a node into hte
-     */
-    private mountPoint: HTMLElement | null;
-    private editor: IEditor;
-
-    // Function called when the plugin is initialized to register two callbacks with the data provider and a reference to the Editor.
-    // The first is called in order to "commit" a new element to the editor body that isn't handled automatically by the editor plugin.
-    // The second sets the isSuggesting value for situations where the UX needs to manipulate the suggesting state that's otherwise plugin managed.
-    onInitalize(
-        insertNodeCallback: InsertNodeCallback,
+const dataProvider = {
+    onInitalize: (
+        insertNodeCallback: (nodeToInsert: HTMLElement) => void,
         setIsSuggestingCallback: (isSuggesting: boolean) => void,
-        editor: IEditor
-    ): void {
-        this.editor = editor;
-
-        const doc = this.editor.getDocument();
-        this.mountPoint = doc.createElement('section');
-        if (this.mountPoint) {
-            this.mountPoint.id = pickerOptions.elementIdPrefix;
-        }
-        doc.body.appendChild(this.mountPoint);
-    }
-
-    // Function called when the plugin is disposed for the data provider to do any cleanup.
-    onDispose(): void {
-        if (this.mountPoint) {
-            const parent = this.mountPoint.parentElement;
-            parent?.removeChild(this.mountPoint);
-        }
-        this.mountPoint = null;
-    }
-
-    // Function called when the picker changes suggesting state (e.g. when your dialog opens)
-    onIsSuggestingChanged(isSuggesting: boolean) {
-        if (isSuggesting) {
-            const doc = this.editor.getDocument();
-            this.mountPoint = doc.createElement('section');
-            if (this.mountPoint) {
-                this.mountPoint.id = pickerOptions.elementIdPrefix;
-            }
-            doc.body.appendChild(this.mountPoint);
-        } else {
-            if (this.mountPoint) {
-                const parent = this.mountPoint.parentElement;
-                parent?.removeChild(this.mountPoint);
-            }
-        }
-    }
-
-    // Function called when the query string (text after the trigger symbol) is updated.
-    queryStringUpdated(queryString: string) {
+        editor?: IEditor
+    ) => {},
+    onDispose: () => {
         return;
-    }
+    },
 
-    // Function that is called when a delete command is issued.
-    // Returns the intended replacement node (if partial delete) or null (if full delete)
-    onRemove(nodeRemoved: Node, isBackwards: boolean): Node | null {
-        return null;
-    }
-}
+    onIsSuggestingChanged: (isSuggesting: boolean) => {
+        return;
+    },
+
+    queryStringUpdated: (queryString: string, isExactMatch: boolean) => {
+        return;
+    },
+
+    onRemove: (nodeRemoved: Node, isBackwards: boolean) => {
+        return document.createTextNode('');
+    },
+
+    onScroll: (scrollContainer: HTMLElement) => {
+        return;
+    },
+
+    onContentChanged: (elementIds: string[]) => {
+        return;
+    },
+};
 
 const pickerOptions = {
-    elementIdPrefix: 'samplePicker-',
-    changeSource: 'SAMPLE_COLOR_PICKER',
-    triggerCharacter: ':',
-    isHorizontal: true,
+    elementIdPrefix: 'picker_test',
+    changeSource: ChangeSource.SetContent,
+    triggerCharacter: ')',
 };
+
 describe('PickerPlugin |', () => {
     let editor: IEditor;
-    const dataProvider = new SampleColorPickerPluginDataProvider();
     const TEST_ID = 'pickerTest';
     const plugin = new PickerPlugin(dataProvider, pickerOptions);
-    let spyOnSetSuggestion: any;
+    let spyOnQueryStringUpdated: any;
+    let spyOnIsSuggestingChanged: any;
+    let spyOnScroll: any;
+    let spyOnContentChange: any;
+    let spyOnInitialize: any;
+    let spyOnDispose: any;
     beforeEach(() => {
         editor = TestHelper.initEditor(TEST_ID, [plugin]);
-        plugin.initialize(editor);
-        spyOnSetSuggestion = spyOn(dataProvider, 'onIsSuggestingChanged');
+        spyOnQueryStringUpdated = spyOn(plugin.dataProvider, 'queryStringUpdated');
+        spyOnIsSuggestingChanged = spyOn(plugin.dataProvider, 'onIsSuggestingChanged');
+        spyOnScroll = spyOn(plugin.dataProvider, 'onScroll');
+        spyOnContentChange = spyOn(plugin.dataProvider, 'onContentChanged');
+        spyOnInitialize = spyOn(plugin.dataProvider, 'onInitalize');
+        spyOnDispose = spyOn(plugin.dataProvider, 'onDispose');
     });
 
     afterEach(() => {
@@ -121,38 +97,88 @@ describe('PickerPlugin |', () => {
         };
     };
 
+    const scroll = (): PluginEvent => {
+        return {
+            eventType: PluginEventType.Scroll,
+            rawEvent: <Event>{},
+            scrollContainer: undefined,
+        };
+    };
+
+    const contentChanged = (): PluginEvent => {
+        return {
+            eventType: PluginEventType.ContentChanged,
+            source: ChangeSource.SetContent,
+        };
+    };
+
     function runTestKeyDown(content: string, keyTyped: string) {
         editor.setContent(content);
-        plugin.onPluginEvent(keyUp(':'));
+        plugin.onPluginEvent(keyUp(')'));
         plugin.onPluginEvent(keyDown(keyTyped));
-        expect(spyOnSetSuggestion).toHaveBeenCalled();
-        expect(spyOnSetSuggestion).toHaveBeenCalledWith(false);
+        expect(spyOnIsSuggestingChanged).toHaveBeenCalled();
     }
 
     function runTestMouseUp(content: string) {
         editor.setContent(content);
-        plugin.onPluginEvent(keyUp(':'));
+        plugin.onPluginEvent(keyUp(')'));
         plugin.onPluginEvent(mouseUp());
-        expect(spyOnSetSuggestion).toHaveBeenCalled();
-        expect(spyOnSetSuggestion).toHaveBeenCalledWith(false);
+        expect(spyOnIsSuggestingChanged).toHaveBeenCalled();
+        expect(spyOnIsSuggestingChanged).toHaveBeenCalledWith(false);
     }
 
-    function runTestKeyUp(content: string, keyTyped: string) {
+    function runTestScroll() {
+        const scrollEvent = scroll();
+        plugin.onPluginEvent(scrollEvent);
+        expect(spyOnScroll).toHaveBeenCalled();
+        expect(spyOnScroll).toHaveBeenCalledWith(spyOnScroll.scrollContainer);
+    }
+
+    function runTestKeyUp(content: string, keyTyped: string, shouldSuggest: boolean) {
         editor.setContent(content);
         plugin.onPluginEvent(keyUp(keyTyped));
-        const mountPoint = editor.getDocument().getElementById(pickerOptions.elementIdPrefix);
-        expect(mountPoint).toBeTruthy();
+        expect(spyOnQueryStringUpdated).toHaveBeenCalled();
+        expect(spyOnIsSuggestingChanged).toHaveBeenCalled();
+        expect(spyOnIsSuggestingChanged).toHaveBeenCalledWith(shouldSuggest);
     }
 
-    it('key down | backspace ', () => {
-        runTestKeyDown('<div>: </div><!--{"start":[0,0,2],"end":[0,0,2]}-->', BACKSPACE_CHAR_CODE);
+    function runTestContentChange() {
+        plugin.onPluginEvent(contentChanged());
+        expect(spyOnContentChange).toHaveBeenCalled();
+        expect(spyOnContentChange).toHaveBeenCalledWith([]);
+    }
+
+    it('should show picker', () => {
+        runTestKeyUp('<div>) </div><!--{"start":[0,0,2],"end":[0,0,2]}-->', ')', true);
     });
 
-    it('key up ', () => {
-        runTestKeyUp('<div>:</div><!--{"start":[0,0,2],"end":[0,0,2]}-->', ':');
+    it('should hide picker | ESC', () => {
+        runTestKeyDown('<div>)</div><!--{"start":[0,0,1],"end":[0,0,1]}-->', ESC_CHAR_CODE);
     });
 
-    it('mouse up', () => {
-        runTestMouseUp('<div>:</div><!--{"start":[0,0,2],"end":[0,0,2]}-->');
+    it('should hide picker | backspace', () => {
+        runTestKeyDown('<div>)</div><!--{"start":[0,0,1],"end":[0,0,1]}-->', BACKSPACE_CHAR_CODE);
+    });
+
+    it('should hide picker | mouseEvent', () => {
+        runTestMouseUp('<div>)</div><!--{"start":[0,0,1],"end":[0,0,1]}-->');
+    });
+
+    it('should execute scroll function', () => {
+        runTestScroll();
+    });
+
+    it('should execute onContentChange function', () => {
+        runTestContentChange();
+    });
+
+    it('should execute initialize function', () => {
+        plugin.initialize(editor);
+        expect(spyOnInitialize).toHaveBeenCalled();
+    });
+
+    it('should execute dispose function', () => {
+        plugin.dispose();
+        expect(spyOnDispose).toHaveBeenCalled();
     });
 });
