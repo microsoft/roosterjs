28515cbd93ac7bb2a320ef16dc1a27b717c1a4fa
Jiuqing Song <jisong@microsoft.com>
2022-11-21 10:56:59 -0800

    Content Model format API step 3: Add paragraph format API (#1420)
    * Content Model format API step 3
    
    * fix build
    
    * add test
    
    * fix build step 1
    
    * fix build step 2
    
    * fix test

---
+++ packages/roosterjs-content-model/test/publicApi/block/setHeaderLevelTest.ts
@@ -0,0 +1,402 @@
+import setHeaderLevel from '../../../lib/publicApi/block/setHeaderLevel';
+import { ContentModelDocument } from '../../../lib/publicTypes/group/ContentModelDocument';
+import { paragraphTestCommon } from './paragraphTestCommon';
+
+describe('setHeaderLevel to 1', () => {
+    function runTest(
+        model: ContentModelDocument,
+        result: ContentModelDocument,
+        calledTimes: number
+    ) {
+        paragraphTestCommon(
+            'setHeaderLevel',
+            editor => setHeaderLevel(editor, 1),
+            model,
+            result,
+            calledTimes
+        );
+    }
+
+    it('empty content', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [],
+            },
+            0
+        );
+    });
+
+    it('no selection', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                            },
+                        ],
+                    },
+                ],
+            },
+            0
+        );
+    });
+
+    it('Collapsed selection', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                            },
+                            {
+                                segmentType: 'SelectionMarker',
+                                format: {},
+                                isSelected: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: { fontSize: '2em', fontWeight: 'bold' },
+                            },
+                            {
+                                segmentType: 'SelectionMarker',
+                                format: { fontSize: '2em', fontWeight: 'bold' },
+                                isSelected: true,
+                            },
+                        ],
+                        header: {
+                            headerLevel: 1,
+                            format: {
+                                fontSize: '2em',
+                                fontWeight: 'bold',
+                            },
+                        },
+                    },
+                ],
+            },
+            1
+        );
+    });
+
+    it('With selection', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                                isSelected: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {
+                                    fontSize: '2em',
+                                    fontWeight: 'bold',
+                                },
+                                isSelected: true,
+                            },
+                        ],
+                        header: {
+                            headerLevel: 1,
+                            format: {
+                                fontSize: '2em',
+                                fontWeight: 'bold',
+                            },
+                        },
+                    },
+                ],
+            },
+            1
+        );
+    });
+
+    it('With existing header', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {
+                                    fontSize: '1em',
+                                    fontWeight: 'normal',
+                                },
+                                isSelected: true,
+                            },
+                        ],
+                        header: {
+                            headerLevel: 3,
+                            format: {
+                                fontSize: '1em',
+                                fontWeight: 'normal',
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {
+                                    fontSize: '2em',
+                                    fontWeight: 'bold',
+                                },
+                                isSelected: true,
+                            },
+                        ],
+                        header: {
+                            headerLevel: 1,
+                            format: {
+                                fontSize: '2em',
+                                fontWeight: 'bold',
+                            },
+                        },
+                    },
+                ],
+            },
+            1
+        );
+    });
+});
+
+describe('setHeaderLevel to 0', () => {
+    function runTest(
+        model: ContentModelDocument,
+        result: ContentModelDocument,
+        calledTimes: number
+    ) {
+        paragraphTestCommon(
+            'setHeaderLevel',
+            editor => setHeaderLevel(editor, 0),
+            model,
+            result,
+            calledTimes
+        );
+    }
+
+    it('empty content', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [],
+            },
+            0
+        );
+    });
+
+    it('no selection', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                            },
+                        ],
+                    },
+                ],
+            },
+            0
+        );
+    });
+
+    it('Collapsed selection', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                            },
+                            {
+                                segmentType: 'SelectionMarker',
+                                format: {},
+                                isSelected: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                            },
+                            {
+                                segmentType: 'SelectionMarker',
+                                format: {},
+                                isSelected: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            1
+        );
+    });
+
+    it('With existing header', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {
+                                    fontSize: '1em',
+                                    fontWeight: 'normal',
+                                },
+                                isSelected: true,
+                            },
+                        ],
+                        header: {
+                            headerLevel: 3,
+                            format: {
+                                fontSize: '1em',
+                                fontWeight: 'normal',
+                            },
+                        },
+                    },
+                ],
+            },
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {},
+                        segments: [
+                            {
+                                segmentType: 'Text',
+                                text: 'test',
+                                format: {},
+                                isSelected: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            1
+        );
+    });
+});
