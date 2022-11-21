7841ab8d250701f3668cc348a37fdd12584fea7b
Jiuqing Song <jisong@microsoft.com>
2022-11-21 10:26:09 -0800

    Content Model format API step 2 (#1410)
    * Content Model format API 1
    
    * Content Model format API 2
    
    * improve test
    
    * improve test
    
    * improve test
    
    * Add API changeFontSize
    
    * Refactor
    
    * improve
    
    * fix test

---
+++ demo/scripts/controls/ribbonButtons/contentModel/underlineButton.ts
@@ -0,0 +1,20 @@
+import isContentModelEditor from '../../editor/isContentModelEditor';
+import { RibbonButton, UnderlineButtonStringKey } from 'roosterjs-react';
+import { toggleUnderline } from 'roosterjs-content-model';
+
+/**
+ * @internal
+ * "Underline" button on the format ribbon
+ */
+export const underlineButton: RibbonButton<UnderlineButtonStringKey> = {
+    key: 'buttonNameUnderline',
+    unlocalizedText: 'Underline',
+    iconName: 'Underline',
+    isChecked: formatState => formatState.isUnderline,
+    onClick: editor => {
+        if (isContentModelEditor(editor)) {
+            toggleUnderline(editor);
+        }
+        return true;
+    },
+};
