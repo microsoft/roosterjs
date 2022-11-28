d816f4d06f178f543801670ad9c2ecc58b5b0476
Júlia Roldi <juliaroldi@microsoft.com>
2022-11-28 18:39:53 -0300

    refactor

--- packages/roosterjs-editor-dom/lib/style/getBiggestZIndex.ts
+++ packages/roosterjs-editor-dom/lib/style/getBiggestZIndex.ts
@@ -8,7 +8,7 @@ import getTagOfNode from '../utils/getTagOfNode';
 export default function getBiggestZIndex(element: HTMLElement) {
     let zIndex = parseInt(element.style.zIndex);
     let child: HTMLElement | null = element;
-    while (child && getTagOfNode(child) === 'BODY') {
+    while (child && getTagOfNode(child) !== 'BODY') {
         const parent: HTMLElement | null = child?.parentElement;
         if (parent) {
             const parentZIndex = parseInt(parent.style?.zIndex);
