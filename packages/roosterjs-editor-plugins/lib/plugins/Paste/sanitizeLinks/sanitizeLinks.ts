5d4a80d682c658a688d2fd0298e36e114fc4abfa
Júlia Roldi <juliaroldi@microsoft.com>
2022-12-09 16:36:29 -0300

    clear local file paths and remove link

---
+++ packages/roosterjs-editor-plugins/lib/plugins/Paste/sanitizeLinks/sanitizeLinks.ts
@@ -0,0 +1,35 @@
+import { chainSanitizerCallback } from 'roosterjs-editor-dom';
+import { HtmlSanitizerOptions } from 'roosterjs-editor-types';
+
+const HTTP = 'http:';
+const HTTPS = 'https:';
+
+/**
+ * @internal
+ * Clear local paths and remove link
+ * @param sanitizingOption the sanitizingOption of BeforePasteEvent
+ * */
+export default function sanitizeLinks(sanitizingOption: Required<HtmlSanitizerOptions>) {
+    ['href'].forEach(property => {
+        chainSanitizerCallback(
+            sanitizingOption.attributeCallbacks,
+            property,
+            (value: string, element: HTMLElement) => validateLink(value, element)
+        );
+    });
+}
+
+function validateLink(link: string, htmlElement: HTMLElement) {
+    let url;
+    try {
+        url = new URL(link);
+    } catch {
+        url = undefined;
+    }
+
+    if (url && (url.protocol === HTTP || url.protocol === HTTPS)) {
+        return link;
+    }
+    htmlElement.removeAttribute('href');
+    return '';
+}
