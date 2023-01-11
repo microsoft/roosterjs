362b909d6b2b96be9b4792f001b731daba0e7e60
Shai Petel <shai@kwizcom.com>
2023-01-11 15:53:02 -0500

    fix image plugin z-index calc

--- packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/editInfoUtils/getLastZIndex.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/editInfoUtils/getLastZIndex.ts
@@ -10,9 +10,9 @@ export default function getLatestZIndex(editorDiv: HTMLElement) {
     let child: HTMLElement | null = editorDiv;
     let zIndex = 0;
     while (child && getTagOfNode(child) !== 'BODY') {
-        const childZIndex = child.style.zIndex || getComputedStyle(child).zIndex;
-        if (childZIndex) {
-            zIndex = parseInt(child.style.zIndex);
+        const childZIndex = parseInt(child.style.zIndex || getComputedStyle(child).zIndex, 10);
+        if (childZIndex && !isNaN(childZIndex)) {
+            zIndex = Math.max(zIndex, childZIndex);
         }
         child = child.parentElement;
     }
