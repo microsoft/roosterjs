2f48916b02398d7c644c0eecbdb50a09aa7927f1
jiuqingsong <jisong@microsoft.com>
2023-01-18 14:06:47 -0800

    Add test

--- packages/roosterjs-content-model/test/modelToDom/handlers/handleDividerTest.ts
+++ packages/roosterjs-content-model/test/modelToDom/handlers/handleDividerTest.ts
@@ -68,4 +68,23 @@ describe('handleDivider', () => {
 
         expect(parent.innerHTML).toBe('<hr style="display: inline-block; width: 98%;">');
     });
+
+    it('DIV with border and padding', () => {
+        const hr: ContentModelDivider = {
+            blockType: 'Divider',
+            tagName: 'hr',
+            format: {
+                borderTop: 'solid 1px black',
+                paddingBottom: '30px',
+            },
+        };
+
+        const parent = document.createElement('div');
+
+        handleDivider(document, parent, hr, context);
+
+        expect(parent.innerHTML).toBe(
+            '<hr style="padding-bottom: 30px; border-top: 1px solid black;">'
+        );
+    });
 });
