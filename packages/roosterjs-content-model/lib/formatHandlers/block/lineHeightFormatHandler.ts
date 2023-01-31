febdb183f7b71646fba70e1319bb594cc7320caa
Ian Elizondo <ianelicha@gmail.com>
2023-01-31 02:16:05 +0000

    Fetch line height from children

--- packages/roosterjs-content-model/lib/formatHandlers/block/lineHeightFormatHandler.ts
+++ packages/roosterjs-content-model/lib/formatHandlers/block/lineHeightFormatHandler.ts
@@ -6,7 +6,27 @@ import { LineHeightFormat } from '../../publicTypes/format/formatParts/LineHeigh
  */
 export const lineHeightFormatHandler: FormatHandler<LineHeightFormat> = {
     parse: (format, element, context, defaultStyle) => {
-        const lineHeight = element.style.lineHeight || defaultStyle.lineHeight;
+        let lineHeight = element.style.lineHeight || defaultStyle.lineHeight;
+        if (
+            !lineHeight &&
+            element.childNodes.length &&
+            element.childNodes.length === element.childElementCount
+        ) {
+            let childLineHeight = (<HTMLElement | null>(
+                element.children.item(0)
+            ))?.style?.getPropertyValue('line-height');
+
+            if (childLineHeight && element.childElementCount > 1) {
+                for (let i = 1, j = element.childElementCount; i < j; i++) {
+                    const childElement = <HTMLElement | null>element.children.item(i);
+                    if (childLineHeight !== childElement?.style.getPropertyValue('line-height')) {
+                        childLineHeight = undefined;
+                    }
+                }
+            }
+
+            lineHeight = childLineHeight || undefined;
+        }
 
         if (lineHeight && lineHeight != 'inherit') {
             format.lineHeight = lineHeight;
