28a087629fdb389374cb6f41ace41f2f2e981544
jiuqingsong <jisong@microsoft.com>
2023-01-31 23:00:00 -0800

    fix build

--- packages/roosterjs-content-model/lib/editor/isContentModelEditor.ts
+++ packages/roosterjs-content-model/lib/editor/isContentModelEditor.ts
@@ -1,4 +1,4 @@
-import { IContentModelEditor } from 'roosterjs-content-model';
+import { IContentModelEditor } from '../publicTypes/IContentModelEditor';
 import { IEditor } from 'roosterjs-editor-types';
 
 export default function isContentModelEditor(editor: IEditor): editor is IContentModelEditor {
