b112452ca7e689da872b0b246a22e1f1137f125d
Ian Elizondo <ianelicha@gmail.com>
2023-02-07 21:59:41 +0000

    Treat no margin as space removed

--- demo/scripts/controls/ribbonButtons/contentModel/spaceBeforeAfterButtons.ts
+++ demo/scripts/controls/ribbonButtons/contentModel/spaceBeforeAfterButtons.ts
@@ -12,7 +12,7 @@ export const spaceAfterButton: RibbonButton<typeof spaceAfterButtonKey> = {
     key: spaceAfterButtonKey,
     unlocalizedText: 'Remove space after',
     iconName: 'CaretDown8',
-    isChecked: formatState => parseInt(formatState.marginBottom) > 0,
+    isChecked: formatState => !formatState.marginBottom || parseInt(formatState.marginBottom) <= 0,
     onClick: editor => {
         if (isContentModelEditor(editor)) {
             toggleMargins(editor, {
