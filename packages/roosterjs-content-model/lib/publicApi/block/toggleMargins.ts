0b84490fb5d58bbcb30abb961d01c1bc2d47ef2c
Ian Elizondo <ianelicha@gmail.com>
2023-02-09 18:37:33 +0000

    Improve tag name selecting

--- packages/roosterjs-content-model/lib/publicApi/block/toggleMargins.ts
+++ packages/roosterjs-content-model/lib/publicApi/block/toggleMargins.ts
@@ -4,15 +4,20 @@ import { MarginFormat } from '../../publicTypes/format/formatParts/MarginFormat'
 import { IContentModelEditor } from '../../publicTypes/IContentModelEditor';
 import { formatParagraphWithContentModel } from '../utils/formatParagraphWithContentModel';
 
+const tagNamesSupportingMargins = 'p,h1,h2,h3,h4,h5,h6';
+
 /**
  * Toggles the current block(s) margin properties.
  * @param editor The editor to operate on
  * @param marginFormat Object containing margin props to be changed.
  * If a margin property is not in this object, it won't be changed.
  */
-export default function toggleMargins(editor: IContentModelEditor, marginFormat: MarginFormat) {
+export default function toggleMargins(
+    editor: IContentModelEditor,
+    marginFormat: Pick<MarginFormat, 'marginTop' | 'marginBottom'>
+) {
     formatParagraphWithContentModel(editor, 'toggleMargins', para => {
-        if (!para.decorator || para.decorator.tagName !== 'p') {
+        if (!para.decorator || tagNamesSupportingMargins.indexOf(para.decorator.tagName) < 0) {
             para.decorator = createParagraphDecorator('p');
         }
         getObjectKeys(marginFormat).forEach(key => {
