edce3ad11d1bb6841461f40e494b3df3207459e2
Ian Elizondo <ianelicha@gmail.com>
2023-02-10 00:04:28 +0000

    Update button impl

--- demo/scripts/controls/ribbonButtons/contentModel/spaceBeforeAfterButtons.ts
+++ demo/scripts/controls/ribbonButtons/contentModel/spaceBeforeAfterButtons.ts
@@ -1,5 +1,5 @@
 import { RibbonButton } from 'roosterjs-react';
-import { isContentModelEditor, toggleMargins } from 'roosterjs-content-model';
+import { isContentModelEditor, setParagraphMargin } from 'roosterjs-content-model';
 
 const spaceAfterButtonKey = 'buttonNameSpaceAfter';
 const spaceBeforeButtonKey = 'buttonNameSpaceBefore';
@@ -13,11 +13,13 @@ export const spaceAfterButton: RibbonButton<typeof spaceAfterButtonKey> = {
     unlocalizedText: 'Remove space after',
     iconName: 'CaretDown8',
     isChecked: formatState => !formatState.marginBottom || parseInt(formatState.marginBottom) <= 0,
-    onClick: editor => {
+    onClick: (editor, _, _1, _2, { marginBottom }) => {
         if (isContentModelEditor(editor)) {
-            toggleMargins(editor, {
-                marginBottom: '8pt',
-            });
+            setParagraphMargin(
+                editor,
+                undefined /* marginTop */,
+                parseInt(marginBottom) ? null : '8pt'
+            );
         }
         return true;
     },
@@ -32,11 +34,13 @@ export const spaceBeforeButton: RibbonButton<typeof spaceBeforeButtonKey> = {
     unlocalizedText: 'Add space before',
     iconName: 'CaretUp8',
     isChecked: formatState => parseInt(formatState.marginTop) > 0,
-    onClick: editor => {
+    onClick: (editor, _, _1, _2, { marginTop }) => {
         if (isContentModelEditor(editor)) {
-            toggleMargins(editor, {
-                marginTop: '12pt',
-            });
+            setParagraphMargin(
+                editor,
+                parseInt(marginTop) ? null : '12pt',
+                undefined /* marginBottom */
+            );
         }
         return true;
     },
