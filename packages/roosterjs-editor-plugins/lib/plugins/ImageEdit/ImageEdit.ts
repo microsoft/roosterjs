98fcfd99fab889c4f84c28e6de50fce02d740f85
Ian Elizondo <ianelicha@gmail.com>
2023-02-24 22:53:51 +0000

    Remove duplicate implementation

--- packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/ImageEdit.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/ImageEdit/ImageEdit.ts
@@ -619,23 +619,6 @@ function setWrapperSizeDimensions(
     wrapper.style.height = getPx(height);
 }
 
-function setWrapperSizeDimensions(
-    wrapper: HTMLElement,
-    image: HTMLImageElement,
-    width: number,
-    height: number
-) {
-    const hasBorder = image.style.borderStyle;
-    if (hasBorder) {
-        const borderWidth = image.style.borderWidth ? 2 * parseInt(image.style.borderWidth) : 2;
-        wrapper.style.width = getPx(width + borderWidth);
-        wrapper.style.height = getPx(height + borderWidth);
-        return;
-    }
-    wrapper.style.width = getPx(width);
-    wrapper.style.height = getPx(height);
-}
-
 function getPx(value: number): string {
     return value + 'px';
 }
