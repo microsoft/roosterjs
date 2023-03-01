dc115de6b5423ba44fdb610bfd4ea1a9b267390c
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2023-03-01 14:15:21 -0600

    Remove

--- packages/roosterjs-content-model/lib/editor/isFeatureEnabled.ts
+++
@@ -1,9 +0,0 @@
-import { EditorContext } from 'roosterjs-content-model/lib/publicTypes';
-import { ExperimentalFeatures } from 'roosterjs-editor-types/lib';
-
-/**
- * @internal
- */
-export default function isFeatureEnabled(context: EditorContext, feature: ExperimentalFeatures) {
-    return context.isFeatureEnabled(feature);
-}
