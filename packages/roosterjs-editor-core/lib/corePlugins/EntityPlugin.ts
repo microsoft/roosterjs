dd3e0071905fe8b2294180c09c0d4a3a73a7f398
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2023-03-10 15:49:01 -0600

    try again

--- packages/roosterjs-editor-core/lib/corePlugins/EntityPlugin.ts
+++ packages/roosterjs-editor-core/lib/corePlugins/EntityPlugin.ts
@@ -1,7 +1,7 @@
 import {
     inlineEntityOnPluginEvent,
     normalizeDelimitersInEditor,
-} from './utils/inlineEntityHandlers/inlineEntityOnPluginEvent';
+} from './utils/inlineEntityOnPluginEvent';
 import {
     Browser,
     commitEntity,
