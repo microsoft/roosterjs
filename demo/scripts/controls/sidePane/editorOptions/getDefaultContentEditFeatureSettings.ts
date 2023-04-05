08aa3e88e8613f8f509c6af05d5051227f916128
Bryan Valverde U <bvalverde@microsoft.com>
2023-04-05 15:42:16 -0600

    Split IndentWithTab and OutdentWhenShiftTab to be able to disable the Alt+Shift+Arrow hotkey  (#1706)
    * Init
    
    * add unit tests

--- demo/scripts/controls/sidePane/editorOptions/getDefaultContentEditFeatureSettings.ts
+++ demo/scripts/controls/sidePane/editorOptions/getDefaultContentEditFeatureSettings.ts
@@ -2,10 +2,14 @@ import { ContentEditFeatureSettings } from 'roosterjs-editor-types';
 import { getAllFeatures } from 'roosterjs-editor-plugins/lib/ContentEdit';
 import { getObjectKeys } from 'roosterjs-editor-dom';
 
-export default function getDefaultContentEditFeatureSettings() {
+export default function getDefaultContentEditFeatureSettings(): ContentEditFeatureSettings {
     const allFeatures = getAllFeatures();
-    return getObjectKeys(allFeatures).reduce((settings, key) => {
-        settings[key] = !allFeatures[key].defaultDisabled;
-        return settings;
-    }, <ContentEditFeatureSettings>{});
+    return {
+        ...getObjectKeys(allFeatures).reduce((settings, key) => {
+            settings[key] = !allFeatures[key].defaultDisabled;
+            return settings;
+        }, <ContentEditFeatureSettings>{}),
+        indentWhenAltShiftRight: true,
+        outdentWhenAltShiftLeft: true,
+    };
 }
