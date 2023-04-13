0c03b1fb46d05ebb3e101136de8f4deb25a0b7cb
Ian Elizondo <ianelicha@gmail.com>
2023-04-13 07:09:25 +0000

    Improve test

--- packages/roosterjs-editor-plugins/test/Watermark/WatermarkTest.ts
+++ packages/roosterjs-editor-plugins/test/Watermark/WatermarkTest.ts
@@ -4,32 +4,50 @@ import { IEditor } from 'roosterjs-editor-types';
 
 const watermarkText = 'this is a watermark';
 const watermarkClass = 'watermarkClass';
-const editorId = 'WatermarkEditor';
+const preEditorId = 'WatermarkEditor';
 
-function blurEverything() {
-    const activeElement = document.activeElement;
-    if (activeElement) {
-        (activeElement as HTMLElement).blur();
-    }
+function blurEditor(editor: IEditor) {
+    //create an element, focus it, then remove it
+    const element = document.createElement('div');
+    element.setAttribute('tabindex', '0');
+    document.body.appendChild(element);
+    element.focus();
+    document.body.removeChild(element);
+    const blurEvent = new FocusEvent('blur', {
+        bubbles: true,
+    });
+    document.querySelector(`#${editor.getEditorDomAttribute('id')}`)?.dispatchEvent(blurEvent);
+}
+
+function focusEditor(editor: IEditor) {
+    editor.focus();
+    const focusEvent = new FocusEvent('focus', {
+        bubbles: true,
+    });
+    document.querySelector(`#${editor.getEditorDomAttribute('id')}`)?.dispatchEvent(focusEvent);
 }
 
 function getWatermarkSpan(editor: IEditor) {
-    return editor.getDocument().querySelector(`.${watermarkClass}`);
+    const list = editor.queryElements(`.${watermarkClass}`);
+    expect(list.length).toBeLessThanOrEqual(1);
+    return <HTMLElement | null>list[0];
 }
 
 describe('Watermark plugin', () => {
     let plugin: Watermark;
     let editor: IEditor;
+    let runNum = 1;
+    let editorId: string;
 
     beforeEach(() => {
         plugin = new Watermark(watermarkText, undefined, watermarkClass);
+        editorId = preEditorId + runNum;
         editor = TestHelper.initEditor(editorId, [plugin]);
+        runNum++;
     });
 
     afterEach(() => {
         editor.dispose();
-        plugin.dispose();
-        document.querySelector(`#${editorId}`)?.remove();
     });
 
     it('correctly initializes', () => {
@@ -41,16 +59,19 @@ describe('Watermark plugin', () => {
     });
 
     it('correctly removes watermark when focused', () => {
+        const remover = spyOn(plugin, <keyof Watermark>'removeWatermark').and.callThrough();
         expect(getWatermarkSpan(editor)).toBeDefined();
-        editor.focus();
-        expect(getWatermarkSpan(editor)).toBeNull();
+        focusEditor(editor);
+        expect(remover).toHaveBeenCalled();
+        expect(getWatermarkSpan(editor)).toBeUndefined();
     });
 
     it('watermark returns after blur', () => {
         expect(getWatermarkSpan(editor)).toBeDefined();
-        editor.focus();
-        expect(getWatermarkSpan(editor)).toBeNull();
-        blurEverything();
+        focusEditor(editor);
+        expect(getWatermarkSpan(editor)).toBeUndefined();
+        blurEditor(editor);
+        expect(editor.hasFocus()).toBe(false);
         expect(getWatermarkSpan(editor)).toBeDefined();
     });
 });
