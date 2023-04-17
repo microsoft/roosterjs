9e4609a3f2517fab2a7f8b90c9e08155ff081832
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2023-04-17 11:23:23 -0600

    Add comment

--- packages/roosterjs-editor-plugins/lib/plugins/TableCellSelection/utils/updateSelection.ts
+++ packages/roosterjs-editor-plugins/lib/plugins/TableCellSelection/utils/updateSelection.ts
@@ -2,6 +2,9 @@ import { IEditor } from 'roosterjs-editor-types';
 
 /**
  * @internal
+ * Use SetBaseAndExtend to update the selection without losing the order that was used in the selection.
+ * Using editor.select may lose the order of the selection if the start of the selection is After
+ * the end container of the selection.
  */
 export function updateSelection(
     editor: IEditor,
