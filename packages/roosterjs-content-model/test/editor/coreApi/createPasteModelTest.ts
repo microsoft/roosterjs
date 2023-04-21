59680b962e33d4619a87a1c90f23a02d6a3413e0
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2023-04-21 08:44:06 -0600

    fix test

--- packages/roosterjs-content-model/test/editor/coreApi/createPasteModelTest.ts
+++ packages/roosterjs-content-model/test/editor/coreApi/createPasteModelTest.ts
@@ -1,3 +1,4 @@
+import * as createFragmentFromClipboardData from 'roosterjs-editor-dom/lib/clipboard/createFragmentFromClipboardData';
 import * as createPasteModelFile from '../../../lib/editor/coreApi/createPasteModel';
 import * as domToContentModel from '../../../lib/domToModel/domToContentModel';
 import ContentModelBeforePasteEvent from '../../../lib/publicTypes/event/ContentModelBeforePasteEvent';
@@ -29,6 +30,8 @@ describe('createPasteModel', () => {
             .createSpy('createEditorContext')
             .and.returnValue(mockedContext);
         spyOn(createPasteModelFile, 'createBeforePasteEvent').and.returnValue(beforePasteEvent);
+        spyOn(domToContentModel, 'default').and.callThrough();
+        spyOn(createFragmentFromClipboardData, 'default').and.returnValue(fragment);
 
         const triggerEvent = jasmine.createSpy().and.callThrough();
 
