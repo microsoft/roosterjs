71411fb86738281dfee21451feaf8d987cb15fa9
Júlia Roldi <juliaroldi@microsoft.com>
2023-06-13 18:45:49 -0300

    remove changes

--- packages/roosterjs-editor-api/lib/table/editTable.ts
+++ packages/roosterjs-editor-api/lib/table/editTable.ts
@@ -18,10 +18,12 @@ export default function editTable(
             editor,
             () => {
                 let vtable = new VTable(td);
+
                 saveTableSelection(editor, vtable);
                 vtable.edit(operation);
                 vtable.writeBack(false /** skipApplyFormat */, editor.getDarkColorHandler());
                 editor.transformToDarkColor(vtable.table);
+
                 editor.focus();
                 if (isUndefined(vtable.row) || isUndefined(vtable.col)) {
                     return;
