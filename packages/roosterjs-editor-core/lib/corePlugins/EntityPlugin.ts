63b096ffefbf2310e277f7e997a5efaa8626b6d2
jiuqingsong <jisong@microsoft.com>
2023-06-16 14:55:47 -0700

    improve

--- packages/roosterjs-editor-core/lib/corePlugins/EntityPlugin.ts
+++ packages/roosterjs-editor-core/lib/corePlugins/EntityPlugin.ts
@@ -64,7 +64,6 @@ const REMOVE_ENTITY_OPERATIONS: (EntityOperation | CompatibleEntityOperation)[] 
 export default class EntityPlugin implements PluginWithState<EntityPluginState> {
     private editor: IEditor | null = null;
     private state: EntityPluginState;
-    private cancelAsyncRun: (() => void) | null = null;
 
     /**
      * Construct a new instance of EntityPlugin
@@ -258,11 +257,11 @@ export default class EntityPlugin implements PluginWithState<EntityPluginState> 
 
     private handleEntityOperationEvent(event: EntityOperationEvent) {
         if (this.editor && REMOVE_ENTITY_OPERATIONS.indexOf(event.operation) >= 0) {
-            this.cancelAsyncRun?.();
-            this.cancelAsyncRun = this.editor.runAsync(() => {
-                this.cancelAsyncRun = null;
-                this.handleContentChangedEvent();
-            });
+            const item = this.state.entityMap[event.entity.id];
+
+            if (item) {
+                item.isDeleted = true;
+            }
         }
     }
 
