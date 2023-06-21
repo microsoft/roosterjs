9e0e27ca3dd8e7c8a91db1feba84e7307adeddce
jiuqingsong <jisong@microsoft.com>
2023-06-21 08:53:10 -0700

    add comment

--- packages/roosterjs-content-model/lib/modelApi/common/normalizeContentModel.ts
+++ packages/roosterjs-content-model/lib/modelApi/common/normalizeContentModel.ts
@@ -4,7 +4,13 @@ import { normalizeParagraph } from './normalizeParagraph';
 import { unwrapBlock } from './unwrapBlock';
 
 /**
- * @internal
+ * For a given content model, normalize it to make the model be consistent.
+ * This process includes:
+ * - For a list item without any list level, unwrap the list item
+ * - For a paragraph, make sure it has BR at the end if it is an empty paragraph
+ * - For text segments under paragraph, make sure its space values are correct (use nbsp to replace space when necessary)
+ * - For an empty block, remove it
+ * @param group The root level block group of content model to normalize
  */
 export function normalizeContentModel(group: ContentModelBlockGroup) {
     for (let i = group.blocks.length - 1; i >= 0; i--) {
