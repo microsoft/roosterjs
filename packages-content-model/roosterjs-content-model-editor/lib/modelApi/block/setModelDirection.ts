a839a8b327bf0bc5e224fc62b270da0edc1e146a
Jiuqing Song <jisong@microsoft.com>
2023-06-29 10:21:15 -0700

    fix test

--- packages-content-model/roosterjs-content-model-editor/lib/modelApi/block/setModelDirection.ts
+++ packages-content-model/roosterjs-content-model-editor/lib/modelApi/block/setModelDirection.ts
@@ -2,9 +2,11 @@ import { findListItemsInSameThread } from '../list/findListItemsInSameThread';
 import { getOperationalBlocks } from '../selection/collectSelections';
 import { isBlockGroupOfType } from '../common/isBlockGroupOfType';
 import {
-    ContentModelBlock,
+    ContentModelBlockFormat,
     ContentModelDocument,
     ContentModelListItem,
+    MarginFormat,
+    PaddingFormat,
 } from 'roosterjs-content-model-types';
 
 /**
@@ -25,29 +27,45 @@ export function setModelDirection(model: ContentModelDocument, direction: 'ltr' 
                 item.levels.forEach(level => {
                     level.direction = direction;
                 });
-            });
 
-            block.blocks.forEach(block => internalSetDirection(block, direction));
+                item.blocks.forEach(block => internalSetDirection(block.format, direction));
+            });
         } else if (block) {
-            internalSetDirection(block, direction);
+            internalSetDirection(block.format, direction);
         }
     });
 
     return paragraphOrListItemOrTable.length > 0;
 }
 
-function internalSetDirection(block: ContentModelBlock, direction: 'ltr' | 'rtl') {
-    block.format.direction = direction;
+function internalSetDirection(format: ContentModelBlockFormat, direction: 'ltr' | 'rtl') {
+    const wasRtl = format.direction == 'rtl';
+    const isRtl = direction == 'rtl';
 
-    // Adjust margin when change direction
-    // TODO: make margin and padding direction-aware, like what we did for textAlign. So no need to adjust them here
-    // TODO: Do we also need to handle border here?
-    const marginLeft = block.format.marginLeft;
-    const paddingLeft = block.format.paddingLeft;
+    if (wasRtl != isRtl) {
+        format.direction = direction;
 
-    block.format.marginLeft = block.format.marginRight;
-    block.format.marginRight = marginLeft;
+        // Adjust margin when change direction
+        // TODO: make margin and padding direction-aware, like what we did for textAlign. So no need to adjust them here
+        // TODO: Do we also need to handle border here?
+        const marginLeft = format.marginLeft;
+        const paddingLeft = format.paddingLeft;
 
-    block.format.paddingLeft = block.format.paddingRight;
-    block.format.paddingRight = paddingLeft;
+        setProperty(format, 'marginLeft', format.marginRight);
+        setProperty(format, 'marginRight', marginLeft);
+        setProperty(format, 'paddingLeft', format.paddingRight);
+        setProperty(format, 'paddingRight', paddingLeft);
+    }
+}
+
+function setProperty(
+    format: MarginFormat & PaddingFormat,
+    key: keyof (MarginFormat & PaddingFormat),
+    value: string | undefined
+) {
+    if (value) {
+        format[key] = value;
+    } else {
+        delete format[key];
+    }
 }
---
+++ packages-content-model/roosterjs-content-model-editor/test/modelApi/block/setModelDirectionTest.ts
@@ -0,0 +1,222 @@
+import { ContentModelDocument } from 'roosterjs-content-model-types';
+import { setModelDirection } from '../../../lib/modelApi/block/setModelDirection';
+
+describe('setModelDirection', () => {
+    function runTest(
+        model: ContentModelDocument,
+        direction: 'ltr' | 'rtl',
+        expectedModel: ContentModelDocument,
+        expectedResult: boolean
+    ) {
+        const result = setModelDirection(model, direction);
+
+        expect(result).toBe(expectedResult);
+        expect(model).toEqual(expectedModel);
+    }
+
+    it('set direction for paragraph', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {
+                            marginLeft: '10px',
+                            paddingRight: '20px',
+                        },
+                        segments: [
+                            {
+                                segmentType: 'SelectionMarker',
+                                format: {},
+                                isSelected: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            'rtl',
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Paragraph',
+                        format: {
+                            direction: 'rtl',
+                            marginRight: '10px',
+                            paddingLeft: '20px',
+                        },
+                        segments: [
+                            {
+                                segmentType: 'SelectionMarker',
+                                format: {},
+                                isSelected: true,
+                            },
+                        ],
+                    },
+                ],
+            },
+            true
+        );
+    });
+
+    it('set direction for divider', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Divider',
+                        format: {},
+                        isSelected: true,
+                        tagName: 'hr',
+                    },
+                ],
+            },
+            'rtl',
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'Divider',
+                        format: {
+                            direction: 'rtl',
+                        },
+                        isSelected: true,
+                        tagName: 'hr',
+                    },
+                ],
+            },
+            true
+        );
+    });
+
+    it('set direction for list item', () => {
+        runTest(
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'BlockGroup',
+                        blockGroupType: 'ListItem',
+                        format: {},
+                        formatHolder: {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: true,
+                        },
+                        levels: [
+                            {
+                                listType: 'OL',
+                            },
+                        ],
+                        blocks: [
+                            {
+                                blockType: 'Paragraph',
+                                segments: [],
+                                format: {},
+                            },
+                            {
+                                blockType: 'Paragraph',
+                                segments: [],
+                                format: {},
+                            },
+                        ],
+                    },
+                    {
+                        blockType: 'BlockGroup',
+                        blockGroupType: 'ListItem',
+                        format: {},
+                        formatHolder: {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: true,
+                        },
+                        levels: [
+                            {
+                                listType: 'OL',
+                            },
+                        ],
+                        blocks: [
+                            {
+                                blockType: 'Paragraph',
+                                segments: [
+                                    {
+                                        segmentType: 'SelectionMarker',
+                                        format: {},
+                                        isSelected: true,
+                                    },
+                                ],
+                                format: {},
+                            },
+                        ],
+                    },
+                ],
+            },
+            'rtl',
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'BlockGroup',
+                        blockGroupType: 'ListItem',
+                        format: {},
+                        formatHolder: {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: true,
+                        },
+                        levels: [
+                            {
+                                listType: 'OL',
+                                direction: 'rtl',
+                            },
+                        ],
+                        blocks: [
+                            {
+                                blockType: 'Paragraph',
+                                segments: [],
+                                format: { direction: 'rtl' },
+                            },
+                            {
+                                blockType: 'Paragraph',
+                                segments: [],
+                                format: { direction: 'rtl' },
+                            },
+                        ],
+                    },
+                    {
+                        blockType: 'BlockGroup',
+                        blockGroupType: 'ListItem',
+                        format: {},
+                        formatHolder: {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: true,
+                        },
+                        levels: [
+                            {
+                                listType: 'OL',
+                                direction: 'rtl',
+                            },
+                        ],
+                        blocks: [
+                            {
+                                blockType: 'Paragraph',
+                                segments: [
+                                    {
+                                        segmentType: 'SelectionMarker',
+                                        format: {},
+                                        isSelected: true,
+                                    },
+                                ],
+                                format: { direction: 'rtl' },
+                            },
+                        ],
+                    },
+                ],
+            },
+            true
+        );
+    });
+});
