0f6e0cc253c79c5f3082d5d6f749eae6111b183f
AndresCT-98 <acampostams@microsoft.com>
2023-07-13 15:16:55 -0600

    add metadata, fix demo and tests

--- demo/scripts/controls/contentModel/components/format/formatPart/TableCellMetadataFormatRender.ts
+++
@@ -1,8 +0,0 @@
-import { createCheckboxFormatRenderer } from '../utils/createCheckboxFormatRenderer';
-import { TableCellMetadataFormat } from 'roosterjs-editor-types';
-
-export const TableCellMetadataFormatRender = createCheckboxFormatRenderer<TableCellMetadataFormat>(
-    'BgColorOverride',
-    format => format.bgColorOverride,
-    (format, value) => (format.bgColorOverride = value)
-);
--- packages/roosterjs-editor-dom/test/table/cloneCellStylesTest.ts
+++ packages/roosterjs-editor-dom/test/table/cloneCellStylesTest.ts
@@ -1,4 +1,5 @@
 import cloneCellStyles from '../../lib/table/cloneCellStyles';
+import { saveTableCellMetadata } from '../../lib/table/tableCellInfo';
 
 describe('cloneCellStyles', () => {
     function runTest(style: string) {
@@ -6,8 +7,15 @@ describe('cloneCellStyles', () => {
         const styledCell = document.createElement('td');
         styledCell.setAttribute('style', style);
         cloneCellStyles(cell, styledCell);
+        console.log(
+            'FAILED:',
+            cell.getAttribute('data-editing-info'),
+            styledCell.getAttribute('data-editing-info')
+        );
         expect(cell.getAttribute('style')).toEqual(style);
-        expect(cell.getAttribute('data-editing-info')).toBe('{"bgColorOverride":true}');
+        expect(cell.getAttribute('data-editing-info')).toBe(
+            styledCell.getAttribute('data-editing-info')
+        );
     }
 
     it('cloneCellStyles | should clone style and add metadata', () => {
