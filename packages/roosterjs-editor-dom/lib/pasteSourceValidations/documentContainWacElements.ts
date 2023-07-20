7eb04032a505d65233fafd995bf2e5e46f56f8a2
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2023-07-20 17:42:50 -0600

    Add more classes from WordOnline

--- packages/roosterjs-editor-dom/lib/pasteSourceValidations/documentContainWacElements.ts
+++ packages/roosterjs-editor-dom/lib/pasteSourceValidations/documentContainWacElements.ts
@@ -1,7 +1,30 @@
 import type { getSourceFunction, getSourceInputParams } from './getPasteSource';
 
+const WORD_CLASSES = [
+    'OutlineElement',
+    'NumberListStyle',
+    'WACImageContainer',
+    'ListContainerWrapper',
+    'BulletListStyle',
+    'TableInsertRowGapBlank',
+    'TableColumnResizeHandle',
+    'TableCellTopBorderHandle',
+    'TableCellLeftBorderHandle',
+    'TableHoverColumnHandle',
+    'TableHoverRowHandle',
+    'ListMarkerWrappingSpan',
+    'TableCellContent',
+    'Paragraph',
+    'WACImageContainer',
+    'WACImageBorder',
+    'TableContainer',
+    'LineBreakBlob',
+    'TableWordWrap',
+];
+
 const WAC_IDENTIFY_SELECTOR =
-    'ul[class^="BulletListStyle"]>.OutlineElement,ol[class^="NumberListStyle"]>.OutlineElement,span.WACImageContainer';
+    'ul[class^="BulletListStyle"]>.OutlineElement,ol[class^="NumberListStyle"]>.OutlineElement,span.WACImageContainer,' +
+    WORD_CLASSES.map(c => '.' + c).join(',');
 
 /**
  * @internal
