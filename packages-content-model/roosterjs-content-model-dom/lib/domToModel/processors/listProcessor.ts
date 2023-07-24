2e88b883fd0b7ec628e83875485bd7aabaf7b447
Jiuqing Song <jisong@microsoft.com>
2023-07-24 16:44:23 -0700

    fix

--- packages-content-model/roosterjs-content-model-dom/lib/domToModel/processors/listProcessor.ts
+++ packages-content-model/roosterjs-content-model-dom/lib/domToModel/processors/listProcessor.ts
@@ -27,6 +27,7 @@ export const listProcessor: ElementProcessor<HTMLOListElement | HTMLUListElement
             const { listFormat } = context;
 
             parseFormat(element, context.formatParsers.dataset, level.dataset, context);
+            parseFormat(element, context.formatParsers.listLevel, level.format, context);
 
             // TODO: Move this out into roosterjs-content-model-editor package
             updateListMetadata(level, metadata => {
@@ -43,7 +44,6 @@ export const listProcessor: ElementProcessor<HTMLOListElement | HTMLUListElement
                 return metadata;
             });
 
-            parseFormat(element, context.formatParsers.listLevel, level.format, context);
             parseFormat(element, context.formatParsers.segment, context.segmentFormat, context);
 
             const originalListParent = listFormat.listParent;
