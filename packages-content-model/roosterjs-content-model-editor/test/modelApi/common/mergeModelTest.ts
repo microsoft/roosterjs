e973ff24ec4fb9aba94088230a33a6ad94e356a1
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2023-08-09 09:24:15 -0600

    init

--- packages-content-model/roosterjs-content-model-editor/test/modelApi/common/mergeModelTest.ts
+++ packages-content-model/roosterjs-content-model-editor/test/modelApi/common/mergeModelTest.ts
@@ -2418,4 +2418,127 @@ describe('mergeModel', () => {
             ],
         });
     });
+
+    it('Merge Table with styles into paragraph', () => {
+        const majorModel = createContentModelDocument();
+        const para1 = createParagraph(false, undefined, {
+            fontFamily: 'Arial',
+            fontSize: '15px',
+            backgroundColor: 'red',
+            textColor: 'blue',
+            italic: false,
+        });
+        const marker = createSelectionMarker();
+        const text1 = createText('test1');
+        const text2 = createText('test2');
+
+        para1.segments.push(text1, marker, text2);
+        majorModel.blocks.push(para1);
+
+        const sourceModel: ContentModelDocument = createContentModelDocument();
+
+        const newPara1 = createParagraph();
+        const newText1 = createText('newText1');
+        const newCell1 = createTableCell(false, false);
+        const newTable1 = createTable(1, {
+            textAlign: 'start',
+            whiteSpace: 'normal',
+            borderTop: '1px solid black',
+            borderRight: '1px solid black',
+            borderBottom: '1px solid black',
+            borderLeft: '1px solid black',
+            backgroundColor: 'rgb(255, 255, 255)',
+            useBorderBox: true,
+            borderCollapse: true,
+        });
+
+        newPara1.segments.push(newText1);
+        newCell1.blocks.push(newPara1);
+        newTable1.rows[0].cells.push(newCell1);
+        sourceModel.blocks.push(newTable1);
+
+        mergeModel(majorModel, sourceModel);
+
+        expect(majorModel).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [{ segmentType: 'Text', text: 'test1', format: {} }],
+                    format: {},
+                    segmentFormat: {
+                        fontFamily: 'Arial',
+                        fontSize: '15px',
+                        backgroundColor: 'red',
+                        textColor: 'blue',
+                        italic: false,
+                    },
+                },
+                {
+                    blockType: 'Table',
+                    rows: [
+                        {
+                            height: 0,
+                            format: {},
+                            cells: [
+                                {
+                                    blockGroupType: 'TableCell',
+                                    blocks: [
+                                        {
+                                            blockType: 'Paragraph',
+                                            segments: [
+                                                {
+                                                    segmentType: 'Text',
+                                                    text: 'newText1',
+                                                    format: {},
+                                                },
+                                            ],
+                                            format: {},
+                                        },
+                                    ],
+                                    format: {},
+                                    spanLeft: false,
+                                    spanAbove: false,
+                                    isHeader: false,
+                                    dataset: {},
+                                },
+                            ],
+                        },
+                    ],
+                    format: {
+                        textAlign: 'start',
+                        whiteSpace: 'normal',
+                        borderTop: '1px solid black',
+                        borderRight: '1px solid black',
+                        borderBottom: '1px solid black',
+                        borderLeft: '1px solid black',
+                        backgroundColor: 'rgb(255, 255, 255)',
+                        useBorderBox: true,
+                        borderCollapse: true,
+                    },
+                    widths: [],
+                    dataset: {},
+                },
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'SelectionMarker',
+                            isSelected: true,
+                            format: {},
+                        },
+                        { segmentType: 'Text', text: 'test2', format: {} },
+                    ],
+                    format: {},
+                    segmentFormat: {
+                        fontFamily: 'Arial',
+                        fontSize: '15px',
+                        backgroundColor: 'red',
+                        textColor: 'blue',
+                        italic: false,
+                    },
+                },
+            ],
+        });
+    });
 });
