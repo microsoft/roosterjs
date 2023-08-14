06736fa3a1059964d6d6c460008b4c1945f08364
Júlia Roldi <juliaroldi@microsoft.com>
2023-08-14 15:03:54 -0300

    fix cell empty cells

--- packages-content-model/roosterjs-content-model-dom/lib/modelToDom/handlers/handleTable.ts
+++ packages-content-model/roosterjs-content-model-dom/lib/modelToDom/handlers/handleTable.ts
@@ -75,79 +75,72 @@ export const handleTable: ContentModelBlockHandler<ContentModelTable> = (
         for (let col = 0; col < tableRow.cells.length; col++) {
             const cell = tableRow.cells[col];
 
-            if (cell) {
-                if (cell.isSelected) {
-                    context.tableSelection = context.tableSelection || {
-                        table: tableNode,
-                        firstCell: { x: col, y: row },
-                        lastCell: { x: col, y: row },
-                    };
+            if (cell.isSelected) {
+                context.tableSelection = context.tableSelection || {
+                    table: tableNode,
+                    firstCell: { x: col, y: row },
+                    lastCell: { x: col, y: row },
+                };
 
-                    if (context.tableSelection.table == tableNode) {
-                        const lastCell = context.tableSelection.lastCell;
+                if (context.tableSelection.table == tableNode) {
+                    const lastCell = context.tableSelection.lastCell;
 
-                        lastCell.x = Math.max(lastCell.x, col);
-                        lastCell.y = Math.max(lastCell.y, row);
+                    lastCell.x = Math.max(lastCell.x, col);
+                    lastCell.y = Math.max(lastCell.y, row);
+                }
+            }
+
+            if (!cell.spanAbove && !cell.spanLeft) {
+                let td =
+                    (context.allowCacheElement && cell.cachedElement) ||
+                    doc.createElement(cell.isHeader ? 'th' : 'td');
+
+                tr.appendChild(td);
+
+                let rowSpan = 1;
+                let colSpan = 1;
+                let width = table.widths[col];
+                let height = tableRow.height;
+
+                for (; table.rows[row + rowSpan]?.cells[col]?.spanAbove; rowSpan++) {
+                    height += table.rows[row + rowSpan].height;
+                }
+                for (; tableRow.cells[col + colSpan]?.spanLeft; colSpan++) {
+                    width += table.widths[col + colSpan];
+                }
+
+                if (rowSpan > 1) {
+                    td.rowSpan = rowSpan;
+                }
+
+                if (colSpan > 1) {
+                    td.colSpan = colSpan;
+                }
+
+                if (!cell.cachedElement || (cell.format.useBorderBox && hasMetadata(table))) {
+                    if (width > 0 && !td.style.width) {
+                        td.style.width = width + 'px';
+                    }
+
+                    if (height > 0 && !td.style.height) {
+                        td.style.height = height + 'px';
                     }
                 }
 
-                if (!cell.spanAbove && !cell.spanLeft) {
-                    let td =
-                        (context.allowCacheElement && cell.cachedElement) ||
-                        doc.createElement(cell.isHeader ? 'th' : 'td');
-
-                    tr.appendChild(td);
-
-                    let rowSpan = 1;
-                    let colSpan = 1;
-                    let width = table.widths[col];
-                    let height = tableRow.height;
-
-                    for (; table.rows[row + rowSpan]?.cells[col]?.spanAbove; rowSpan++) {
-                        height += table.rows[row + rowSpan].height;
-                    }
-                    for (; tableRow.cells[col + colSpan]?.spanLeft; colSpan++) {
-                        width += table.widths[col + colSpan];
+                if (!cell.cachedElement) {
+                    if (context.allowCacheElement) {
+                        cell.cachedElement = td;
                     }
 
-                    if (rowSpan > 1) {
-                        td.rowSpan = rowSpan;
-                    }
-
-                    if (colSpan > 1) {
-                        td.colSpan = colSpan;
-                    }
-
-                    if (!cell.cachedElement || (cell.format.useBorderBox && hasMetadata(table))) {
-                        if (width > 0 && !td.style.width) {
-                            td.style.width = width + 'px';
-                        }
-
-                        if (height > 0 && !td.style.height) {
-                            td.style.height = height + 'px';
-                        }
-                    }
-
-                    if (!cell.cachedElement) {
-                        if (context.allowCacheElement) {
-                            cell.cachedElement = td;
-                        }
-
-                        applyFormat(td, context.formatAppliers.block, cell.format, context);
-                        applyFormat(td, context.formatAppliers.tableCell, cell.format, context);
-                        applyFormat(
-                            td,
-                            context.formatAppliers.tableCellBorder,
-                            cell.format,
-                            context
-                        );
-                        applyFormat(td, context.formatAppliers.dataset, cell.dataset, context);
-                    }
-
-                    context.modelHandlers.blockGroupChildren(doc, td, cell, context);
-
-                    context.onNodeCreated?.(cell, td);
+                    applyFormat(td, context.formatAppliers.block, cell.format, context);
+                    applyFormat(td, context.formatAppliers.tableCell, cell.format, context);
+                    applyFormat(td, context.formatAppliers.tableCellBorder, cell.format, context);
+                    applyFormat(td, context.formatAppliers.dataset, cell.dataset, context);
                 }
+
+                context.modelHandlers.blockGroupChildren(doc, td, cell, context);
+
+                context.onNodeCreated?.(cell, td);
             }
         }
     }
