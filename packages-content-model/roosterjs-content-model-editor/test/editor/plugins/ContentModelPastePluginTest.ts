8c7fa2b39621bc1e23f50847349a25dbbc4795db
Bryan Valverde U <bvalverde@microsoft.com>
2023-08-16 16:13:57 -0600

    Remove `display: flex` style on paste (#2031)
    * init
    
    * Fix

--- packages-content-model/roosterjs-content-model-editor/test/editor/plugins/ContentModelPastePluginTest.ts
+++ packages-content-model/roosterjs-content-model-editor/test/editor/plugins/ContentModelPastePluginTest.ts
@@ -1,16 +1,29 @@
 import * as addParser from '../../../lib/editor/plugins/PastePlugin/utils/addParser';
+import * as chainSanitizerCallbackFile from 'roosterjs-editor-dom/lib/htmlSanitizer/chainSanitizerCallback';
+import * as ExcelFile from '../../../lib/editor/plugins/PastePlugin/Excel/processPastedContentFromExcel';
 import * as getPasteSource from 'roosterjs-editor-dom/lib/pasteSourceValidations/getPasteSource';
+import * as PowerPointFile from '../../../lib/editor/plugins/PastePlugin/PowerPoint/processPastedContentFromPowerPoint';
+import * as setProcessor from '../../../lib/editor/plugins/PastePlugin/utils/setProcessor';
+import * as WacFile from '../../../lib/editor/plugins/PastePlugin/WacComponents/processPastedContentWacComponents';
 import * as WordDesktopFile from '../../../lib/editor/plugins/PastePlugin/WordDesktop/processPastedContentFromWordDesktop';
 import ContentModelBeforePasteEvent from '../../../lib/publicTypes/event/ContentModelBeforePasteEvent';
 import ContentModelPastePlugin from '../../../lib/editor/plugins/PastePlugin/ContentModelPastePlugin';
 import { IContentModelEditor } from '../../../lib/publicTypes/IContentModelEditor';
-import { KnownPasteSourceType, PluginEventType } from 'roosterjs-editor-types';
+import { KnownPasteSourceType, PasteType, PluginEventType } from 'roosterjs-editor-types';
+
+const trustedHTMLHandler = <any>'mock';
+const GOOGLE_SHEET_NODE_NAME = 'google-sheets-html-origin';
 
 describe('Paste', () => {
     let editor: IContentModelEditor;
 
     beforeEach(() => {
-        editor = ({} as any) as IContentModelEditor;
+        editor = ({
+            getTrustedHTMLHandler: () => trustedHTMLHandler,
+        } as any) as IContentModelEditor;
+        spyOn(addParser, 'default').and.callThrough();
+        spyOn(chainSanitizerCallbackFile, 'default').and.callThrough();
+        spyOn(setProcessor, 'setProcessor').and.callThrough();
     });
 
     let event: ContentModelBeforePasteEvent = <ContentModelBeforePasteEvent>(<any>{
@@ -57,6 +70,11 @@ describe('Paste', () => {
                     preserveHtmlComments: false,
                     unknownTagReplacement: null,
                 },
+                pasteType: PasteType.Normal,
+                clipboardData: <any>{
+                    html: '',
+                },
+                fragment: document.createDocumentFragment(),
             });
         });
 
@@ -71,18 +89,131 @@ describe('Paste', () => {
             expect(event.domToModelOption.processorOverride?.element).toBe(
                 WordDesktopFile.wordDesktopElementProcessor
             );
+            expect(addParser.default).toHaveBeenCalledTimes(4);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(5);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(1);
         });
 
-        it('Default', () => {
-            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.Default);
-            spyOn(WordDesktopFile, 'processPastedContentFromWordDesktop');
-            spyOn(addParser, 'default').and.callThrough();
+        it('Excel | merge format', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.ExcelDesktop);
+            spyOn(ExcelFile, 'processPastedContentFromExcel').and.callThrough();
+
+            (<any>event).pasteType = PasteType.MergeFormat;
+            plugin.initialize(editor);
+            plugin.onPluginEvent(event);
+
+            expect(ExcelFile.processPastedContentFromExcel).toHaveBeenCalledWith(
+                event,
+                trustedHTMLHandler
+            );
+            expect(addParser.default).toHaveBeenCalledTimes(2);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(0);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+        });
+
+        it('Excel | image', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.ExcelDesktop);
+            spyOn(ExcelFile, 'processPastedContentFromExcel').and.callThrough();
+
+            (<any>event).pasteType = PasteType.AsImage;
+            plugin.initialize(editor);
+            plugin.onPluginEvent(event);
+
+            expect(ExcelFile.processPastedContentFromExcel).not.toHaveBeenCalledWith(
+                event,
+                trustedHTMLHandler
+            );
+            expect(addParser.default).toHaveBeenCalledTimes(1);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(0);
+        });
+
+        it('Excel', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.ExcelDesktop);
+            spyOn(ExcelFile, 'processPastedContentFromExcel').and.callThrough();
 
             plugin.initialize(editor);
             plugin.onPluginEvent(event);
 
-            expect(WordDesktopFile.processPastedContentFromWordDesktop).not.toHaveBeenCalled();
-            expect(event.domToModelOption.processorOverride?.element).toBeUndefined();
+            expect(ExcelFile.processPastedContentFromExcel).toHaveBeenCalledWith(
+                event,
+                trustedHTMLHandler
+            );
+            expect(addParser.default).toHaveBeenCalledTimes(2);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(0);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+        });
+
+        it('Excel Online', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.ExcelOnline);
+            spyOn(ExcelFile, 'processPastedContentFromExcel').and.callThrough();
+
+            plugin.initialize(editor);
+            plugin.onPluginEvent(event);
+
+            expect(ExcelFile.processPastedContentFromExcel).toHaveBeenCalledWith(
+                event,
+                trustedHTMLHandler
+            );
+            expect(addParser.default).toHaveBeenCalledTimes(2);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(0);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+        });
+
+        it('Power Point', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(
+                KnownPasteSourceType.PowerPointDesktop
+            );
+            spyOn(PowerPointFile, 'processPastedContentFromPowerPoint').and.callThrough();
+
+            plugin.initialize(editor);
+            plugin.onPluginEvent(event);
+
+            expect(PowerPointFile.processPastedContentFromPowerPoint).toHaveBeenCalledWith(
+                event,
+                trustedHTMLHandler
+            );
+            expect(addParser.default).toHaveBeenCalledTimes(1);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(0);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+        });
+
+        it('Wac', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.WacComponents);
+            spyOn(WacFile, 'processPastedContentWacComponents').and.callThrough();
+
+            plugin.initialize(editor);
+            plugin.onPluginEvent(event);
+
+            expect(WacFile.processPastedContentWacComponents).toHaveBeenCalledWith(event);
+            expect(addParser.default).toHaveBeenCalledTimes(5);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(4);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+        });
+
+        it('Default', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.Default);
+
+            plugin.initialize(editor);
+            plugin.onPluginEvent(event);
+
+            expect(addParser.default).toHaveBeenCalledTimes(1);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(0);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+        });
+
+        it('Google Sheets', () => {
+            spyOn(getPasteSource, 'default').and.returnValue(KnownPasteSourceType.GoogleSheets);
+
+            plugin.initialize(editor);
+            plugin.onPluginEvent(event);
+
+            expect(addParser.default).toHaveBeenCalledTimes(1);
+            expect(setProcessor.setProcessor).toHaveBeenCalledTimes(0);
+            expect(chainSanitizerCallbackFile.default).toHaveBeenCalledTimes(3);
+            expect(
+                event.sanitizingOption.additionalTagReplacements[GOOGLE_SHEET_NODE_NAME]
+            ).toEqual('*');
         });
     });
 });
