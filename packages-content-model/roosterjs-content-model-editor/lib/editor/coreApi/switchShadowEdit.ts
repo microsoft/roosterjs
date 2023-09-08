a3384ea29e53075e00b4237d445b009c73443bf7
jiuqingsong <jisong@microsoft.com>
2023-09-07 22:30:50 -0700

    improve

--- packages-content-model/roosterjs-content-model-editor/lib/editor/coreApi/switchShadowEdit.ts
+++ packages-content-model/roosterjs-content-model-editor/lib/editor/coreApi/switchShadowEdit.ts
@@ -20,19 +20,23 @@ export const switchShadowEdit: SwitchShadowEdit = (editorCore, isOn): void => {
 
             const range = core.api.getSelectionRange(core, true /*tryGetFromCache*/);
 
-            core.lifecycle.shadowEditSelectionPath =
-                range && getSelectionPath(core.contentDiv, range);
-            core.lifecycle.shadowEditFragment = core.contentDiv.ownerDocument.createDocumentFragment();
+            // Fake object, not used in Content Model Editor, just to satisfy original editor code
+            // TODO: we can remove them once we have standalone Content Model Editor
+            const fragment = core.contentDiv.ownerDocument.createDocumentFragment();
+            const selectionPath = range && getSelectionPath(core.contentDiv, range);
 
             core.api.triggerEvent(
                 core,
                 {
                     eventType: PluginEventType.EnteredShadowEdit,
-                    fragment: core.lifecycle.shadowEditFragment,
-                    selectionPath: core.lifecycle.shadowEditSelectionPath,
+                    fragment,
+                    selectionPath,
                 },
                 false /*broadcast*/
             );
+
+            core.lifecycle.shadowEditSelectionPath = selectionPath;
+            core.lifecycle.shadowEditFragment = fragment;
         } else {
             core.lifecycle.shadowEditFragment = null;
             core.lifecycle.shadowEditSelectionPath = null;
