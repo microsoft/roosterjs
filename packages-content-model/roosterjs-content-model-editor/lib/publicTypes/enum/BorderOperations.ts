aca7e122acdb5a11baf12dbaa61df2daf6a5167f
AndresCT-98 <acampostams@microsoft.com>
2023-10-19 16:28:51 -0600

    add other operations, refactor applyTableFormat

--- packages-content-model/roosterjs-content-model-editor/lib/publicTypes/enum/BorderOperations.ts
+++ packages-content-model/roosterjs-content-model-editor/lib/publicTypes/enum/BorderOperations.ts
@@ -5,4 +5,32 @@ export type BorderOperations =
     /**
      * Apply border format to all borders
      */
-    'AllBorders';
+    | 'AllBorders'
+    /**
+     * Remove al borders
+     */
+    | 'NoBorders'
+    /**
+     * Apply border format to left borders
+     */
+    | 'LeftBorders'
+    /**
+     * Apply border format to right borders
+     */
+    | 'RightBorders'
+    /**
+     * Apply border format to top borders
+     */
+    | 'TopBorders'
+    /**
+     * Apply border format to bottom borders
+     */
+    | 'BottomBorders'
+    /**
+     * Apply border format to inside borders
+     */
+    | 'InsideBorders'
+    /**
+     * Apply border format to outside borders
+     */
+    | 'OutsideBorders';
