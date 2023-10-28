a0afcba5aeebccc825ca9ddcc0a1eec07924ea83
jiuqingsong <jisong@microsoft.com>
2023-10-27 21:46:17 -0700

    remove unnecessary change

--- packages-content-model/roosterjs-content-model/lib/index.ts
+++ packages-content-model/roosterjs-content-model/lib/index.ts
@@ -2,4 +2,3 @@ export { createContentModelEditor } from './createContentModelEditor';
 export * from 'roosterjs-content-model-types';
 export * from 'roosterjs-content-model-dom';
 export * from 'roosterjs-content-model-editor';
-export * from 'roosterjs-content-model-adapter';
