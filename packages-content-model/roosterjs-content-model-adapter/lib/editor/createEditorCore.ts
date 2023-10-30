66e6d957ec4045737ba59184d35c33a8c3924d18
Jiuqing Song <jisong@microsoft.com>
2023-10-30 15:50:06 -0700

    Standalone editor step 0: Create a copy of Editor class (#2175)
    * Standalone editor step 0: Create a copy of Editor class
    
    * remove unnecessary change
    
    * improve

---
+++ packages-content-model/roosterjs-content-model-adapter/lib/editor/createEditorCore.ts
@@ -0,0 +1,61 @@
+import createCorePlugins, { getPluginState } from '../corePlugins/createCorePlugins';
+import DarkColorHandlerImpl from './DarkColorHandlerImpl';
+import { arrayPush, getIntersectedRect } from 'roosterjs-editor-dom';
+import { coreApiMap } from '../coreApi/coreApiMap';
+import { getObjectKeys } from 'roosterjs-content-model-dom';
+import type { CoreCreator, EditorCore, EditorOptions, EditorPlugin } from 'roosterjs-editor-types';
+
+/**
+ * @internal
+ * Create a new instance of Editor Core
+ * @param contentDiv The DIV HTML element which will be the container element of editor
+ * @param options An optional options object to customize the editor
+ */
+export const createEditorCore: CoreCreator<EditorCore, EditorOptions> = (contentDiv, options) => {
+    const corePlugins = createCorePlugins(contentDiv, options);
+    const plugins: EditorPlugin[] = [];
+
+    getObjectKeys(corePlugins).forEach(name => {
+        if (name == '_placeholder') {
+            if (options.plugins) {
+                arrayPush(plugins, options.plugins);
+            }
+        } else {
+            plugins.push(corePlugins[name]);
+        }
+    });
+
+    const pluginState = getPluginState(corePlugins);
+    const zoomScale: number = (options.zoomScale ?? -1) > 0 ? options.zoomScale! : 1;
+    const getVisibleViewport =
+        options.getVisibleViewport ||
+        (() => {
+            const scrollContainer = pluginState.domEvent.scrollContainer;
+
+            return getIntersectedRect(
+                scrollContainer == core.contentDiv
+                    ? [scrollContainer]
+                    : [scrollContainer, core.contentDiv]
+            );
+        });
+
+    const core: EditorCore = {
+        contentDiv,
+        api: {
+            ...coreApiMap,
+            ...(options.coreApiOverride || {}),
+        },
+        originalApi: coreApiMap,
+        plugins: plugins.filter(x => !!x),
+        ...pluginState,
+        trustedHTMLHandler: options.trustedHTMLHandler || ((html: string) => html),
+        zoomScale: zoomScale,
+        sizeTransformer: options.sizeTransformer || ((size: number) => size / zoomScale),
+        getVisibleViewport,
+        imageSelectionBorderColor: options.imageSelectionBorderColor,
+        darkColorHandler: new DarkColorHandlerImpl(contentDiv, pluginState.lifecycle.getDarkColor),
+        disposeErrorHandler: options.disposeErrorHandler,
+    };
+
+    return core;
+};
