64bb4ec5c295e5bdb80c4ca4505e3d7ebf402dfe
jiuqingsong <jisong@microsoft.com>
2023-12-12 23:02:53 -0800

    fix build

--- packages-content-model/roosterjs-content-model-core/lib/publicApi/table/getSelectedCells.ts
+++ packages-content-model/roosterjs-content-model-core/lib/publicApi/table/getSelectedCells.ts
@@ -1,4 +1,4 @@
-import { hasSelectionInBlockGroup } from 'roosterjs-content-model-core';
+import hasSelectionInBlockGroup from '../selection/hasSelectionInBlockGroup';
 import type { ContentModelTable, TableSelectionCoordinates } from 'roosterjs-content-model-types';
 
 /**
