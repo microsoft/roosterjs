2c4ff2dbe4bc55f29a0f029a7ffad752e971e26a
Bryan Valverde U <bvalverde@microsoft.com>
2024-01-12 15:15:59 -0600

    Table fidelity Improvement: Add each border radius when the shorthand css is not provided and `borderCollapse: separate` support (#2325)
    * Add border radius props
    
    * fix
    
    * fix test
    
    * Change test name
    
    * address comment

--- packages-content-model/roosterjs-content-model-types/lib/format/formatParts/SpacingFormat.ts
+++ packages-content-model/roosterjs-content-model-types/lib/format/formatParts/SpacingFormat.ts
@@ -6,4 +6,9 @@ export type SpacingFormat = {
      * Whether borders of cells are collapsed together
      */
     borderCollapse?: boolean;
+
+    /**
+     * Whether borders of cells are separated
+     */
+    borderSeparate?: boolean;
 };
