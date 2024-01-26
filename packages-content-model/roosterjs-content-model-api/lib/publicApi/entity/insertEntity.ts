4e78323fe6f64286ca6f49f90bebd09f966c35bb
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2024-01-26 11:05:21 -0600

    remove unneeded change

--- packages-content-model/roosterjs-content-model-api/lib/publicApi/entity/insertEntity.ts
+++ packages-content-model/roosterjs-content-model-api/lib/publicApi/entity/insertEntity.ts
@@ -72,8 +72,7 @@ export default function insertEntity(
         true /*isReadonly*/,
         undefined /*format*/,
         type,
-        undefined /*id*/,
-        isBlock
+        undefined /*id*/
     );
 
     editor.formatContentModel(
