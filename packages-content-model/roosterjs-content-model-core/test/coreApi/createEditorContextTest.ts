36010934337cfa774e23e1e4cf07723bf9e004ab
Bryan Valverde U <bvalverde@microsoft.com>
2024-02-09 13:43:29 -0600

    Fix REM Unit behavior (#2403)
    * init
    
    * add test
    
    * add computed value to context
    
    * fix type
    
    * Fix tests
    
    * address comment
    
    * change to documentElement

--- packages-content-model/roosterjs-content-model-core/test/coreApi/createEditorContextTest.ts
+++ packages-content-model/roosterjs-content-model-core/test/coreApi/createEditorContextTest.ts
@@ -46,6 +46,7 @@ describe('createEditorContext', () => {
             domIndexer: undefined,
             pendingFormat: undefined,
             zoomScale: 1,
+            rootFontSize: 16,
         });
     });
 
@@ -93,6 +94,7 @@ describe('createEditorContext', () => {
             domIndexer,
             pendingFormat: undefined,
             zoomScale: 1,
+            rootFontSize: 16,
         });
     });
 
@@ -139,6 +141,7 @@ describe('createEditorContext', () => {
             domIndexer: undefined,
             pendingFormat: mockedPendingFormat,
             zoomScale: 1,
+            rootFontSize: 16,
         });
     });
 });
@@ -193,6 +196,7 @@ describe('createEditorContext - checkZoomScale', () => {
             allowCacheElement: true,
             domIndexer: undefined,
             pendingFormat: undefined,
+            rootFontSize: 16,
         });
     });
 });
@@ -248,6 +252,7 @@ describe('createEditorContext - checkRootDir', () => {
             domIndexer: undefined,
             pendingFormat: undefined,
             zoomScale: 1,
+            rootFontSize: 16,
         });
     });
 
@@ -268,6 +273,7 @@ describe('createEditorContext - checkRootDir', () => {
             domIndexer: undefined,
             pendingFormat: undefined,
             zoomScale: 1,
+            rootFontSize: 16,
         });
     });
 });
