0f334bb0235f4c4f548b6b88e9b7a06dc2ee6a70
Andres-CT98 <107568016+Andres-CT98@users.noreply.github.com>
2024-02-15 11:49:30 -0600

    Select newly inserted row/column (#2412)
    * create clear selection
    
    * reorder selection hierarchy
    
    * fix insertRow/Column and export
    
    * revert selection hierarchy
    
    * add comments
    
    * simplify clear selection
    
    * fix tests
    
    * move clearSelectedCells
    
    * add tests and valid range check
    
    * remove unused import

--- packages-content-model/roosterjs-content-model-api/lib/modelApi/table/insertTableColumn.ts
+++ packages-content-model/roosterjs-content-model-api/lib/modelApi/table/insertTableColumn.ts
@@ -1,3 +1,4 @@
+import { clearSelectedCells } from './clearSelectedCells';
 import { createTableCell } from 'roosterjs-content-model-dom';
 import { getSelectedCells } from 'roosterjs-content-model-core';
 import type {
@@ -6,7 +7,9 @@ import type {
 } from 'roosterjs-content-model-types';
 
 /**
- * @internal
+ * Insert a column to the table
+ * @param table The table model where the column is to be inserted
+ * @param operation The operation to be performed
  */
 export function insertTableColumn(
     table: ContentModelTable,
@@ -16,21 +19,21 @@ export function insertTableColumn(
     const insertLeft = operation == 'insertLeft';
 
     if (sel) {
+        clearSelectedCells(table, sel);
         for (let i = sel?.firstColumn; i <= sel.lastColumn; i++) {
             table.rows.forEach(row => {
                 const cell = row.cells[insertLeft ? sel.firstColumn : sel.lastColumn];
 
-                row.cells.splice(
-                    insertLeft ? sel.firstColumn : sel.lastColumn + 1,
-                    0,
-                    createTableCell(
-                        cell.spanLeft,
-                        cell.spanAbove,
-                        cell.isHeader,
-                        cell.format,
-                        cell.dataset
-                    )
+                const newCell = createTableCell(
+                    cell.spanLeft,
+                    cell.spanAbove,
+                    cell.isHeader,
+                    cell.format,
+                    cell.dataset
                 );
+                newCell.isSelected = true;
+
+                row.cells.splice(insertLeft ? sel.firstColumn : sel.lastColumn + 1, 0, newCell);
             });
             table.widths.splice(
                 insertLeft ? sel.firstColumn : sel.lastColumn + 1,
