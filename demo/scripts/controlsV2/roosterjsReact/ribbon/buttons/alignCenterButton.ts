31be2cb7be06105cb9699e40e49fcd093329025f
Jiuqing Song <jisong@microsoft.com>
2024-03-04 13:14:31 -0800

    Port demo site step 4

---
+++ demo/scripts/controlsV2/roosterjsReact/ribbon/buttons/alignCenterButton.ts
@@ -0,0 +1,16 @@
+import { setAlignment } from 'roosterjs-content-model-api';
+import type { AlignCenterButtonStringKey } from '../type/RibbonButtonStringKeys';
+import type { RibbonButton } from '../type/RibbonButton';
+
+/**
+ * @internal
+ * "Align center" button on the format ribbon
+ */
+export const alignCenterButton: RibbonButton<AlignCenterButtonStringKey> = {
+    key: 'buttonNameAlignCenter',
+    unlocalizedText: 'Align center',
+    iconName: 'AlignCenter',
+    onClick: editor => {
+        setAlignment(editor, 'center');
+    },
+};
---
+++ demo/scripts/controlsV2/roosterjsReact/ribbon/buttons/numberedListButton.ts
@@ -0,0 +1,17 @@
+import { toggleNumbering } from 'roosterjs-content-model-api';
+import type { RibbonButton } from '../type/RibbonButton';
+import type { NumberedListButtonStringKey } from '../type/RibbonButtonStringKeys';
+
+/**
+ * @internal
+ * "Numbering list" button on the format ribbon
+ */
+export const numberedListButton: RibbonButton<NumberedListButtonStringKey> = {
+    key: 'buttonNameNumberedList',
+    unlocalizedText: 'Numbered List',
+    iconName: 'NumberedList',
+    isChecked: formatState => formatState.isNumbering,
+    onClick: editor => {
+        toggleNumbering(editor);
+    },
+};
