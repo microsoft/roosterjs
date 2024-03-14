bb55615d8bc9eb15887890f14aeca1ff4831469e
Jiuqing Song <jisong@microsoft.com>
2024-03-14 16:13:17 -0700

    improve

--- packages/roosterjs-content-model-plugins/test/watermark/WatermarkPluginTest.ts
+++ packages/roosterjs-content-model-plugins/test/watermark/WatermarkPluginTest.ts
@@ -84,39 +84,6 @@ describe('WatermarkPlugin', () => {
         );
     });
 
-    it('No format, empty editor, with callback', () => {
-        isModelEmptyFastSpy.and.returnValue(true);
-
-        const watermarkCallback = jasmine.createSpy('watermark').and.returnValue('test');
-        const plugin = new WatermarkPlugin(watermarkCallback);
-
-        plugin.initialize(editor);
-
-        plugin.onPluginEvent({ eventType: 'editorReady' });
-
-        expect(formatContentModelSpy).toHaveBeenCalledTimes(1);
-        expect(setEditorStyleSpy).toHaveBeenCalledTimes(1);
-        expect(setEditorStyleSpy).toHaveBeenCalledWith(
-            '_WatermarkContent',
-            'position: absolute; pointer-events: none; content: "test";font-size: 14px!important;color: #AAAAAA!important;',
-            'before'
-        );
-        expect(watermarkCallback).toHaveBeenCalledTimes(1);
-
-        plugin.onPluginEvent({ eventType: 'input' } as any);
-        expect(formatContentModelSpy).toHaveBeenCalledTimes(2);
-        expect(setEditorStyleSpy).toHaveBeenCalledTimes(1);
-        expect(watermarkCallback).toHaveBeenCalledTimes(1);
-
-        isModelEmptyFastSpy.and.returnValue(false);
-
-        plugin.onPluginEvent({ eventType: 'input' } as any);
-        expect(formatContentModelSpy).toHaveBeenCalledTimes(3);
-        expect(setEditorStyleSpy).toHaveBeenCalledTimes(2);
-        expect(setEditorStyleSpy).toHaveBeenCalledWith('_WatermarkContent', null);
-        expect(watermarkCallback).toHaveBeenCalledTimes(1);
-    });
-
     it('Has format, empty editor, with text', () => {
         isModelEmptyFastSpy.and.returnValue(true);
 
