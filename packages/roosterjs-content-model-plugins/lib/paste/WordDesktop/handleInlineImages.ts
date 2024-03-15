08467143dd682d88a76d0deb1d3c2005952a4886
Jiuqing Song <jisong@microsoft.com>
2024-03-15 16:58:03 -0700

    Improve

--- packages/roosterjs-content-model-plugins/lib/paste/WordDesktop/handleInlineImages.ts
+++ packages/roosterjs-content-model-plugins/lib/paste/WordDesktop/handleInlineImages.ts
@@ -15,10 +15,34 @@ export function handleInlineImages(fragment: DocumentFragment, rtf?: string) {
             imageElements.forEach((imageElement, i) => {
                 if (imageContents[i]) {
                     imageElement.src = 'data:image/png;base64,' + imageContents[i];
+                } else {
+                    imageElement.parentElement?.removeChild(imageElement);
                 }
             });
         }
     }
+}
+
+function parsePair(content: string, startIndex: number) {
+    let level = 1;
+    let index = startIndex;
+
+    while (index < content.length) {
+        const c = content[index];
+
+        if (c == '{') {
+            level++;
+        } else if (c == '}') {
+            level--;
+
+            if (level == 0) {
+                break;
+            }
+        }
+        index++;
+    }
+
+    return index;
 }
 
 function getImagesFromRtf(content: string) {
@@ -29,26 +53,49 @@ function getImagesFromRtf(content: string) {
 
     while (startIndex >= 0 && startIndex < content.length) {
         const imageTagStartIndex = content.indexOf('{\\*\\blipuid', startIndex);
-        const imageTagEndIndex =
-            imageTagStartIndex >= 0 ? content.indexOf('}', imageTagStartIndex) : -1;
-        const imageTag =
-            imageTagEndIndex >= 0
-                ? content.substring(imageTagStartIndex + 1, imageTagEndIndex)
-                : '';
+        const shapeStartIndex = content.indexOf('{\\*\\shpinst', startIndex);
+        const shapeGroupStartIndex = content.indexOf('\\shpgrp', startIndex);
 
-        if (imageTag) {
-            const imageStartIndex = imageTagEndIndex + 1;
-            const imageEndIndex = content.indexOf('}', imageStartIndex);
-            const imageData = content.substring(imageStartIndex, imageEndIndex);
+        if (
+            imageTagStartIndex >= 0 &&
+            (shapeStartIndex < 0 || imageTagStartIndex < shapeStartIndex) &&
+            (shapeGroupStartIndex < 0 || imageTagStartIndex < shapeGroupStartIndex)
+        ) {
+            const imageTagEndIndex =
+                imageTagStartIndex >= 0 ? content.indexOf('}', imageTagStartIndex) : -1;
+            const imageTag =
+                imageTagEndIndex >= 0
+                    ? content.substring(imageTagStartIndex + 1, imageTagEndIndex)
+                    : '';
 
-            if (knownImageTags.indexOf(imageTag) < 0) {
-                const blob = imageData.replace(/[\r\n]/g, '');
-                const dataUri = createDataUrlFromImageContent(blob);
-                result.push(dataUri);
-                knownImageTags.push(imageTag);
+            if (imageTag) {
+                const imageStartIndex = imageTagEndIndex + 1;
+                const imageEndIndex = content.indexOf('}', imageStartIndex);
+                const imageData = content.substring(imageStartIndex, imageEndIndex);
+
+                if (knownImageTags.indexOf(imageTag) < 0) {
+                    const blob = imageData.replace(/[\r\n]/g, '');
+                    const dataUri = createDataUrlFromImageContent(blob);
+                    result.push(dataUri);
+                    knownImageTags.push(imageTag);
+                }
+
+                startIndex = imageEndIndex + 1;
+            } else {
+                break;
             }
-
-            startIndex = imageEndIndex + 1;
+        } else if (
+            shapeGroupStartIndex >= 0 &&
+            (shapeStartIndex < 0 || shapeStartIndex > shapeGroupStartIndex)
+        ) {
+            result.push('');
+            startIndex = parsePair(content, shapeGroupStartIndex + 1);
+        } else if (
+            shapeStartIndex >= 0 &&
+            (shapeGroupStartIndex < 0 || shapeGroupStartIndex > shapeStartIndex)
+        ) {
+            result.push('');
+            startIndex = parsePair(content, shapeStartIndex + 1);
         } else {
             break;
         }
