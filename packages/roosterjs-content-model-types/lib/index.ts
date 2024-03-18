cc864a2bcef1f0c528f8536eb569c81af37335b8
Jiuqing Song <jisong@microsoft.com>
2024-03-18 09:56:31 -0700

    fix build

--- packages/roosterjs-content-model-types/lib/index.ts
+++ packages/roosterjs-content-model-types/lib/index.ts
@@ -285,6 +285,14 @@ export { DOMHelper } from './parameter/DOMHelper';
 export { ImageEditOperation, ImageEditor } from './parameter/ImageEditor';
 export { CachedElementHandler, CloneModelOptions } from './parameter/CloneModelOptions';
 export { LinkData } from './parameter/LinkData';
+export { MergeModelOption } from './parameter/MergeModelOption';
+export {
+    IterateSelectionsCallback,
+    IterateSelectionsOption,
+} from './parameter/IterateSelectionsOption';
+export { NodeTypeMap } from './parameter/NodeTypeMap';
+export { TypeOfBlockGroup } from './parameter/TypeOfBlockGroup';
+export { OperationalBlocks } from './parameter/OperationalBlocks';
 export { ParsedTable, ParsedTableCell } from './parameter/ParsedTable';
 
 export { BasePluginEvent, BasePluginDomEvent } from './event/BasePluginEvent';
