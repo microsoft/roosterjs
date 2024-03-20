a40384e836b1b783a883354919405b8b4b7d60c6
Júlia Roldi <juliaroldi@microsoft.com>
2024-03-20 18:31:28 -0300

    WIP

--- packages/roosterjs-content-model-plugins/lib/markdown/utils/setFormat.ts
+++ packages/roosterjs-content-model-plugins/lib/markdown/utils/setFormat.ts
@@ -1,4 +1,4 @@
-import { createSelectionMarker, createText } from 'roosterjs-content-model-dom';
+import { createText } from 'roosterjs-content-model-dom';
 import { getSelectedSegmentsAndParagraphs } from 'roosterjs-content-model-core';
 
 import {
@@ -9,14 +9,16 @@ import {
 
 export function setFormat(editor: IEditor, character: string, format: ContentModelSegmentFormat) {
     editor.formatContentModel(
-        model => {
+        (model, context) => {
             const selectedSegmentsAndParagraphs = getSelectedSegmentsAndParagraphs(
                 model,
                 false /*includeFormatHolder*/
             );
+
             if (selectedSegmentsAndParagraphs.length > 0 && selectedSegmentsAndParagraphs[0][1]) {
                 const marker = selectedSegmentsAndParagraphs[0][0];
-                const initialFormat = marker.format;
+                context.newPendingFormat = marker.format;
+
                 const paragraph = selectedSegmentsAndParagraphs[0][1];
                 if (marker.segmentType == 'SelectionMarker') {
                     const markerIndex = paragraph.segments.indexOf(marker);
@@ -47,27 +49,14 @@ export function setFormat(editor: IEditor, character: string, format: ContentMod
                             const formattedSegment = createText(textSegments[1], marker.format);
                             paragraph.segments.splice(index + 1, 0, formattedSegment);
 
-                            for (let i = index + 1; i <= markerIndex; i++) {
-                                const segment = paragraph.segments[i];
-                                if (segment && segment.segmentType == 'Text') {
+                            paragraph.segments.forEach((segment, i) => {
+                                if (i >= index + 1 && segment.segmentType == 'Text') {
                                     segment.format = {
                                         ...segment.format,
                                         ...format,
                                     };
                                 }
-                            }
-
-                            const newMarkerIndex = paragraph.segments.findIndex(
-                                segment => segment.segmentType == 'SelectionMarker'
-                            );
-
-                            if (newMarkerIndex > -1) {
-                                paragraph.segments.splice(
-                                    newMarkerIndex,
-                                    1,
-                                    createSelectionMarker(initialFormat)
-                                );
-                            }
+                            });
 
                             return true;
                         }
