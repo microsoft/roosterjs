b91d0f8a98ee186067721be3e2af0d53af0b767d
Jiuqing Song <jisong@microsoft.com>
2024-03-25 09:29:16 -0700

    Remove a redundant parameter from domToContentModel (#2521)

--- packages/roosterjs-content-model-dom/lib/domToModel/domToContentModel.ts
+++ packages/roosterjs-content-model-dom/lib/domToModel/domToContentModel.ts
@@ -1,26 +1,19 @@
 import { createContentModelDocument } from '../modelApi/creators/createContentModelDocument';
 import { normalizeContentModel } from '../modelApi/common/normalizeContentModel';
-import type {
-    ContentModelDocument,
-    DOMSelection,
-    DomToModelContext,
-} from 'roosterjs-content-model-types';
+import type { ContentModelDocument, DomToModelContext } from 'roosterjs-content-model-types';
 
 /**
  * Create Content Model from DOM tree in this editor
  * @param root Root element of DOM tree to create Content Model from
  * @param context Context object for DOM to Content Model conversion
- * @param selection Selection that already exists in content
  * @returns A ContentModelDocument object that contains all the models created from the give root element
  */
 export function domToContentModel(
     root: HTMLElement | DocumentFragment,
-    context: DomToModelContext,
-    selection?: DOMSelection
+    context: DomToModelContext
 ): ContentModelDocument {
     const model = createContentModelDocument(context.defaultFormat);
 
-    context.selection = selection;
     context.elementProcessors.child(model, root, context);
 
     normalizeContentModel(model);
