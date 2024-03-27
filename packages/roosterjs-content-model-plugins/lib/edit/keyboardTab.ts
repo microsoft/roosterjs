534cb5d335b8f3ca9cf340f72490e93127bc0213
AndresCT-98 <acampostams@microsoft.com>
2024-03-27 12:37:57 -0600

    add Table selection and single cell handling

--- packages/roosterjs-content-model-plugins/lib/edit/keyboardTab.ts
+++ packages/roosterjs-content-model-plugins/lib/edit/keyboardTab.ts
@@ -1,10 +1,13 @@
 import { getOperationalBlocks, isBlockGroupOfType } from 'roosterjs-content-model-dom';
 import { handleTabOnList } from './tabUtils/handleTabOnList';
 import { handleTabOnParagraph } from './tabUtils/handleTabOnParagraph';
+import { handleTabOnTable } from './tabUtils/handleTabOnTable';
+import { handleTabOnTableCell } from './tabUtils/handleTabOnTableCell';
 import { setModelIndentation } from 'roosterjs-content-model-api';
 import type {
     ContentModelDocument,
     ContentModelListItem,
+    ContentModelTableCell,
     IEditor,
 } from 'roosterjs-content-model-types';
 
@@ -14,31 +17,51 @@ import type {
 export function keyboardTab(editor: IEditor, rawEvent: KeyboardEvent) {
     const selection = editor.getDOMSelection();
 
-    if (selection?.type == 'range') {
-        editor.formatContentModel(
-            model => {
-                return handleTab(model, rawEvent);
-            },
-            {
-                apiName: 'handleTabKey',
-            }
-        );
+    switch (selection?.type) {
+        case 'range':
+            editor.formatContentModel(
+                model => {
+                    return handleTab(model, rawEvent, editor);
+                },
+                {
+                    apiName: 'handleTabKey',
+                }
+            );
 
-        return true;
+            return true;
+        case 'table':
+            editor.formatContentModel(
+                model => {
+                    return handleTabOnTable(model, rawEvent);
+                },
+                {
+                    apiName: 'handleTabKey',
+                }
+            );
+            return true;
     }
 }
 
 /**
  * If multiple blocks are selected, indent or outdent the selected blocks with setModelIndentation.
- * If only one block is selected, call handleTabOnParagraph or handleTabOnList to handle the tab key.
+ * If only one block is selected:
+ * - If it is a table cell, call handleTabOnTableCell to handle the tab key.
+ * - If it is a paragraph, call handleTabOnParagraph to handle the tab key.
+ * - If it is a list item, call handleTabOnList to handle the tab key.
  */
-function handleTab(model: ContentModelDocument, rawEvent: KeyboardEvent) {
-    const blocks = getOperationalBlocks<ContentModelListItem>(model, ['ListItem'], ['TableCell']);
+export function handleTab(model: ContentModelDocument, rawEvent: KeyboardEvent, editor: IEditor) {
+    const blocks = getOperationalBlocks<ContentModelListItem | ContentModelTableCell>(
+        model,
+        ['ListItem', 'TableCell'],
+        []
+    );
     const block = blocks.length > 0 ? blocks[0].block : undefined;
     if (blocks.length > 1) {
         setModelIndentation(model, rawEvent.shiftKey ? 'outdent' : 'indent');
         rawEvent.preventDefault();
         return true;
+    } else if (isBlockGroupOfType<ContentModelTableCell>(block, 'TableCell')) {
+        return handleTabOnTableCell(model, block, rawEvent);
     } else if (block?.blockType === 'Paragraph') {
         return handleTabOnParagraph(model, block, rawEvent);
     } else if (isBlockGroupOfType<ContentModelListItem>(block, 'ListItem')) {
