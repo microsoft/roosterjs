d5d25428129c1a9090609a50e31e36e7872a7f1b
Jiuqing Song <jisong@microsoft.com>
2024-03-28 14:55:55 -0700

    Fix 262677: Allow insert entity to a specified position without changing selection (#2537)

--- packages/roosterjs-content-model-dom/test/modelApi/common/addSegmentTest.ts
+++ packages/roosterjs-content-model-dom/test/modelApi/common/addSegmentTest.ts
@@ -1,8 +1,10 @@
 import { addBlock } from '../../../lib/modelApi/common/addBlock';
 import { addSegment } from '../../../lib/modelApi/common/addSegment';
 import { ContentModelGeneralBlock, ContentModelParagraph } from 'roosterjs-content-model-types';
+import { createBr } from '../../../lib/modelApi/creators/createBr';
 import { createContentModelDocument } from '../../../lib/modelApi/creators/createContentModelDocument';
 import { createParagraph } from '../../../lib/modelApi/creators/createParagraph';
+import { createSelectionMarker } from '../../../lib/modelApi/creators/createSelectionMarker';
 import { createText } from '../../../lib/modelApi/creators/createText';
 
 describe('addSegment', () => {
@@ -129,6 +131,268 @@ describe('addSegment', () => {
                             segmentType: 'Text',
                             text: 'test',
                             format: {},
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add selection marker in empty paragraph', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+
+        doc.blocks.push(para);
+
+        const newMarker = createSelectionMarker({ fontFamily: 'Arial' });
+
+        addSegment(doc, newMarker);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: { fontFamily: 'Arial' },
+                            isSelected: true,
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add selection marker after selection marker', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+        const marker = createSelectionMarker();
+
+        para.segments.push(marker);
+        doc.blocks.push(para);
+
+        const newMarker = createSelectionMarker({ fontFamily: 'Arial' });
+
+        addSegment(doc, newMarker);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: true,
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add selection marker after selected segment', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+        const br = createBr();
+
+        br.isSelected = true;
+        para.segments.push(br);
+        doc.blocks.push(para);
+
+        const newMarker = createSelectionMarker({ fontFamily: 'Arial' });
+
+        addSegment(doc, newMarker);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'Br',
+                            format: {},
+                            isSelected: true,
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add selection marker after selection marker', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+        const marker = createSelectionMarker();
+
+        para.segments.push(marker);
+        doc.blocks.push(para);
+
+        const newMarker = createSelectionMarker({ fontFamily: 'Arial' });
+
+        addSegment(doc, newMarker);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: true,
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add selection marker after selection marker that is not selected', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+        const marker = createSelectionMarker();
+
+        marker.isSelected = false;
+        para.segments.push(marker);
+        doc.blocks.push(para);
+
+        const newMarker = createSelectionMarker({ fontFamily: 'Arial' });
+
+        addSegment(doc, newMarker);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: false,
+                        },
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: { fontFamily: 'Arial' },
+                            isSelected: true,
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add unselected selection marker after selection marker', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+        const marker = createSelectionMarker();
+
+        para.segments.push(marker);
+        doc.blocks.push(para);
+
+        const newMarker = createSelectionMarker({ fontFamily: 'Arial' });
+
+        newMarker.isSelected = false;
+
+        addSegment(doc, newMarker);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: true,
+                        },
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: { fontFamily: 'Arial' },
+                            isSelected: false,
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add selected segment after selection marker', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+        const marker = createSelectionMarker();
+
+        para.segments.push(marker);
+        doc.blocks.push(para);
+
+        const br = createBr({ fontFamily: 'Arial' });
+
+        br.isSelected = true;
+
+        addSegment(doc, br);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'Br',
+                            format: { fontFamily: 'Arial' },
+                            isSelected: true,
+                        },
+                    ],
+                    format: {},
+                },
+            ],
+        });
+    });
+
+    it('Add selected segment after unselected selection marker', () => {
+        const doc = createContentModelDocument();
+        const para = createParagraph();
+        const marker = createSelectionMarker();
+
+        marker.isSelected = false;
+        para.segments.push(marker);
+        doc.blocks.push(para);
+
+        const br = createBr({ fontFamily: 'Arial' });
+
+        br.isSelected = true;
+
+        addSegment(doc, br);
+
+        expect(doc).toEqual({
+            blockGroupType: 'Document',
+            blocks: [
+                {
+                    blockType: 'Paragraph',
+                    segments: [
+                        {
+                            segmentType: 'SelectionMarker',
+                            format: {},
+                            isSelected: false,
+                        },
+                        {
+                            segmentType: 'Br',
+                            format: { fontFamily: 'Arial' },
+                            isSelected: true,
                         },
                     ],
                     format: {},
