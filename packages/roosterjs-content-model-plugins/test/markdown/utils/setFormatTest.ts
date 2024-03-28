274e8bf08d4a81a9faae496e5df50602f6858f8a
Júlia Roldi <juliaroldi@microsoft.com>
2024-03-28 19:17:09 -0300

    fix test

--- packages/roosterjs-content-model-plugins/test/markdown/utils/setFormatTest.ts
+++ packages/roosterjs-content-model-plugins/test/markdown/utils/setFormatTest.ts
@@ -299,7 +299,7 @@ describe('setFormat', () => {
                     segments: [
                         {
                             segmentType: 'Text',
-                            text: 'test  one ',
+                            text: 'test  one  ',
                             format: {},
                             isSelected: undefined,
                         },
