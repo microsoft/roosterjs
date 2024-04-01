18bb8ba92f765cec1ea52d1df02c0cf6b2856026
AndresCT-98 <acampostams@microsoft.com>
2024-04-01 13:50:06 -0600

    remove unused editor

--- packages/roosterjs-content-model-plugins/lib/edit/keyboardTab.ts
+++ packages/roosterjs-content-model-plugins/lib/edit/keyboardTab.ts
@@ -21,7 +21,7 @@ export function keyboardTab(editor: IEditor, rawEvent: KeyboardEvent) {
         case 'range':
             editor.formatContentModel(
                 model => {
-                    return handleTab(model, rawEvent, editor);
+                    return handleTab(model, rawEvent);
                 },
                 {
                     apiName: 'handleTabKey',
@@ -49,7 +49,7 @@ export function keyboardTab(editor: IEditor, rawEvent: KeyboardEvent) {
  * - If it is a paragraph, call handleTabOnParagraph to handle the tab key.
  * - If it is a list item, call handleTabOnList to handle the tab key.
  */
-function handleTab(model: ContentModelDocument, rawEvent: KeyboardEvent, editor: IEditor) {
+function handleTab(model: ContentModelDocument, rawEvent: KeyboardEvent) {
     const blocks = getOperationalBlocks<ContentModelListItem | ContentModelTableCell>(
         model,
         ['ListItem', 'TableCell'],
