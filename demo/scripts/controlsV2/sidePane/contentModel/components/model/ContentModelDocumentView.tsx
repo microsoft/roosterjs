6f4bd7c1db48c03f2aa608ccc9904561206f7979
Jiuqing Song <jisong@microsoft.com>
2024-04-16 17:10:43 -0700

    improve

--- demo/scripts/controlsV2/sidePane/contentModel/components/model/ContentModelDocumentView.tsx
+++ demo/scripts/controlsV2/sidePane/contentModel/components/model/ContentModelDocumentView.tsx
@@ -3,14 +3,41 @@ import { BlockGroupContentView } from './BlockGroupContentView';
 import { ContentModelDocument } from 'roosterjs-content-model-types';
 import { ContentModelView } from '../ContentModelView';
 import { hasSelectionInBlockGroup } from 'roosterjs-content-model-dom';
+import { SegmentFormatView } from '../format/SegmentFormatView';
+import { useProperty } from '../../hooks/useProperty';
 
 const styles = require('./ContentModelDocumentView.scss');
 
 export function ContentModelDocumentView(props: { doc: ContentModelDocument }) {
     const { doc } = props;
+    const [isReverted, setIsReverted] = useProperty(!!doc.hasRevertedRangeSelection);
+    const revertedCheckbox = React.useRef<HTMLInputElement>(null);
+    const onIsRevertedChange = React.useCallback(() => {
+        const newValue = revertedCheckbox.current.checked;
+        doc.hasRevertedRangeSelection = newValue;
+        setIsReverted(newValue);
+    }, [doc, setIsReverted]);
+
     const getContent = React.useCallback(() => {
-        return <BlockGroupContentView group={doc} />;
-    }, [doc]);
+        return (
+            <>
+                <div>
+                    <input
+                        type="checkbox"
+                        checked={isReverted}
+                        ref={revertedCheckbox}
+                        onChange={onIsRevertedChange}
+                    />
+                    Reverted range selection
+                </div>
+                <BlockGroupContentView group={doc} />
+            </>
+        );
+    }, [doc, isReverted]);
+
+    const getFormat = React.useCallback(() => {
+        return doc.format ? <SegmentFormatView format={doc.format} /> : null;
+    }, [doc.format]);
 
     return (
         <ContentModelView
@@ -19,6 +46,7 @@ export function ContentModelDocumentView(props: { doc: ContentModelDocument }) {
             hasSelection={hasSelectionInBlockGroup(doc)}
             jsonSource={doc}
             getContent={getContent}
+            getFormat={getFormat}
         />
     );
 }
