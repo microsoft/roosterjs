98655527a72fb8156141112c63bb4ea631aa7ea5
JiuqingSong <jisong@microsoft.com>
2024-04-30 09:31:02 -0700

    fix comment

--- packages/roosterjs-content-model-plugins/lib/edit/utils/splitParagraph.ts
+++ packages/roosterjs-content-model-plugins/lib/edit/utils/splitParagraph.ts
@@ -1,11 +1,17 @@
 import {
-    createBr,
     createParagraph,
     normalizeParagraph,
     setParagraphNotImplicit,
 } from 'roosterjs-content-model-dom';
 import type { InsertPoint } from 'roosterjs-content-model-types';
 
+/**
+ * @internal
+ * Split the given paragraph from insert point into two paragraphs,
+ * and move the selection marker to the beginning of the second paragraph
+ * @param insertPoint The input insert point which includes the paragraph and selection marker
+ * @returns The new paragraph it created
+ */
 export function splitParagraph(insertPoint: InsertPoint) {
     const { paragraph, marker } = insertPoint;
     const newParagraph = createParagraph(
@@ -24,10 +30,9 @@ export function splitParagraph(insertPoint: InsertPoint) {
 
     setParagraphNotImplicit(paragraph);
 
-    if (paragraph.segments.every(x => x.segmentType == 'SelectionMarker')) {
-        paragraph.segments.push(createBr(marker.format));
-    }
+    insertPoint.paragraph = newParagraph;
 
+    normalizeParagraph(paragraph);
     normalizeParagraph(newParagraph);
 
     return newParagraph;
