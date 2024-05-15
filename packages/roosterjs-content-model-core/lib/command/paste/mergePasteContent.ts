674d9cb2b1e40495fa19fd4204aab3d157ba8366
Jiuqing Song <jisong@microsoft.com>
2024-05-15 10:04:47 -0700

    add comment

--- packages/roosterjs-content-model-core/lib/command/paste/mergePasteContent.ts
+++ packages/roosterjs-content-model-core/lib/command/paste/mergePasteContent.ts
@@ -94,7 +94,7 @@ export function mergePasteContent(
         {
             changeSource: ChangeSource.Paste,
             getChangeData: () => clipboardData,
-            scrollCaretIntoView: false,
+            scrollCaretIntoView: false, // TODO #2633: Make a full fix to the scroll behavior
             apiName: 'paste',
         }
     );
