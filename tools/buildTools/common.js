6b383f0c81e1f1c9bc7142292e8f929874e0ac75
Jiuqing Song <jisong@microsoft.com>
2024-05-29 12:39:57 -0700

    improve

--- tools/buildTools/common.js
+++ tools/buildTools/common.js
@@ -129,7 +129,6 @@ const legacyPackages = [
     'roosterjs-editor-api',
     'roosterjs-editor-plugins',
 ];
-// const reactPackages = ['roosterjs-react'];
 const mainPackages = [
     'roosterjs',
     'roosterjs-content-model-types',
@@ -142,15 +141,6 @@ const mainPackages = [
 const legacyAdapterPackages = ['roosterjs-editor-adapter'];
 
 const buildConfig = {
-    // react: {
-    //     jsFileBaseName: 'rooster-react',
-    //     libraryName: 'roosterjsReact',
-    //     externalHandler: getWebpackExternalCallback([]),
-    //     dependsOnLegacy: true,
-    //     dependsOnReact: true,
-    //     packages: reactPackages,
-    //     entry: 'roosterjs-react',
-    // },
     main: {
         jsFileBaseName: 'rooster',
         libraryName: 'roosterjs',
