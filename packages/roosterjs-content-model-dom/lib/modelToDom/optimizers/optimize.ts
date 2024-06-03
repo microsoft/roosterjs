71f36067973b4172b8db8ddef82c1e32ec5946d9
庄黛淳华 <miku1958@users.noreply.github.com>
2024-06-03 09:32:20 +0800

    Increase the probability that removeUnnecessarySpan and mergeNode will be triggered.

--- packages/roosterjs-content-model-dom/lib/modelToDom/optimizers/optimize.ts
+++ packages/roosterjs-content-model-dom/lib/modelToDom/optimizers/optimize.ts
@@ -1,22 +1,36 @@
+import { enumerateComputedStyle, removeUnnecessaryAttribute, removeUnnecessarySpan, removeUnnecessaryStyle } from './removeUnnecessarySpan';
 import { isEntityElement } from '../../domUtils/entityUtils';
 import { mergeNode } from './mergeNode';
-import { removeUnnecessarySpan } from './removeUnnecessarySpan';
 
 /**
  * @internal
  */
-export function optimize(root: Node) {
+export function optimize(root: Node, isRecursive = false) {
     /**
      * Do no do any optimization to entity
      */
     if (isEntityElement(root)) {
         return;
     }
+    if (!isRecursive && root.parentElement != null) {
+        let computedAttributes = {} as Record<string, Attr>;
+        // html doesn't provide computed attributes, use parent's attributes directly
+        Array.from(root.parentElement.attributes).forEach((attr) => {
+            computedAttributes[attr.name] = attr;
+        });
+        removeUnnecessaryAttribute(root, computedAttributes);
+
+        let computedStyle = {} as Record<string, Set<string>>;
+        enumerateComputedStyle(root.parentElement, (key, values) => {
+            computedStyle[key] = values;
+        });
+        removeUnnecessaryStyle(root, computedStyle);
+    }
 
     removeUnnecessarySpan(root);
     mergeNode(root);
 
     for (let child = root.firstChild; child; child = child.nextSibling) {
-        optimize(child);
+        optimize(child, true);
     }
 }
