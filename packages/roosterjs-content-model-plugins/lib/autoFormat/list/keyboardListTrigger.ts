1a4c70832aad9f45159c057f1aac4ca14514f3a2
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2024-07-10 17:01:30 -0600

    add missing file

--- packages/roosterjs-content-model-plugins/lib/autoFormat/list/keyboardListTrigger.ts
+++ packages/roosterjs-content-model-plugins/lib/autoFormat/list/keyboardListTrigger.ts
@@ -1,5 +1,5 @@
 import { getListTypeStyle } from './getListTypeStyle';
-import { getOperationalBlocks, isBlockGroupOfType } from 'roosterjs-content-model-dom/lib';
+import { getOperationalBlocks, isBlockGroupOfType } from 'roosterjs-content-model-dom';
 import {
     getListAnnounceData,
     setListType,
