b4796e3ef3b1fed234b39e81bbb053aec6d8677e
Júlia Roldi <juliaroldi@microsoft.com>
2024-09-10 15:10:20 -0300

    comment

--- packages/roosterjs-content-model-core/lib/corePlugin/copyPaste/utils/adjustImageSelectionOnSafari.ts
+++ packages/roosterjs-content-model-core/lib/corePlugin/copyPaste/utils/adjustImageSelectionOnSafari.ts
@@ -1,5 +1,9 @@
 import type { IEditor, DOMSelection } from 'roosterjs-content-model-types';
 
+/**
+ * @internal
+ * Adjust Image selection, so the copy by keyboard does not remove image selection.
+ */
 export function adjustImageSelectionOnSafari(editor: IEditor, selection: DOMSelection | null) {
     if (editor.getEnvironment().isSafari && selection?.type == 'image') {
         const range = new Range();
