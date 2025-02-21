afe82ff08b8f296039859416c901f80260faa1d0
biwu <biwu@microsoft.com>
2025-02-21 14:44:35 -0800

    address comment

--- packages/roosterjs-editor-types/lib/compatibleEnum/RegionType.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Type of all possible regions. Currently we only support region of Table
- */
-export enum CompatibleRegionType {
-    /**
-     * Region split by Table
-     */
-    Table,
-}
