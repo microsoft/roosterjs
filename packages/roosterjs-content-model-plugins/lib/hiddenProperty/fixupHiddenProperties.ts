ff84eabf480ae117951a8e9950bfdadd1c77201e
jiuqingsong <jisong@microsoft.com>
2025-03-28 13:53:18 -0700

    improve comments

--- packages/roosterjs-content-model-plugins/lib/hiddenProperty/fixupHiddenProperties.ts
+++ packages/roosterjs-content-model-plugins/lib/hiddenProperty/fixupHiddenProperties.ts
@@ -3,6 +3,9 @@ import type { IEditor } from 'roosterjs-content-model-types';
 
 /**
  * @internal
+ * Maintain hidden properties in DOM after editor content is reset using HTML
+ * This includes:
+ * TODO: add more checkers here
  */
 export function fixupHiddenProperties(editor: IEditor, options: HiddenPropertyOptions) {
     // TODO: We can add more checkers here
