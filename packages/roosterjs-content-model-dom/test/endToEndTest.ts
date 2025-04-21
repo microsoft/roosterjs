a6071da7367cd05f18dee2c980800a4917039a67
Jiuqing Song <jisong@microsoft.com>
2025-04-21 10:20:04 -0700

    Use FormatContainer to represent DIV with id (#3003)

--- packages/roosterjs-content-model-dom/test/endToEndTest.ts
+++ packages/roosterjs-content-model-dom/test/endToEndTest.ts
@@ -2379,4 +2379,99 @@ describe('End to end test for DOM => Model => DOM/TEXT', () => {
             '<table><tbody><tr><th>test</th></tr></tbody></table>'
         );
     });
+
+    it('div with id, no child', () => {
+        runTest(
+            '<div id="test"></div>',
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'BlockGroup',
+                        blockGroupType: 'FormatContainer',
+                        tagName: 'div',
+                        blocks: [],
+                        format: { id: 'test' },
+                    },
+                ],
+            },
+            '',
+            '<div id="test"></div>'
+        );
+    });
+
+    it('div with id, 1 child', () => {
+        runTest(
+            '<div id="test"><div>test</div></div>',
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'BlockGroup',
+                        blockGroupType: 'FormatContainer',
+                        tagName: 'div',
+                        blocks: [
+                            {
+                                blockType: 'Paragraph',
+                                segments: [
+                                    {
+                                        segmentType: 'Text',
+                                        text: 'test',
+                                        format: {},
+                                    },
+                                ],
+                                format: {},
+                            },
+                        ],
+                        format: { id: 'test' },
+                    },
+                ],
+            },
+            'test',
+            '<div id="test"><div>test</div></div>'
+        );
+    });
+
+    it('div with id, 2 child', () => {
+        runTest(
+            '<div id="test"><div>test1</div><div>test2</div></div>',
+            {
+                blockGroupType: 'Document',
+                blocks: [
+                    {
+                        blockType: 'BlockGroup',
+                        blockGroupType: 'FormatContainer',
+                        tagName: 'div',
+                        blocks: [
+                            {
+                                blockType: 'Paragraph',
+                                segments: [
+                                    {
+                                        segmentType: 'Text',
+                                        text: 'test1',
+                                        format: {},
+                                    },
+                                ],
+                                format: {},
+                            },
+                            {
+                                blockType: 'Paragraph',
+                                segments: [
+                                    {
+                                        segmentType: 'Text',
+                                        text: 'test2',
+                                        format: {},
+                                    },
+                                ],
+                                format: {},
+                            },
+                        ],
+                        format: { id: 'test' },
+                    },
+                ],
+            },
+            'test1\r\ntest2',
+            '<div id="test"><div>test1</div><div>test2</div></div>'
+        );
+    });
 });
