3caa15e9c0d5840ed4c71816c082b29aa0a73ecf
Jiuqing Song (from Dev Box) <jisong@microsoft.com>
2025-04-30 14:44:45 -0700

    imporve

--- demo/scripts/utils/trustedHTMLHandler.ts
+++ demo/scripts/utils/trustedHTMLHandler.ts
@@ -4,7 +4,7 @@ import { DOMCreator } from 'roosterjs-content-model-types';
 export const trustedHTMLHandler: DOMCreator = {
     htmlToDOM: (html: string) => {
         return DOMPurify.sanitize(html, {
-            ADD_TAGS: ['head', 'meta', 'iframe'],
+            ADD_TAGS: ['head', 'meta', '#comment', 'iframe'],
             ADD_ATTR: ['name', 'content'],
             WHOLE_DOCUMENT: true,
             ALLOW_UNKNOWN_PROTOCOLS: true,
