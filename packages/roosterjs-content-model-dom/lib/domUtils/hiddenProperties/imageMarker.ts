d7c4e66bdc5c8b36508158c2492fa648fb26dabb
Julia Roldi (from Dev Box) <juliaroldi@microsoft.com>
2025-05-06 18:58:05 -0300

    comment

--- packages/roosterjs-content-model-dom/lib/domUtils/hiddenProperties/imageMarker.ts
+++ packages/roosterjs-content-model-dom/lib/domUtils/hiddenProperties/imageMarker.ts
@@ -1,14 +1,14 @@
 import { getHiddenProperty, setHiddenProperty } from './hiddenProperty';
 
 /**
- * Get image marker from element. This is used to mark the end of a image in a block element.
+ * Get image marker from element. This is used to mark to store a image property or state.
  */
 export function getImageMarker(element: HTMLElement): string | undefined {
     return getHiddenProperty(element, 'imageMarker');
 }
 
 /**
- * Set image marker to element. This is used to mark the end of a image in a block element.
+ * Set image marker to element. This is used to mark to store a image property or state.
  */
 export function setImageMarker(element: HTMLElement, marker: string): void {
     setHiddenProperty(element, 'imageMarker', marker);
