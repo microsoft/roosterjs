7f0a27883415b5f574a4781b3d4001e19c3b08cc
SOUTHAMERICA\bvalverde <bvalverde@microsoft.com>
2025-05-06 09:36:00 -0600

    Reapply "Refactor getStyleMetadata to not rely on DomCreator and only use Stri…" (#3020)
    This reverts commit 32f47bf7c5af2b581acc7c474b211a1164cd0ee8.

--- packages/roosterjs-content-model-plugins/lib/paste/WordDesktop/getStyleMetadata.ts
+++ packages/roosterjs-content-model-plugins/lib/paste/WordDesktop/getStyleMetadata.ts
@@ -3,6 +3,26 @@ import type { WordMetadata } from './WordMetadata';
 import type { BeforePasteEvent } from 'roosterjs-content-model-types';
 
 const FORMATING_REGEX = /[\n\t'{}"]+/g;
+const STYLE_TAG = '<style>';
+const STYLE_TAG_END = '</style>';
+
+function extractStyleTagsFromHtml(htmlContent: string): string[] {
+    const styles: string[] = [];
+    const lowerCaseHtmlContent = htmlContent.toLowerCase();
+
+    let styleStartIndex = lowerCaseHtmlContent.indexOf(STYLE_TAG);
+    while (styleStartIndex >= 0) {
+        const styleEndIndex = lowerCaseHtmlContent.indexOf(STYLE_TAG_END, styleStartIndex);
+        if (styleEndIndex >= 0) {
+            const styleContent = htmlContent.substring(styleStartIndex + 7, styleEndIndex).trim();
+            styles.push(styleContent);
+            styleStartIndex = lowerCaseHtmlContent.indexOf(STYLE_TAG, styleEndIndex);
+        } else {
+            break;
+        }
+    }
+    return styles;
+}
 
 /**
  * @internal
@@ -26,11 +46,9 @@ const FORMATING_REGEX = /[\n\t'{}"]+/g;
  */
 export function getStyleMetadata(ev: BeforePasteEvent) {
     const metadataMap: Map<string, WordMetadata> = new Map();
-    const styles = ev.fragment.querySelectorAll('style');
+    const headStyles = extractStyleTagsFromHtml(ev.htmlBefore);
 
-    styles.forEach(style => {
-        const text = style?.innerHTML.trim() || '';
-
+    headStyles.forEach(text => {
         let index = 0;
         while (index >= 0) {
             const indexAt = text.indexOf('@', index + 1);
@@ -76,5 +94,6 @@ export function getStyleMetadata(ev: BeforePasteEvent) {
             }
         }
     });
+
     return metadataMap;
 }
