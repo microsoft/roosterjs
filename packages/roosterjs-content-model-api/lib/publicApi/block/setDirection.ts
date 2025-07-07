911a423e71cec3d78802482064f52fda5ebe1cb0
wisaulni <186232390+wisaulni@users.noreply.github.com>
2025-07-07 09:55:04 -0700

    Add auto direction to setDirection (#3082)
    * Initial commit
    
    * Pending changes exported from your codespace
    
    * fix code and tests
    
    * Address comments
    
    ---------
    
    Co-authored-by: wisaulni <wisaulni@microsoft.com>
    Co-authored-by: Bryan Valverde U <bvalverde@microsoft.com>

--- packages/roosterjs-content-model-api/lib/publicApi/block/setDirection.ts
+++ packages/roosterjs-content-model-api/lib/publicApi/block/setDirection.ts
@@ -4,9 +4,9 @@ import type { IEditor } from 'roosterjs-content-model-types';
 /**
  * Set text direction of selected paragraphs (Left to right or Right to left)
  * @param editor The editor to set alignment
- * @param direction Direction value: ltr (Left to right) or rtl (Right to left)
+ * @param direction Direction value: ltr (Left to right) or rtl (Right to left), or 'auto' (Based on the first characters of the document, set the direction automatically)
  */
-export function setDirection(editor: IEditor, direction: 'ltr' | 'rtl') {
+export function setDirection(editor: IEditor, direction: 'ltr' | 'rtl' | 'auto') {
     editor.focus();
 
     editor.formatContentModel(model => setModelDirection(model, direction), {
