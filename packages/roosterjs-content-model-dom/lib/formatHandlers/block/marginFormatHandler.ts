eb9508a22877b20da8e7836784a6a19bfcb278e1
jiuqingsong <jisong@microsoft.com>
2025-08-14 14:42:38 -0700

    Support DL,DT,DD in content model

--- packages/roosterjs-content-model-dom/lib/formatHandlers/block/marginFormatHandler.ts
+++ packages/roosterjs-content-model-dom/lib/formatHandlers/block/marginFormatHandler.ts
@@ -1,6 +1,6 @@
 import { parseValueWithUnit } from '../utils/parseValueWithUnit';
 import type { FormatHandler } from '../FormatHandler';
-import type { MarginFormat } from 'roosterjs-content-model-types';
+import type { DirectionFormat, MarginFormat } from 'roosterjs-content-model-types';
 
 const MarginKeys: (keyof MarginFormat & keyof CSSStyleDeclaration)[] = [
     'marginTop',
@@ -9,13 +9,44 @@ const MarginKeys: (keyof MarginFormat & keyof CSSStyleDeclaration)[] = [
     'marginLeft',
 ];
 
+const AlternativeKeyLtr: Partial<Record<
+    keyof MarginFormat,
+    keyof CSSStyleDeclaration | undefined
+>> = {
+    marginLeft: 'marginInlineStart',
+};
+
+const AlternativeKeyRtl: Partial<Record<
+    keyof MarginFormat,
+    keyof CSSStyleDeclaration | undefined
+>> = {
+    marginRight: 'marginInlineStart',
+};
+
+const LTR: Record<keyof MarginFormat, keyof MarginFormat> = {
+    marginLeft: 'marginRight',
+    marginRight: 'marginLeft',
+    marginTop: 'marginTop',
+    marginBottom: 'marginBottom',
+};
+
 /**
  * @internal
  */
-export const marginFormatHandler: FormatHandler<MarginFormat> = {
+export const marginFormatHandler: FormatHandler<MarginFormat & DirectionFormat> = {
     parse: (format, element, _, defaultStyle) => {
         MarginKeys.forEach(key => {
-            const value = element.style[key] || defaultStyle[key];
+            let value = element.style[key];
+
+            if (!value) {
+                const alterativeKey = (format.direction == 'rtl'
+                    ? AlternativeKeyRtl
+                    : AlternativeKeyLtr)[key];
+
+                value =
+                    defaultStyle[key] ??
+                    ((alterativeKey ? defaultStyle[alterativeKey] : '') as string);
+            }
 
             if (value) {
                 switch (key) {
@@ -39,8 +70,9 @@ export const marginFormatHandler: FormatHandler<MarginFormat> = {
     apply: (format, element, context) => {
         MarginKeys.forEach(key => {
             const value = format[key];
+            const ltrKey = format.direction == 'rtl' ? LTR[key] : key;
 
-            if (value != context.implicitFormat[key]) {
+            if (value != context.implicitFormat[ltrKey]) {
                 element.style[key] = value || '0';
             }
         });
