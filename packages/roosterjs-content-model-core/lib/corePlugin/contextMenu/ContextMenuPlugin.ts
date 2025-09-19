84cac40cb5169a1dee70ef1e6789a420a8273f00
Vi Nguyen <nguyenvi@microsoft.com>
2025-09-19 14:31:14 -0700

    fix

--- packages/roosterjs-content-model-core/lib/corePlugin/contextMenu/ContextMenuPlugin.ts
+++ packages/roosterjs-content-model-core/lib/corePlugin/contextMenu/ContextMenuPlugin.ts
@@ -46,7 +46,8 @@ class ContextMenuPlugin implements PluginWithState<ContextMenuPluginState> {
             { [P in keyof HTMLElementEventMap]: DOMEventRecord<HTMLElementEventMap[P]> }
         > = {
             contextmenu: {
-                beforeDispatch: e => this.onContextMenuEvent(e),
+                beforeDispatch: (event: MouseEvent | PointerEvent) =>
+                    this.onContextMenuEvent(event),
             },
         };
         this.disposer = this.editor.attachDomEvent(<Record<string, DOMEventRecord>>eventHandlers);
@@ -68,10 +69,11 @@ class ContextMenuPlugin implements PluginWithState<ContextMenuPluginState> {
         return this.state;
     }
 
-    private onContextMenuEvent = (e: PointerEvent) => {
+    private onContextMenuEvent = (e: MouseEvent | PointerEvent) => {
         if (this.editor) {
             const allItems: any[] = [];
             const mouseEvent = e as MouseEvent;
+            const pointerEvent = e as PointerEvent;
 
             // ContextMenu event can be triggered from mouse right click or keyboard (e.g. Shift+F10 on Windows)
             // Need to check if this is from keyboard, we need to get target node from selection because in that case
@@ -79,8 +81,8 @@ class ContextMenuPlugin implements PluginWithState<ContextMenuPluginState> {
             const targetNode =
                 mouseEvent.button == ContextMenuButton
                     ? (mouseEvent.target as Node)
-                    : e.pointerType === 'touch' || e.pointerType === 'pen'
-                    ? (e.target as Node)
+                    : pointerEvent?.pointerType === 'touch' || pointerEvent?.pointerType === 'pen'
+                    ? (pointerEvent.target as Node)
                     : this.getFocusedNode(this.editor);
 
             if (targetNode) {
