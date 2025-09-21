8b68ed49581be4bef33f2b1021086a5db9e87be1
Vi Nguyen <74168693+vinguyen12@users.noreply.github.com>
2025-09-21 11:38:04 -0700

    Bump version to 9.37.0 (#3160)
    * add repositionTouchSelection handler
    
    * fix
    
    * define constant
    
    * fix
    
    * fix
    
    * revert previous PR change
    
    * add Touch Plugin
    
    * add repositionTouchSelection in Touch Plugin
    
    * small fix
    
    * add touch plugin to demo page
    
    * add comment
    
    * fix
    
    * export pointer event
    
    * add touchplugin test
    
    * reset pointerEvent to null after trigger action and settimeout to wait for updated cursor position
    
    * Remove tslint from recommended extension list (#3153)
    
    * default to end of the word if user tapped in the middle
    
    * add pointer event double click
    
    * handle selecting first space if selection is an open space with no word on right side
    
    * remove console.log
    
    * clearTimeout when dispose plugin
    
    * address Copilot comment
    
    * fix
    
    * Address comment
    
    * Lint code
    
    * handle the entire flow of touch selection, not rely on browser
    
    * remove test
    
    * move timeout and prevent default to within touch plugin, use doubleclick as general mouse event instead of pointer
    
    * address Copilot comments
    
    * add comment
    
    * fix comment
    
    * create const for regex
    
    * resuse getNodePositionFromEvent
    
    * remove unused change
    
    * fix issue with selection in middle of word
    
    * fix
    
    * address comments
    
    * Add comments
    
    * Remove auto-capitalizatio for first character
    
    * Remove Boolean() wrap
    
    * [Image Edit] Image with borders (#3155)
    
    recalculate the image wrapper size considering the border width
    
    * fix
    
    * remove word matching regex but using !space regex and !punctuation regex instead
    
    * fix version number
    
    ---------
    
    Co-authored-by: Vi Nguyen <nguyenvi@microsoft.com>
    Co-authored-by: Jiuqing Song <jisong@microsoft.com>
    Co-authored-by: Zander Wang <xiaozwan@microsoft.com>
    Co-authored-by: Haowen Chen <haowchen@microsoft.com>
    Co-authored-by: Julia Roldi <87443959+juliaroldi@users.noreply.github.com>

--- packages/roosterjs-content-model-core/lib/corePlugin/selection/SelectionPlugin.ts
+++ packages/roosterjs-content-model-core/lib/corePlugin/selection/SelectionPlugin.ts
@@ -21,7 +21,6 @@ import type {
     ParsedTable,
     TableSelectionInfo,
     TableCellCoordinate,
-    DOMEventRecord,
 } from 'roosterjs-content-model-types';
 
 const MouseLeftButton = 0;
@@ -49,7 +48,6 @@ class SelectionPlugin implements PluginWithState<SelectionPluginState> {
     private isSafari = false;
     private isMac = false;
     private scrollTopCache: number = 0;
-    private pointerEvent: PointerEvent | null = null;
 
     constructor(options: EditorOptions) {
         this.state = {
@@ -101,15 +99,18 @@ class SelectionPlugin implements PluginWithState<SelectionPluginState> {
         this.isSafari = !!env.isSafari;
         this.isMac = !!env.isMac;
         document.addEventListener('selectionchange', this.onSelectionChange);
-        const eventHandlers: Partial<
-            { [P in keyof HTMLElementEventMap]: DOMEventRecord<HTMLElementEventMap[P]> }
-        > = {
-            focus: { beforeDispatch: this.onFocus },
-            drop: { beforeDispatch: this.onDrop },
-            blur: this.isSafari ? undefined : { beforeDispatch: this.onBlur },
-            pointerdown: { beforeDispatch: (event: PointerEvent) => this.onPointerDown(event) },
-        };
-        this.disposer = this.editor.attachDomEvent(<Record<string, DOMEventRecord>>eventHandlers);
+        if (this.isSafari) {
+            this.disposer = this.editor.attachDomEvent({
+                focus: { beforeDispatch: this.onFocus },
+                drop: { beforeDispatch: this.onDrop },
+            });
+        } else {
+            this.disposer = this.editor.attachDomEvent({
+                focus: { beforeDispatch: this.onFocus },
+                blur: { beforeDispatch: this.onBlur },
+                drop: { beforeDispatch: this.onDrop },
+            });
+        }
     }
 
     dispose() {
@@ -245,10 +246,6 @@ class SelectionPlugin implements PluginWithState<SelectionPluginState> {
                 },
             });
         }
-
-        if (rawEvent.defaultPrevented) {
-            this.pointerEvent = null;
-        }
     }
 
     private onMouseMove = (event: Event) => {
@@ -313,25 +310,10 @@ class SelectionPlugin implements PluginWithState<SelectionPluginState> {
 
     private onMouseUp() {
         this.detachMouseEvent();
-        if (this.pointerEvent && this.pointerEvent.pointerType === 'touch') {
-            requestAnimationFrame(() => {
-                if (this.editor && this.pointerEvent) {
-                    // Handle touch selection here since the cursor position is updated
-                    // Work-in-progress
-                }
-                this.pointerEvent = null;
-            });
-        }
     }
 
     private onDrop = () => {
         this.detachMouseEvent();
-    };
-
-    private onPointerDown = (e: PointerEvent) => {
-        if (e.pointerType === 'touch' || e.pointerType === 'pen') {
-            this.pointerEvent = e;
-        }
     };
 
     private onKeyDown(editor: IEditor, rawEvent: KeyboardEvent) {
--- packages/roosterjs-content-model-plugins/lib/edit/EditPlugin.ts
+++ packages/roosterjs-content-model-plugins/lib/edit/EditPlugin.ts
@@ -33,6 +33,14 @@ export type EditOptions = {
      * @returns A boolean
      */
     shouldHandleEnterKey?: ((editor: IEditor) => boolean) | boolean;
+
+    /**
+     * Callback or boolean to determine whether the browser (not Content Model) should handle the Backspace key press.
+     * If the value/callback returns true, Rooster will NOT handle Backspace and will defer to the browser's native behavior.
+     * @param editor - The editor instance (when using callback).
+     * @returns A boolean
+     */
+    shouldHandleBackspaceKey?: ((editor: IEditor) => boolean) | boolean;
 };
 
 const BACKSPACE_KEY = 8;
@@ -201,7 +209,13 @@ export class EditPlugin implements EditorPlugin {
                 case 'Backspace':
                     // Use our API to handle BACKSPACE/DELETE key.
                     // No need to clear cache here since if we rely on browser's behavior, there will be Input event and its handler will reconcile cache
-                    keyboardDelete(editor, rawEvent, this.options.handleExpandedSelectionOnDelete);
+                    if (!this.shouldBrowserHandleBackspace(editor)) {
+                        keyboardDelete(
+                            editor,
+                            rawEvent,
+                            this.options.handleExpandedSelectionOnDelete
+                        );
+                    }
                     break;
 
                 case 'Delete':
@@ -260,15 +274,18 @@ export class EditPlugin implements EditorPlugin {
         let handled = false;
         switch (rawEvent.inputType) {
             case 'deleteContentBackward':
-                handled = keyboardDelete(
-                    editor,
-                    new KeyboardEvent('keydown', {
-                        key: 'Backspace',
-                        keyCode: BACKSPACE_KEY,
-                        which: BACKSPACE_KEY,
-                    }),
-                    this.options.handleExpandedSelectionOnDelete
-                );
+                if (!this.shouldBrowserHandleBackspace(editor)) {
+                    // This logic is Android specific. It's because some Android keyboard doesn't support key and keycode, the value of them is always Unidentified, so we have to manually create a new one.
+                    handled = keyboardDelete(
+                        editor,
+                        new KeyboardEvent('keydown', {
+                            key: 'Backspace',
+                            keyCode: BACKSPACE_KEY,
+                            which: BACKSPACE_KEY,
+                        }),
+                        this.options.handleExpandedSelectionOnDelete
+                    );
+                }
                 break;
             case 'deleteContentForward':
                 handled = keyboardDelete(
@@ -289,6 +306,18 @@ export class EditPlugin implements EditorPlugin {
             // Restore the selection on keyup event to avoid the cursor jump issue
             // See: https://issues.chromium.org/issues/330596261
             this.selectionAfterDelete = editor.getDOMSelection();
+        }
+    }
+
+    private shouldBrowserHandleBackspace(editor: IEditor): boolean {
+        const opt = this.options.shouldHandleBackspaceKey;
+        switch (typeof opt) {
+            case 'function':
+                return opt(editor);
+            case 'boolean':
+                return opt;
+            default:
+                return false;
         }
     }
 }
