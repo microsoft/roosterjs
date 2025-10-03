a082ae2cd36d7d1980da4926d7e8aecddb03b973
Vi Nguyen <nguyenvi@microsoft.com>
2025-10-03 10:36:35 -0700

    add return true

--- packages/roosterjs-content-model-plugins/lib/touch/TouchPlugin.ts
+++ packages/roosterjs-content-model-plugins/lib/touch/TouchPlugin.ts
@@ -238,6 +238,7 @@ export class TouchPlugin implements EditorPlugin {
                             range: newRange,
                             isReverted: false,
                         });
+                        return true;
                     }
                 }
             }
