41eac89c46202812ca144c9a5d8330ccd566d727
Julia Roldi (from Dev Box) <juliaroldi@microsoft.com>
2026-02-06 19:21:04 -0300

    fix

--- packages/roosterjs-content-model-dom/lib/modelApi/editing/mergeModel.ts
+++ packages/roosterjs-content-model-dom/lib/modelApi/editing/mergeModel.ts
@@ -198,8 +198,7 @@ function mergeTables(
                 if (i == 0 && colIndex + j >= table.rows[0].cells.length) {
                     for (let k = 0; k < table.rows.length; k++) {
                         const leftCell = table.rows[k]?.cells[colIndex + j - 1];
-                        const index = leftCell.spanLeft ? colIndex + j + 1 : colIndex + j;
-                        table.rows[k].cells[index] = createTableCell(
+                        table.rows[k].cells[colIndex + j] = createTableCell(
                             false /*spanLeft*/,
                             false /*spanAbove*/,
                             leftCell?.isHeader,
@@ -219,8 +218,7 @@ function mergeTables(
 
                     for (let k = 0; k < table.rows[rowIndex].cells.length; k++) {
                         const aboveCell = table.rows[rowIndex + i - 1]?.cells[k];
-                        const index = aboveCell.spanAbove ? rowIndex + i + 1 : rowIndex + i;
-                        table.rows[index].cells[k] = createTableCell(
+                        table.rows[rowIndex + i].cells[k] = createTableCell(
                             false /*spanLeft*/,
                             false /*spanAbove*/,
                             false /*isHeader*/,
@@ -230,10 +228,7 @@ function mergeTables(
                 }
 
                 const oldCell = table.rows[rowIndex + i].cells[colIndex + j];
-                const cellIndex = oldCell.spanLeft ? colIndex + j + 1 : colIndex + j;
-                const index = oldCell.spanAbove ? rowIndex + i + 1 : rowIndex + i;
-
-                table.rows[index].cells[cellIndex] = newCell;
+                table.rows[rowIndex + i].cells[colIndex + j] = newCell;
 
                 if (i == 0 && j == 0) {
                     const newMarker = createSelectionMarker(marker.format);
