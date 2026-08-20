2c1a2666d61c59e7de1a8fde34417b34e208bb8a
Julia Roldi <juliaroldi@microsoft.com>
2026-08-20 13:13:35 -0300

    prefere target in markown

--- packages/roosterjs-content-model-markdown/lib/plugins/MarkdownPastePlugin.ts
+++ packages/roosterjs-content-model-markdown/lib/plugins/MarkdownPastePlugin.ts
@@ -88,22 +88,22 @@ export class MarkdownPastePlugin implements EditorPlugin {
                 clipboardData.modelBeforePaste
             ) {
                 const modelBeforePaste = cloneModelForPaste(clipboardData.modelBeforePaste);
+                mergeModel(
+                    modelBeforePaste,
+                    convertMarkdownToContentModel(clipboardData.text, {
+                        emptyLine: 'merge',
+                    }),
+                    undefined /*context*/,
+                    {
+                        mergeFormat: 'preferTarget',
+                    }
+                );
 
                 if (this.options.undoConversion) {
                     this.editor.takeSnapshot();
                 }
                 this.editor.formatContentModel(
                     (model, context) => {
-                        mergeModel(
-                            modelBeforePaste,
-                            convertMarkdownToContentModel(clipboardData.text, {
-                                emptyLine: 'merge',
-                            }),
-                            context,
-                            {
-                                mergeFormat: 'preferTarget',
-                            }
-                        );
                         model.blocks = modelBeforePaste.blocks;
                         return true;
                     },
